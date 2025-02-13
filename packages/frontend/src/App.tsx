24c77f365043dda9749722b98d4c296b9fc4c82d
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-13 11:28:01 +0000

    Set up project config files and frontend entry point

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import "./index.css";
+
+export function App() {
+    // Calculate 'vw' and 'vh' units
+    useEffect(() => {
+        const setUnits = () => {
+            const vw = window.innerWidth * 0.01;
+            const vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty("--vw", `${vw}px`);
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        };
+
+        setUnits();
+        window.addEventListener("resize", setUnits);
+
+        return () => {
+            window.removeEventListener("resize", setUnits);
+        };
+    }, []);
+
+    return <div>Online Shop</div>;
+}
