09f616978d8e7151a90aa76e47f3e8160a35850a
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-14 10:03:38 +0000

    Render Logo component within Header component

--- packages/frontend/src/features/Header/index.tsx
+++ packages/frontend/src/features/Header/index.tsx
@@ -1,9 +1,11 @@
+import { Logo } from "./components/Logo";
 import { Navigation } from "./components/Navigation";
 import styles from "./index.module.css";
 
 export function Header() {
     return (
         <header className={styles["header"]}>
+            <Logo />
             <Navigation />
         </header>
     );
