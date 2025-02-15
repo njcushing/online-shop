13edee8878fa18c762bb3cbdf6bf342188955f67
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-15 18:20:20 +0000

    Set up empty routes for /create-account and /login

--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -4,6 +4,16 @@ import { Pages } from "@/pages";
 export function Router() {
     const browserRouter = createBrowserRouter([
         {
+            path: "/create-account",
+            element: <div></div>,
+            errorElement: <div></div>,
+        },
+        {
+            path: "/login",
+            element: <div></div>,
+            errorElement: <div></div>,
+        },
+        {
             path: "/",
             element: <Pages.Home />,
             errorElement: <div></div>,
