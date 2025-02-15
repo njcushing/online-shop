2b111106aaf3ad84f5ead4b451dad246aafd4630
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-15 18:19:11 +0000

    Set up routing within Root page component

--- packages/frontend/src/pages/Root/index.tsx
+++ packages/frontend/src/pages/Root/index.tsx
@@ -1,5 +1,25 @@
+import { Outlet } from "react-router-dom";
+import { Home } from "../Home";
+import { Product } from "../Product";
 import styles from "./index.module.css";
 
+export const routes = [
+    {
+        path: "",
+        element: <Home />,
+        errorElement: <div></div>,
+    },
+    {
+        path: "product/:productId",
+        element: <Product />,
+        errorElement: <div></div>,
+    },
+];
+
 export function Root() {
-    return <div className={styles["page"]}></div>;
+    return (
+        <div className={styles["page"]}>
+            <Outlet />
+        </div>
+    );
 }
