4a286fa3874ea9e01b6fc577fd19499a55e08cb4
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-16 18:08:48 +0000

    Render LoginForm within Login page component

--- packages/frontend/src/pages/Login/index.tsx
+++ packages/frontend/src/pages/Login/index.tsx
@@ -1,5 +1,10 @@
+import { LoginForm } from "@/features/LoginForm";
 import styles from "./index.module.css";
 
 export function Login() {
-    return <div className={styles["page"]}></div>;
+    return (
+        <div className={styles["page"]}>
+            <LoginForm />
+        </div>
+    );
 }
