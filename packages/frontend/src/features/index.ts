d48fb568e13368d10efb533cebbb68e17a79680d
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-16 17:36:34 +0000

    Include LoginForm in feature components' export file

--- packages/frontend/src/features/index.ts
+++ packages/frontend/src/features/index.ts
@@ -1,9 +1,11 @@
 import { AccountCreationForm } from "./AccountCreationForm";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
+import { LoginForm } from "./LoginForm";
 
 export const Features = {
     AccountCreationForm,
     Header,
     Hero,
+    LoginForm,
 };
