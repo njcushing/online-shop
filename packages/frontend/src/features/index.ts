d27419b94fef27661dedbd25951d8dd1ea072c72
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-16 17:31:39 +0000

    Include AccountCreationForm in feature components' export file

--- packages/frontend/src/features/index.ts
+++ packages/frontend/src/features/index.ts
@@ -1,7 +1,9 @@
+import { AccountCreationForm } from "./AccountCreationForm";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
 
 export const Features = {
+    AccountCreationForm,
     Header,
     Hero,
 };
