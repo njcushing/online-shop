433ba4f624fe2d57d569910081eab09ea0b90185
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-16 17:33:10 +0000

    Include CreateAccount in page components' export file

--- packages/frontend/src/pages/index.ts
+++ packages/frontend/src/pages/index.ts
@@ -1,8 +1,10 @@
+import { CreateAccount } from "./CreateAccount";
 import { Home } from "./Home";
 import { Product } from "./Product";
 import { Root, Routes as RootRoutes } from "./Root";
 
 export const Pages = {
+    CreateAccount,
     Home,
     Product,
     Root,
