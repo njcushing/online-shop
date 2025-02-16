63d54ada42f6776723f4dd98cc988d36d52ec083
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-16 17:37:39 +0000

    Include Login in page components' export file

--- packages/frontend/src/pages/index.ts
+++ packages/frontend/src/pages/index.ts
@@ -1,11 +1,13 @@
 import { CreateAccount } from "./CreateAccount";
 import { Home } from "./Home";
+import { Login } from "./Login";
 import { Product } from "./Product";
 import { Root, Routes as RootRoutes } from "./Root";
 
 export const Pages = {
     CreateAccount,
     Home,
+    Login,
     Product,
     Root,
 };
