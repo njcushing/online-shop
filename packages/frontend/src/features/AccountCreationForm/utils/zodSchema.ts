425afb9eb30aca545539d2173fe3aadfd969ca7d
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-19 22:20:29 +0000

    Update email field error to provide example for correct email format

--- packages/frontend/src/features/AccountCreationForm/utils/zodSchema.ts
+++ packages/frontend/src/features/AccountCreationForm/utils/zodSchema.ts
@@ -9,7 +9,9 @@ export const accountCreationFormDataSchema = z
     .object({
         firstName: z.string().min(1, { message: "Please enter your first name" }),
         lastName: z.string().min(1, { message: "Please enter your last name" }),
-        email: z.string().email("Invalid email format"),
+        email: z
+            .string()
+            .email("Invalid email format. Please use the following format: example@email.com"),
         password: z
             .string()
             .min(8, { message: "Please enter a password at least 8 characters in length" }),
