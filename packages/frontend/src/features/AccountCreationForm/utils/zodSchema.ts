b185d9fac69e68c4bce56839a376fdc3349dc504
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-19 22:20:02 +0000

    Update account password requirement to match NIST guidelines

--- packages/frontend/src/features/AccountCreationForm/utils/zodSchema.ts
+++ packages/frontend/src/features/AccountCreationForm/utils/zodSchema.ts
@@ -1,12 +1,21 @@
 import { z } from "zod";
 
+/*
+ * Password requirement of 8+ characters in length in line with NIST guidelines
+ * https://pages.nist.gov/800-63-3/sp800-63b.html
+ */
+
 export const accountCreationFormDataSchema = z
     .object({
         firstName: z.string().min(1, { message: "Please enter your first name" }),
         lastName: z.string().min(1, { message: "Please enter your last name" }),
         email: z.string().email("Invalid email format"),
-        password: z.string().min(1, { message: "Please enter your password" }),
-        confirmPassword: z.string().min(1, { message: "Please confirm your password" }),
+        password: z
+            .string()
+            .min(8, { message: "Please enter a password at least 8 characters in length" }),
+        confirmPassword: z
+            .string()
+            .min(8, { message: "Please enter a password at least 8 characters in length" }),
     })
     .refine((data) => data.password === data.confirmPassword, {
         message: "Passwords do not match",
