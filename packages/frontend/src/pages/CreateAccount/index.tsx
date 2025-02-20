1d4a9c0f6f4509e70005203b748a765fb2393a0b
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-20 11:12:35 +0000

    Add a heading to CreateAccount page and adjust layout

--- packages/frontend/src/pages/CreateAccount/index.tsx
+++ packages/frontend/src/pages/CreateAccount/index.tsx
@@ -4,7 +4,10 @@ import styles from "./index.module.css";
 export function CreateAccount() {
     return (
         <div className={styles["page"]}>
-            <AccountCreationForm />
+            <div className={styles["page-content"]}>
+                <h1 className={styles["page-heading"]}>Sign up to get started</h1>
+                <AccountCreationForm />
+            </div>
         </div>
     );
 }
