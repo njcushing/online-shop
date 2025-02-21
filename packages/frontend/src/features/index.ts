b827b3560f1057676d54cfef7b5a0b0c053744c0
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-21 11:29:58 +0000

    Include SetPersonalInformationForm in feature components' export file

--- packages/frontend/src/features/index.ts
+++ packages/frontend/src/features/index.ts
@@ -2,10 +2,12 @@ import { AccountCreationForm } from "./AccountCreationForm";
 import { Header } from "./Header";
 import { Hero } from "./Hero";
 import { LoginForm } from "./LoginForm";
+import { SetPersonalInformationForm } from "./SetPersonalInformationForm";
 
 export const Features = {
     AccountCreationForm,
     Header,
     Hero,
     LoginForm,
+    SetPersonalInformationForm,
 };
