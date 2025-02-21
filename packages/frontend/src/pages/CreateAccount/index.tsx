d9fd538f3d234ab48fa96d7f09c42cfafbc967aa
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-21 11:24:36 +0000

    Provide state and setter about current account creation stage in context

--- packages/frontend/src/pages/CreateAccount/index.tsx
+++ packages/frontend/src/pages/CreateAccount/index.tsx
@@ -1,13 +1,50 @@
+import { createContext, useMemo, useState } from "react";
 import { AccountCreationForm } from "@/features/AccountCreationForm";
 import styles from "./index.module.css";
 
+export interface ICreateAccountContext {
+    accountCreationStage: number;
+    setAccountCreationStage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const defaultCreateAccountContext: ICreateAccountContext = {
+    accountCreationStage: 0,
+    setAccountCreationStage: () => {},
+};
+
+export const CreateAccountContext = createContext<ICreateAccountContext>(
+    defaultCreateAccountContext,
+);
+
 export function CreateAccount() {
+    const [accountCreationStage, setAccountCreationStage] = useState<number>(
+        defaultCreateAccountContext.accountCreationStage,
+    );
+
+    const pageContent = useMemo(() => {
+        switch (accountCreationStage) {
+            case 0:
+                return (
+                    <>
+                        <h1 className={styles["page-heading"]}>Sign up to get started</h1>
+                        <AccountCreationForm />
+                    </>
+                );
+            default:
+                return null;
+        }
+    }, [accountCreationStage]);
+
     return (
-        <div className={styles["page"]}>
-            <div className={styles["page-content"]}>
-                <h1 className={styles["page-heading"]}>Sign up to get started</h1>
-                <AccountCreationForm />
+        <CreateAccountContext.Provider
+            value={useMemo(
+                () => ({ accountCreationStage, setAccountCreationStage }),
+                [accountCreationStage, setAccountCreationStage],
+            )}
+        >
+            <div className={styles["page"]}>
+                <div className={styles["page-content"]}>{pageContent}</div>
             </div>
-        </div>
+        </CreateAccountContext.Provider>
     );
 }
