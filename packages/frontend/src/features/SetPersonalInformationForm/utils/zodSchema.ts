40b28018790963111a9662b677f254a047f526fb
njcushing <129661299+njcushing@users.noreply.github.com>
2025-02-23 12:07:46 +0000

    Write validator for date of birth ('dob') field

--- packages/frontend/src/features/SetPersonalInformationForm/utils/zodSchema.ts
+++ packages/frontend/src/features/SetPersonalInformationForm/utils/zodSchema.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import dayjs from "dayjs";
 import parsePhoneNumberFromString from "libphonenumber-js";
 
 export type PersonalInformationFormData = {
@@ -6,7 +7,13 @@ export type PersonalInformationFormData = {
     lastName?: string;
     phone?: string;
     gender?: "male" | "female" | "other" | "unspecified";
+    dob?: Date;
 };
+
+/*
+ * Minimum age requirement of 13 in line with UK GDPR & ICO guidance on minimum age for data consent
+ * https://ico.org.uk/for-organisations/uk-gdpr-guidance-and-resources/childrens-information/children-and-the-uk-gdpr/what-are-the-rules-about-an-iss-and-consent
+ */
 
 export const personalInformationFormDataSchema: z.ZodType<PersonalInformationFormData> = z.object({
     firstName: z.string().optional(),
@@ -27,4 +34,10 @@ export const personalInformationFormDataSchema: z.ZodType<PersonalInformationFor
     gender: z.enum(["male", "female", "other", "unspecified"], {
         message: "Please select an option",
     }),
+    dob: z.coerce
+        .date()
+        .refine((dob) => dayjs(dob).isBefore(dayjs().subtract(13, "year")), {
+            message: "You must be at least 13 years old to make an account",
+        })
+        .optional(),
 });
