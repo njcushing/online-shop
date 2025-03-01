4153e02943b2388b4270277776324383dd243ed1
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-01 11:58:07 +0000

    Navigate to root URL path when form successfully submitted

--- packages/frontend/src/pages/CreateAccount/index.tsx
+++ packages/frontend/src/pages/CreateAccount/index.tsx
@@ -1,27 +1,24 @@
 import { useMemo, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { AccountCreationForm } from "@/features/AccountCreationForm";
 import { SetPersonalInformationForm } from "@/features/SetPersonalInformationForm";
 import styles from "./index.module.css";
 
 export function CreateAccount() {
+    const navigate = useNavigate();
+
     const [currentStage, setCurrentStage] = useState<number>(1);
 
     const pageContent = useMemo(() => {
         switch (currentStage) {
             case 0:
-                return (
-                    <AccountCreationForm
-                        onSuccess={() => {
-                            setCurrentStage(1);
-                        }}
-                    />
-                );
+                return <AccountCreationForm onSuccess={() => setCurrentStage(1)} />;
             case 1:
-                return <SetPersonalInformationForm />;
+                return <SetPersonalInformationForm onSuccess={() => navigate("/")} />;
             default:
                 return null;
         }
-    }, [currentStage]);
+    }, [navigate, currentStage]);
 
     return (
         <div className={styles["page"]}>
