877e4625bc2e84d0c8c753dbafacfe5f7718295f
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-01 13:33:26 +0000

    Include new page components in export file

--- packages/frontend/src/pages/index.ts
+++ packages/frontend/src/pages/index.ts
@@ -1,15 +1,19 @@
 import { CreateAccount } from "./CreateAccount";
 import { Home } from "./Home";
 import { Login } from "./Login";
+import { PrivacyPolicy } from "./PrivacyPolicy";
 import { Product } from "./Product";
 import { Root, Routes as RootRoutes } from "./Root";
+import { TermsAndConditions } from "./TermsAndConditions";
 
 export const Pages = {
     CreateAccount,
     Home,
     Login,
+    PrivacyPolicy,
     Product,
     Root,
+    TermsAndConditions,
 };
 
 export const Routes = {
