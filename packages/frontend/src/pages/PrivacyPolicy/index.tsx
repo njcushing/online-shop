f40c1a2204ac074961493c37ea3c93099be4b5f8
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-01 13:32:39 +0000

    Write boilerplate for Privacy Policy page component

---
+++ packages/frontend/src/pages/PrivacyPolicy/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export function PrivacyPolicy() {
+    return (
+        <div className={styles["page"]}>
+            <div className={styles["page-content"]}>Privacy Policy</div>
+        </div>
+    );
+}
