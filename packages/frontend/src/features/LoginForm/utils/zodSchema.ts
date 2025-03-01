4e0fddec44ceced8f3d1828a09a4e86192243cdf
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-01 12:34:48 +0000

    Write Zod schema for login form

---
+++ packages/frontend/src/features/LoginForm/utils/zodSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export type LoginFormData = {
+    email: string;
+    password: string;
+};
+
+export const loginFormDataSchema: z.ZodType<LoginFormData> = z.object({
+    email: z.string({ message: "Please enter your email address" }),
+    password: z.string({ message: "Please enter your password" }),
+});
