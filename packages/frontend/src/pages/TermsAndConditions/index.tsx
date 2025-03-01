cd21f211a5833e80740daa0268d8638cac55289c
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-01 13:31:50 +0000

    Write boilerplate for Terms and Conditions page component

---
+++ packages/frontend/src/pages/TermsAndConditions/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export function TermsAndConditions() {
+    return (
+        <div className={styles["page"]}>
+            <div className={styles["page-content"]}>Terms and Conditions</div>
+        </div>
+    );
+}
