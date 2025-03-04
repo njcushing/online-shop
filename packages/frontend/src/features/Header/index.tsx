c8e59275488788eadd5f4c6486b909f7f3f05fb8
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-04 22:13:15 +0000

    Move Header off screen on scroll down, and vice versa

--- packages/frontend/src/features/Header/index.tsx
+++ packages/frontend/src/features/Header/index.tsx
@@ -1,9 +1,24 @@
+import { useState, useEffect, useRef } from "react";
 import { Navigation } from "./components/Navigation";
 import styles from "./index.module.css";
 
 export function Header() {
+    const lastScrollPos = useRef<number>(0);
+    const [openState, setOpenState] = useState<boolean>(true);
+    useEffect(() => {
+        const scrollDirectionCheck = () => {
+            const newScrollPos = document.body.getBoundingClientRect().top;
+            setOpenState(newScrollPos > lastScrollPos.current);
+            lastScrollPos.current = newScrollPos;
+        };
+
+        window.addEventListener("scroll", scrollDirectionCheck);
+
+        return () => window.removeEventListener("scroll", scrollDirectionCheck);
+    }, []);
+
     return (
-        <header className={styles["header"]}>
+        <header className={`${styles["header"]} ${styles[openState ? "open" : "closed"]}`}>
             <Navigation />
         </header>
     );
