238f2a7753994780eb5c37dd2178788775a5998b
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-05 22:38:26 +0000

    Render CategoryBanner in Category page

--- packages/frontend/src/pages/Category/index.tsx
+++ packages/frontend/src/pages/Category/index.tsx
@@ -1,5 +1,10 @@
+import { CategoryBanner } from "@/features/CategoryBanner";
 import styles from "./index.module.css";
 
 export function Category() {
-    return <div className={styles["page"]}></div>;
+    return (
+        <div className={styles["page"]}>
+            <CategoryBanner />
+        </div>
+    );
 }
