0be427b093c826ac0226dd155d7f1cb240bec7d4
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-07 12:00:00 +0000

    Validate URL path and provide context to child components

--- packages/frontend/src/pages/Category/index.tsx
+++ packages/frontend/src/pages/Category/index.tsx
@@ -1,10 +1,64 @@
+import { createContext, useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { Pages } from "@/pages";
 import { CategoryBanner } from "@/features/CategoryBanner";
+import { categories, Category as CategoryDataType } from "@/utils/products/categories";
 import styles from "./index.module.css";
 
+export interface ICategoryContext {
+    urlPathFull: string;
+    urlPathSplit: string[];
+    categoryData: CategoryDataType[];
+}
+
+export const defaultCategoryContext: ICategoryContext = {
+    urlPathFull: "",
+    urlPathSplit: [],
+    categoryData: [],
+};
+
+export const CategoryContext = createContext<ICategoryContext>(defaultCategoryContext);
+
+const validatePath = (
+    stage: number,
+    urlPathSplit: string[],
+    currentCategories: CategoryDataType[],
+    categoriesArray: CategoryDataType[],
+): CategoryDataType[] | null => {
+    if (stage === urlPathSplit.length) return null;
+
+    const slug = urlPathSplit[stage];
+    const categoryIndex = categoriesArray.findIndex((category) => category.slug === slug);
+    if (categoryIndex > -1) {
+        const foundCategory = categoriesArray[categoryIndex];
+        const newCategories = [...currentCategories, categoriesArray[categoryIndex]];
+        if (stage === urlPathSplit.length - 1) return newCategories;
+        if (!foundCategory.subcategories) return null;
+        return validatePath(stage + 1, urlPathSplit, newCategories, foundCategory.subcategories);
+    }
+
+    return null;
+};
+
 export function Category() {
+    const { "*": urlPathFull } = useParams();
+    const urlPathSplit = useMemo(() => (urlPathFull ? urlPathFull.split("/") : []), [urlPathFull]);
+
+    const categoryData = useMemo(() => {
+        return validatePath(0, urlPathSplit, [], categories) || [];
+    }, [urlPathSplit]);
+
+    const contextValue = useMemo<ICategoryContext>(() => {
+        return { urlPathFull: urlPathFull || "", urlPathSplit, categoryData };
+    }, [urlPathFull, urlPathSplit, categoryData]);
+
+    if (categoryData.length === 0) return <Pages.ErrorPage />;
+
     return (
-        <div className={styles["page"]}>
-            <CategoryBanner />
-        </div>
+        <CategoryContext.Provider value={contextValue}>
+            <div className={styles["page"]}>
+                <CategoryBanner />
+            </div>
+        </CategoryContext.Provider>
     );
 }
