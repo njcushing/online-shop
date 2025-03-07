a527862f4e8f52b5fa7a64e500bb085b67bccc4a
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-07 12:17:02 +0000

    Update CategoryBanner component to utilise correct category information

--- packages/frontend/src/features/CategoryBanner/index.tsx
+++ packages/frontend/src/features/CategoryBanner/index.tsx
@@ -1,25 +1,15 @@
+import { useContext } from "react";
+import { CategoryContext } from "@/pages/Category";
 import { Breadcrumbs, Image } from "@mantine/core";
 import { Link } from "react-router-dom";
 import styles from "./index.module.css";
 
-const pathFromHome = [
-    { title: "Home", to: "/" },
-    { title: "Category", to: "/c/category" },
-];
-
-const subcategories = [
-    { title: "Subcategory 1", to: "subcategory-1", img: "" },
-    { title: "Subcategory 2", to: "subcategory-2", img: "" },
-    { title: "Subcategory 3", to: "subcategory-3", img: "" },
-    { title: "Subcategory 4", to: "subcategory-4", img: "" },
-    { title: "Subcategory 5", to: "subcategory-5", img: "" },
-    { title: "Subcategory 6", to: "subcategory-6", img: "" },
-    { title: "Subcategory 7", to: "subcategory-7", img: "" },
-    { title: "Subcategory 8", to: "subcategory-8", img: "" },
-    { title: "Subcategory 9", to: "subcategory-9", img: "" },
-];
-
 export function CategoryBanner() {
+    const { urlPathSplit, categoryData } = useContext(CategoryContext);
+
+    const currentCategory = categoryData[categoryData.length - 1];
+    const { name, description, subcategories } = currentCategory;
+
     return (
         <div className={styles["category-banner"]}>
             <Breadcrumbs
@@ -29,41 +19,48 @@ export function CategoryBanner() {
                     breadcrumb: styles["category-breadcrumb"],
                 }}
             >
-                {pathFromHome.map((item, i) => {
-                    const { title, to } = item;
-                    const current = i === pathFromHome.length - 1;
+                <Link to="/">Home</Link>
+                {categoryData.map((category, i) => {
+                    const { slug, name: catName } = category;
+                    const current = i === categoryData.length - 1;
                     return !current ? (
-                        <Link to={to} key={title}>
-                            {title}
+                        <Link to={slug} key={catName}>
+                            {catName}
                         </Link>
                     ) : (
-                        <span className={styles["category-breadcrumb-current"]} key={title}>
-                            {title}
+                        <span className={styles["category-breadcrumb-current"]} key={catName}>
+                            {catName}
                         </span>
                     );
                 })}
             </Breadcrumbs>
 
-            <h1 className={styles["category-header"]}>Category Header</h1>
-            <p className={styles["category-description"]}>Category description</p>
+            <h1 className={styles["category-header"]}>{name}</h1>
+            <p className={styles["category-description"]}>{description}</p>
 
-            <div className={styles["subcategory-links"]}>
-                {subcategories.map((item) => {
-                    const { title, to, img } = item;
-                    return (
-                        <Link to={to} key={title} className={styles["subcategory-link"]}>
-                            <Image
-                                radius="md"
-                                src={img}
-                                w={120}
-                                h={120}
-                                className={styles["subcategory-link-image"]}
-                            />
-                            {title}
-                        </Link>
-                    );
-                })}
-            </div>
+            {subcategories && (
+                <div className={styles["subcategory-links"]}>
+                    {subcategories.map((subcategory) => {
+                        const { slug, name: catName, img } = subcategory;
+                        return (
+                            <Link
+                                to={[...urlPathSplit, slug].join("/")}
+                                key={catName}
+                                className={styles["subcategory-link"]}
+                            >
+                                <Image
+                                    radius="md"
+                                    src={img || ""}
+                                    w={120}
+                                    h={120}
+                                    className={styles["subcategory-link-image"]}
+                                />
+                                {catName}
+                            </Link>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 }
