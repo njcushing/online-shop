d1d2af8e323c0c607fa94ecf88d7e47f60833ed8
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-08 20:41:08 +0000

    Create Product type and write function for generating mock products

---
+++ packages/frontend/src/utils/products/product/index.ts
@@ -0,0 +1,37 @@
+import { v4 as uuid } from "uuid";
+
+export type Product = {
+    id: string;
+    name: string;
+    img: string;
+    price: {
+        current: number;
+        base: number;
+    };
+    rating: {
+        value: number;
+        quantity: number;
+    };
+    stock: number;
+    releaseDate: string;
+};
+
+export const generateMockProduct = (): Product => {
+    const basePrice = Math.floor(Math.random() * 16000 + 6000);
+
+    return {
+        id: uuid(),
+        name: "Product Name",
+        img: "",
+        price: {
+            current: Math.random() < 0.5 ? basePrice : Math.floor(basePrice * Math.random()),
+            base: basePrice,
+        },
+        rating: {
+            value: Math.random() * 2 + 3,
+            quantity: Math.floor(Math.random() * 200 + 50),
+        },
+        stock: Math.floor(Math.random() * 100),
+        releaseDate: new Date().toISOString(),
+    };
+};
