b5a442a724d5011e8c9ece7d7c26c2d0a6b1bc9c
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-08 18:25:36 +0000

    Add image, name and price elements to Item component

--- packages/frontend/src/features/ItemList/components/Item/index.tsx
+++ packages/frontend/src/features/ItemList/components/Item/index.tsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import { Image } from "@mantine/core";
 import styles from "./index.module.css";
 
 export function Item() {
-    return <div className={styles["item"]}></div>;
+    return (
+        <Link to="/p/product" className={styles["item"]}>
+            <Image className={styles["item-image"]} />
+            <p className={styles["item-name"]}>Item Name</p>
+            <span className={styles["item-price"]}>Item Price</span>
+        </Link>
+    );
 }
