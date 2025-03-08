2eb640cdc86c6bf701ddef24de986e1bd5c6332a
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-08 19:39:30 +0000

    Display item current price, base price, and price reduction percentage

--- packages/frontend/src/features/ItemList/components/Item/index.tsx
+++ packages/frontend/src/features/ItemList/components/Item/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { Image, Rating } from "@mantine/core";
 import styles from "./index.module.css";
@@ -19,11 +19,30 @@ const mockItemData = {
 export function Item() {
     const [itemData, setItemData] = useState(mockItemData);
 
+    const priceReductionString = useMemo<string>(() => {
+        const reduction = (itemData.price.current / Math.max(itemData.price.base, 1)) * 100 - 100;
+        return `${reduction < 0 ? "" : "+"}${reduction.toFixed(0)}%`;
+    }, [itemData.price]);
+
     return (
         <Link to="/p/product" className={styles["item"]}>
             <Image className={styles["item-image"]} src={itemData.img} />
             <p className={styles["item-name"]}>{itemData.name}</p>
-            <span className={styles["item-price"]}>{itemData.price.current}</span>
+            <div className={styles["item-price-container"]}>
+                <span className={styles["item-price-current"]}>
+                    £{(itemData.price.current / 100).toFixed(2)}
+                </span>
+                {itemData.price.current !== itemData.price.base && (
+                    <>
+                        <span className={styles["item-price-base"]}>
+                            £{(itemData.price.base / 100).toFixed(2)}
+                        </span>
+                        <span className={styles["item-price-discount-percentage"]}>
+                            {priceReductionString}
+                        </span>
+                    </>
+                )}
+            </div>
             <div className={styles["item-rating-container"]}>
                 <Rating
                     className={styles["item-rating"]}
