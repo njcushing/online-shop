9ec3fa43a660d3d0d893f7bef03e5c73f92ee9a1
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-08 18:32:09 +0000

    Display Items in ItemList component and design grid layout

--- packages/frontend/src/features/ItemList/index.tsx
+++ packages/frontend/src/features/ItemList/index.tsx
@@ -1,5 +1,18 @@
+import { Item } from "./components/Item";
 import styles from "./index.module.css";
 
 export function ItemList() {
-    return <section className={styles["item-list"]}></section>;
+    return (
+        <section className={styles["item-list"]}>
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+        </section>
+    );
 }
