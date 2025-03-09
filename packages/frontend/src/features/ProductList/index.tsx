17f4d234d3629fe3bc0e2fb2ba3b4ca3ce5393c9
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-09 13:22:20 +0000

    Move padding to width-controlling elements where applicable
    The effective maximum page width was actually 1280px + padding, but I want it to be 1280px total including the padding.

--- packages/frontend/src/features/ProductList/index.tsx
+++ packages/frontend/src/features/ProductList/index.tsx
@@ -25,59 +25,55 @@ export function ProductList() {
 
     return (
         <section className={styles["product-list"]}>
-            <div className={styles["product-list-width-controller"]}>
-                {products && products.length > 0 && (
-                    <div className={styles["product-list-category-group"]}>
-                        {products.map((product) => (
-                            <Product productData={product} key={product.id} />
-                        ))}
-                    </div>
-                )}
-                {subcategories &&
-                    subcategories.length > 0 &&
-                    subcategories.map((subcategory, i) => {
-                        if (!subcategory.products) return null;
-                        if (subcategory.products.length === 0) return null;
+            {products && products.length > 0 && (
+                <div className={styles["product-list-category-group"]}>
+                    {products.map((product) => (
+                        <Product productData={product} key={product.id} />
+                    ))}
+                </div>
+            )}
+            {subcategories &&
+                subcategories.length > 0 &&
+                subcategories.map((subcategory, i) => {
+                    if (!subcategory.products) return null;
+                    if (subcategory.products.length === 0) return null;
 
-                        let productsToDisplay = 7;
-                        if (breakpointLg) productsToDisplay = 5;
-                        if (breakpointMd) productsToDisplay = 5;
-                        if (breakpointXs) productsToDisplay = 3;
+                    let productsToDisplay = 7;
+                    if (breakpointLg) productsToDisplay = 5;
+                    if (breakpointMd) productsToDisplay = 5;
+                    if (breakpointXs) productsToDisplay = 3;
 
-                        return (
-                            <Fragment key={subcategory.name}>
-                                <div className={styles["product-list-category-group"]}>
-                                    <div className={styles["subcategory-information"]}>
-                                        <h2 className={styles["subcategory-name"]}>
-                                            {subcategory.name}
-                                        </h2>
-                                        <p className={styles["subcategory-description"]}>
-                                            {subcategory.description}
-                                        </p>
-                                        <NavLink
-                                            component={Link}
-                                            to={[...urlPathSplit, subcategory.slug].join("/")}
-                                            label="Shop all"
-                                            rightSection={<CaretRight size={16} />}
-                                            style={{
-                                                flexShrink: 0,
-                                                padding: "8px",
-                                                marginLeft: "-8px",
-                                                width: "calc(100% + 8px)",
-                                            }}
-                                        />
-                                    </div>
-                                    {subcategory.products
-                                        .slice(0, productsToDisplay)
-                                        .map((product) => (
-                                            <Product productData={product} key={product.id} />
-                                        ))}
+                    return (
+                        <Fragment key={subcategory.name}>
+                            <div className={styles["product-list-category-group"]}>
+                                <div className={styles["subcategory-information"]}>
+                                    <h2 className={styles["subcategory-name"]}>
+                                        {subcategory.name}
+                                    </h2>
+                                    <p className={styles["subcategory-description"]}>
+                                        {subcategory.description}
+                                    </p>
+                                    <NavLink
+                                        component={Link}
+                                        to={[...urlPathSplit, subcategory.slug].join("/")}
+                                        label="Shop all"
+                                        rightSection={<CaretRight size={16} />}
+                                        style={{
+                                            flexShrink: 0,
+                                            padding: "8px",
+                                            marginLeft: "-8px",
+                                            width: "calc(100% + 8px)",
+                                        }}
+                                    />
                                 </div>
-                                {i < currentCategory.subcategories!.length - 1 && <Divider />}
-                            </Fragment>
-                        );
-                    })}
-            </div>
+                                {subcategory.products.slice(0, productsToDisplay).map((product) => (
+                                    <Product productData={product} key={product.id} />
+                                ))}
+                            </div>
+                            {i < currentCategory.subcategories!.length - 1 && <Divider />}
+                        </Fragment>
+                    );
+                })}
         </section>
     );
 }
