5cba7194509659916d9f4d3433926fc69c47a066
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-09 11:36:24 +0000

    Display information about subcategories and separate them

--- packages/frontend/src/features/ProductList/index.tsx
+++ packages/frontend/src/features/ProductList/index.tsx
@@ -1,5 +1,6 @@
 import { useContext, useMemo } from "react";
 import { CategoryContext } from "@/pages/Category";
+import { Divider } from "@mantine/core";
 import { Product } from "./components/Product";
 import styles from "./index.module.css";
 
@@ -11,19 +12,38 @@ export function ProductList() {
     }, [categoryData]);
     if (!currentCategory) return null;
 
+    const { subcategories, products } = currentCategory;
+
     return (
         <section className={styles["product-list"]}>
-            {currentCategory.products &&
-                currentCategory.products.map((product) => (
-                    <Product productData={product} key={product.id} />
-                ))}
-            {currentCategory.subcategories &&
-                currentCategory.subcategories.map((subcategory) => {
-                    return subcategory.products
-                        ? subcategory.products
-                              .slice(0, 6)
-                              .map((product) => <Product productData={product} key={product.id} />)
-                        : null;
+            {products && products.length > 0 && (
+                <div className={styles["product-list-category-group"]}>
+                    {products.map((product) => (
+                        <Product productData={product} key={product.id} />
+                    ))}
+                </div>
+            )}
+            {subcategories &&
+                subcategories.length > 0 &&
+                subcategories.map((subcategory, i) => {
+                    if (!subcategory.products) return null;
+                    if (subcategory.products.length === 0) return null;
+                    return (
+                        <>
+                            <div className={styles["product-list-category-group"]}>
+                                <div className={styles["subcategory-information"]}>
+                                    <p className={styles["subcategory-name"]}>{subcategory.name}</p>
+                                    <p className={styles["subcategory-description"]}>
+                                        {subcategory.description}
+                                    </p>
+                                </div>
+                                {subcategory.products.slice(0, 7).map((product) => (
+                                    <Product productData={product} key={product.id} />
+                                ))}
+                            </div>
+                            {i < currentCategory.subcategories!.length - 1 && <Divider />}
+                        </>
+                    );
                 })}
         </section>
     );
