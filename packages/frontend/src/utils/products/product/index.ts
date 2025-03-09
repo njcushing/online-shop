18f742373af034107b86008d7a4cb55c508de2e6
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-09 13:51:01 +0000

    Add 'description' field to Product type

--- packages/frontend/src/utils/products/product/index.ts
+++ packages/frontend/src/utils/products/product/index.ts
@@ -1,9 +1,11 @@
 import dayjs from "dayjs";
+import { loremIpsum } from "lorem-ipsum";
 import { v4 as uuid } from "uuid";
 
 export type Product = {
     id: string;
     name: string;
+    description: string[];
     img: string;
     price: {
         current: number;
@@ -23,6 +25,9 @@ export const generateMockProduct = (): Product => {
     return {
         id: uuid(),
         name: "Product Name",
+        description: Array.from({ length: Math.floor(Math.random() * 4) + 1 }).map(() => {
+            return loremIpsum({ count: Math.floor(Math.random() * 3) + 2 });
+        }),
         img: "",
         price: {
             current: Math.random() < 0.5 ? basePrice : Math.floor(basePrice * Math.random()),
