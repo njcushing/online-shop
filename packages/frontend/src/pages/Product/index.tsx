871728811fdf00eade4734ffa6522cbea57cd966
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-09 13:36:46 +0000

    Render ProductHero component in Product page

--- packages/frontend/src/pages/Product/index.tsx
+++ packages/frontend/src/pages/Product/index.tsx
@@ -1,5 +1,10 @@
+import { ProductHero } from "@/features/ProductHero";
 import styles from "./index.module.css";
 
 export function Product() {
-    return <div className={styles["page"]}></div>;
+    return (
+        <div className={styles["page"]}>
+            <ProductHero />
+        </div>
+    );
 }
