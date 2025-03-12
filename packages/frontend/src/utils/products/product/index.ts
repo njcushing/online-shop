2c13e0073cbc1250805e87ab858b3859e6b2d6ef
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-12 23:31:46 +0000

    Add more fields to Product type

--- packages/frontend/src/utils/products/product/index.ts
+++ packages/frontend/src/utils/products/product/index.ts
@@ -6,19 +6,29 @@ export type Product = {
     id: string;
     name: string;
     description: string[];
-    img: string;
+    sku: string;
+    slug: string;
     price: {
         current: number;
         base: number;
     };
+    images: { thumb: string; dynamic: string[] };
     rating: {
         value: number;
         quantity: number;
     };
     stock: number;
     allowance: number;
+    tags: string[];
     releaseDate: string;
 };
+
+export type ProductCollection = {
+    type: string;
+    products: string[];
+};
+
+export const collections: Record<string, ProductCollection> = {};
 
 export const generateMockProduct = (): Product => {
     const basePrice = Math.floor(Math.random() * 16000 + 6000);
@@ -29,17 +39,20 @@ export const generateMockProduct = (): Product => {
         description: Array.from({ length: Math.floor(Math.random() * 4) + 1 }).map(() => {
             return loremIpsum({ count: Math.floor(Math.random() * 3) + 2 });
         }),
-        img: "",
+        sku: uuid(),
+        slug: uuid(),
         price: {
             current: Math.random() < 0.5 ? basePrice : Math.floor(basePrice * Math.random()),
             base: basePrice,
         },
+        images: { thumb: "", dynamic: [] },
         rating: {
             value: Math.random() * 2 + 3,
             quantity: Math.floor(Math.random() * 200 + 50),
         },
         stock: Math.floor(Math.random() * 100),
         allowance: Math.floor(Math.random() * 100) + 1,
+        tags: [],
         releaseDate: dayjs(new Date())
             .subtract(Math.floor(Math.random() * 365), "day")
             .toISOString(),
