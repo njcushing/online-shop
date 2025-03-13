ca99980a2dcd7c860dc4d2f372568c19650572fc
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-13 10:25:08 +0000

    Implement variants into Product type

--- packages/frontend/src/utils/products/product/index.ts
+++ packages/frontend/src/utils/products/product/index.ts
@@ -28,20 +28,15 @@ export type Product = {
     id: string;
     name: string;
     description: string[];
-    sku: string;
     slug: string;
-    price: {
-        current: number;
-        base: number;
-    };
     images: { thumb: string; dynamic: string[] };
     rating: {
         value: number;
         quantity: number;
     };
-    stock: number;
     allowance: number;
     tags: string[];
+    variants: ProductVariant[];
     customisations: { id: string; options: string[] }[];
     releaseDate: string;
 };
@@ -62,20 +57,27 @@ export const generateMockProduct = (): Product => {
         description: Array.from({ length: Math.floor(Math.random() * 4) + 1 }).map(() => {
             return loremIpsum({ count: Math.floor(Math.random() * 3) + 2 });
         }),
-        sku: uuid(),
         slug: uuid(),
-        price: {
-            current: Math.random() < 0.5 ? basePrice : Math.floor(basePrice * Math.random()),
-            base: basePrice,
-        },
         images: { thumb: "", dynamic: [] },
         rating: {
             value: Math.random() * 2 + 3,
             quantity: Math.floor(Math.random() * 200 + 50),
         },
-        stock: Math.floor(Math.random() * 100),
         allowance: Math.floor(Math.random() * 100) + 1,
         tags: [],
+        variants: [
+            {
+                id: uuid(),
+                name: "Product Variant 1",
+                sku: uuid(),
+                price: {
+                    current:
+                        Math.random() < 0.5 ? basePrice : Math.floor(basePrice * Math.random()),
+                    base: basePrice,
+                },
+                stock: Math.floor(Math.random() * 100),
+            },
+        ],
         customisations: [],
         releaseDate: dayjs(new Date())
             .subtract(Math.floor(Math.random() * 365), "day")
