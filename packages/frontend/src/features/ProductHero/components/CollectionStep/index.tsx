014b607884767b4775e62e679156e00cb2659324
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-20 12:20:48 +0000

    Design CollectionStep component

--- packages/frontend/src/features/ProductHero/components/CollectionStep/index.tsx
+++ packages/frontend/src/features/ProductHero/components/CollectionStep/index.tsx
@@ -1,5 +1,50 @@
+import { useMemo } from "react";
+import { useParams, Link } from "react-router-dom";
+import { findCollections } from "@/utils/products/product";
+import { Image } from "@mantine/core";
 import styles from "./index.module.css";
 
-export function CollectionStep() {
-    return <div className={styles["product-hero-step"]}></div>;
+export type TCollectionStep = {
+    collectionData: ReturnType<typeof findCollections>[number];
+};
+
+export function CollectionStep({ collectionData }: TCollectionStep) {
+    const params = useParams();
+    const { productId: URLProductId } = params;
+
+    const { collection, products } = collectionData;
+    const { id, type } = collection;
+
+    const title = useMemo<string>(() => {
+        if (type === "quantity") return "Select a quantity";
+        return "Other products in this collection";
+    }, [type]);
+
+    const items = useMemo(() => {
+        return products.map((product) => {
+            const { id: productId, images, name, shorthands } = product;
+            const { thumb } = images;
+            const shorthand = shorthands.find((entry) => entry.type === type)?.value;
+            const usedName = shorthand || name;
+            return (
+                <Link
+                    to={`/p/${productId}`}
+                    className={styles["product-hero-step-product-link"]}
+                    data-selected={productId === URLProductId}
+                    tabIndex={productId === URLProductId ? -1 : 0}
+                    key={`variant-options-${id}-${usedName}`}
+                >
+                    <Image className={styles["product-thumbnail-image"]} src={thumb || ""} />
+                    {usedName}
+                </Link>
+            );
+        });
+    }, [URLProductId, products, id, type]);
+
+    return (
+        <div className={styles["product-hero-step"]}>
+            <p className={styles["product-hero-step-title"]}>{title}</p>
+            <div className={styles["product-hero-step-options"]}>{items}</div>
+        </div>
+    );
 }
