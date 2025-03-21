cd4d1b6f25c341cdbcd0c7f194bec4848b5690d0
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-21 12:30:01 +0000

    Add 'size' prop to Logo component

--- packages/frontend/src/features/Logo/index.tsx
+++ packages/frontend/src/features/Logo/index.tsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 import styles from "./index.module.css";
 
 export type TLogo = {
+    size?: "s" | "m" | "l";
     onClick?: () => unknown;
 };
 
-export function Logo({ onClick }: TLogo) {
+export function Logo({ size = "m", onClick }: TLogo) {
     return (
-        <Link to="/" onClick={() => onClick && onClick()} className={styles["logo"]}>
+        <Link
+            to="/"
+            onClick={() => onClick && onClick()}
+            className={styles["logo"]}
+            data-size={size}
+        >
             <p>CAFREE</p>
         </Link>
     );
