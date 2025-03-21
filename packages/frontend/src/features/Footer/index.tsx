3d440d5c77a0762d82168b634a2cdb48fbf4f432
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-21 12:32:08 +0000

    Render Logo component in Footer component

--- packages/frontend/src/features/Footer/index.tsx
+++ packages/frontend/src/features/Footer/index.tsx
@@ -1,5 +1,12 @@
+import { Divider } from "@mantine/core";
+import { Logo } from "../Logo";
 import styles from "./index.module.css";
 
 export function Footer() {
-    return <footer className={styles["footer"]}></footer>;
+    return (
+        <footer className={styles["footer"]}>
+            <Logo size="l" />
+            <Divider size="xl" orientation="vertical" color="black" />
+        </footer>
+    );
 }
