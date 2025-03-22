b455a6542da6c58dedf17dec32e8492afcb29858
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-22 11:45:52 +0000

    Change field names in CartItemData type to something more suitable

--- packages/frontend/src/utils/products/cart/index.ts
+++ packages/frontend/src/utils/products/cart/index.ts
@@ -1,15 +1,15 @@
 import { Product } from "@/utils/products/product";
 
 export type CartItemData = {
-    id: Product["id"];
-    variant: Product["variants"][number]["id"];
+    productId: Product["id"];
+    variantId: Product["variants"][number]["id"];
     quantity: number;
 };
 
 export const mockCart: CartItemData[] = [
-    { id: "1", variant: "1-1", quantity: 10 },
-    { id: "1", variant: "1-2", quantity: 5 },
-    { id: "2", variant: "2-2", quantity: 15 },
-    { id: "3", variant: "2-1", quantity: 6 },
-    { id: "3", variant: "2-3", quantity: 18 },
+    { productId: "1", variantId: "1-1", quantity: 10 },
+    { productId: "1", variantId: "1-2", quantity: 5 },
+    { productId: "2", variantId: "2-2", quantity: 15 },
+    { productId: "3", variantId: "2-1", quantity: 6 },
+    { productId: "3", variantId: "2-3", quantity: 18 },
 ];
