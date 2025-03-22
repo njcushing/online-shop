a952456848af370b7d4f49479a70be59f8c629d4
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-22 11:35:39 +0000

    Create mock cart data

--- packages/frontend/src/utils/products/cart/index.ts
+++ packages/frontend/src/utils/products/cart/index.ts
@@ -5,3 +5,11 @@ export type CartItemData = {
     variant: Product["variants"][number]["id"];
     quantity: number;
 };
+
+export const mockCart: CartItemData[] = [
+    { id: "1", variant: "1-1", quantity: 10 },
+    { id: "1", variant: "1-2", quantity: 5 },
+    { id: "2", variant: "2-2", quantity: 15 },
+    { id: "3", variant: "2-1", quantity: 6 },
+    { id: "3", variant: "2-3", quantity: 18 },
+];
