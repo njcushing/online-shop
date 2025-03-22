89e136813dc9e85fc92a4edb6df513c22a778f97
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-22 12:52:04 +0000

    Design CartItem component

--- packages/frontend/src/features/Cart/components/CartItem/index.tsx
+++ packages/frontend/src/features/Cart/components/CartItem/index.tsx
@@ -1,5 +1,102 @@
+import { useMemo } from "react";
+import { Image } from "@mantine/core";
+import { Quantity } from "@/components/Inputs/Quantity";
+import {
+    ProductVariant,
+    Product,
+    variantOptions,
+    findProductFromId,
+    findVariantFromId,
+} from "@/utils/products/product";
+import { CartItemData } from "@/utils/products/cart";
+import { createPriceAdjustmentString } from "@/utils/createPriceAdjustmentString";
 import styles from "./index.module.css";
 
-export function CartItem() {
-    return <li className={styles["cart-item"]}></li>;
+export type TCartItem = {
+    itemData: CartItemData;
+};
+
+const calculateMaximumAvailability = (
+    stock: ProductVariant["stock"],
+    allowance: Product["allowance"],
+    allowanceOverride: ProductVariant["allowanceOverride"],
+): number => {
+    if (allowanceOverride) return Math.min(stock, allowanceOverride);
+    return Math.min(stock, allowance);
+};
+
+export function CartItem({ itemData }: TCartItem) {
+    const { productId, variantId, quantity } = itemData;
+
+    const productData = useMemo<ReturnType<typeof findProductFromId>>(() => {
+        return findProductFromId(productId || "");
+    }, [productId]);
+
+    const variantData = useMemo<ReturnType<typeof findVariantFromId>>(() => {
+        return productData ? findVariantFromId(variantId) : null;
+    }, [variantId, productData]);
+
+    if (!productData || !variantData) return null;
+
+    const { name, images, allowance } = productData;
+    const { price, stock, options, allowanceOverride, image } = variantData;
+
+    return (
+        <div className={styles["cart-item"]}>
+            <Image className={styles["cart-item-thumbnail-image"]} src={image || images.thumb} />
+
+            <div className={styles["cart-item-content"]}>
+                <p className={styles["cart-item-name"]}>{name.full}</p>
+
+                <div className={styles["cart-item-content-middle"]}>
+                    {Object.entries(options).map((option) => {
+                        const [key, value] = option;
+                        const variantOption = variantOptions.find((vOpt) => vOpt.id === key);
+                        const variantOptionValue = variantOption?.values.find(
+                            (vOptVal) => vOptVal.id === value,
+                        );
+                        return (
+                            <div className={styles["cart-item-variant-option-info"]} key={key}>
+                                <p className={styles["cart-item-variant-option-name"]}>
+                                    {variantOption?.name || key}:{" "}
+                                </p>
+                                <p className={styles["cart-item-variant-option-value"]}>
+                                    {variantOptionValue?.name || value}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+
+                <div className={styles["cart-item-content-bottom"]}>
+                    <Quantity
+                        defaultValue={quantity}
+                        min={1}
+                        max={calculateMaximumAvailability(stock, allowance, allowanceOverride)}
+                        size="s"
+                    />
+
+                    <div className={styles["cart-item-price-container"]}>
+                        <span className={styles["cart-item-price-current"]}>
+                            £{((price.current * quantity) / 100).toFixed(2)}
+                        </span>
+
+                        {price.current !== price.base && (
+                            <>
+                                <span className={styles["cart-item-price-base"]}>
+                                    £{((price.base * quantity) / 100).toFixed(2)}
+                                </span>
+                                <span className={styles["cart-item-price-discount-percentage"]}>
+                                    {createPriceAdjustmentString(
+                                        price.current * quantity,
+                                        price.base * quantity,
+                                    )}
+                                </span>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
