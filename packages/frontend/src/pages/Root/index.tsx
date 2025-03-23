ff0b851fe5bcee8e44768ab4f8f9b86b5d170391
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-23 12:45:41 +0000

    Provide populated cart data in user context from Root page component
    I'm using the mock function as the API is not yet set up.

--- packages/frontend/src/pages/Root/index.tsx
+++ packages/frontend/src/pages/Root/index.tsx
@@ -1,6 +1,9 @@
+import { createContext, useState, useEffect, useRef, useMemo } from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "@/features/Header";
 import { Footer } from "@/features/Footer";
+import { PopulatedCartItemData } from "@/utils/products/cart";
+import { mockGetPopulatedCartItemData } from "@/api/cart";
 import { Home } from "../Home";
 import { Category } from "../Category";
 import { Product } from "../Product";
@@ -25,12 +28,62 @@ export const Routes = [
     },
 ];
 
+export interface IUserContext {
+    cart: { data: PopulatedCartItemData[]; awaiting: boolean; status: number; message: string };
+}
+
+const defaultUserContext: IUserContext = {
+    cart: { data: [], awaiting: false, status: 200, message: "Success" },
+};
+
+export const UserContext = createContext<IUserContext>(defaultUserContext);
+
 export function Root() {
+    const [cart, setCart] = useState<IUserContext["cart"]>(defaultUserContext.cart);
+
+    const getPopulatedCartDataTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+    useEffect(() => {
+        const fetchCart = async () => {
+            // Simulate API request delay
+            await new Promise((resolve) => {
+                getPopulatedCartDataTimeoutRef.current = setTimeout(resolve, 1000);
+            });
+
+            getPopulatedCartDataTimeoutRef.current = null;
+
+            const response = {
+                data: mockGetPopulatedCartItemData(),
+                awaiting: false,
+                status: 200,
+                message: "Success",
+            };
+
+            setCart(response);
+        };
+
+        fetchCart();
+
+        return () => {
+            if (getPopulatedCartDataTimeoutRef.current) {
+                clearTimeout(getPopulatedCartDataTimeoutRef.current);
+            }
+        };
+    }, []);
+
     return (
-        <div className={styles["page"]}>
-            <Header />
-            <Outlet />
-            <Footer />
-        </div>
+        <UserContext.Provider
+            value={useMemo(
+                () => ({
+                    cart,
+                }),
+                [cart],
+            )}
+        >
+            <div className={styles["page"]}>
+                <Header />
+                <Outlet />
+                <Footer />
+            </div>
+        </UserContext.Provider>
     );
 }
