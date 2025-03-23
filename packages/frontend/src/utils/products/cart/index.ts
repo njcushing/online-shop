5e41a5da5c91e057a565d10f9b6c4f227abb04ac
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-23 11:08:13 +0000

    Fix incorrect variantId for items in mockCart

--- packages/frontend/src/utils/products/cart/index.ts
+++ packages/frontend/src/utils/products/cart/index.ts
@@ -10,6 +10,6 @@ export const mockCart: CartItemData[] = [
     { productId: "1", variantId: "1-1", quantity: 10 },
     { productId: "1", variantId: "1-2", quantity: 5 },
     { productId: "2", variantId: "2-2", quantity: 15 },
-    { productId: "3", variantId: "2-1", quantity: 6 },
-    { productId: "3", variantId: "2-3", quantity: 18 },
+    { productId: "3", variantId: "3-1", quantity: 6 },
+    { productId: "3", variantId: "3-3", quantity: 18 },
 ];
