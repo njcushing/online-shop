d2ff0a1e42f4c3db43b76b514654e76f87ccfd66
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-26 17:44:52 +0000

    Set up Product page component to provide context to child components

--- packages/frontend/src/pages/Product/index.tsx
+++ packages/frontend/src/pages/Product/index.tsx
@@ -1,12 +1,30 @@
+import { createContext, useMemo, useState } from "react";
 import { ProductHero } from "@/features/ProductHero";
 import { ProductInformation } from "@/features/ProductInformation";
+import { Product as ProductDataType } from "@/utils/products/product";
 import styles from "./index.module.css";
 
+export interface IProductContext {
+    product: { data: ProductDataType | null; awaiting: boolean; status: number; message: string };
+}
+
+const defaultProductContext: IProductContext = {
+    product: { data: null, awaiting: false, status: 200, message: "Success" },
+};
+
+export const ProductContext = createContext<IProductContext>(defaultProductContext);
+
 export function Product() {
+    const [product, setProduct] = useState<IProductContext["product"]>(
+        defaultProductContext.product,
+    );
+
     return (
-        <div className={styles["page"]}>
-            <ProductHero />
-            <ProductInformation />
-        </div>
+        <ProductContext.Provider value={useMemo(() => ({ product }), [product])}>
+            <div className={styles["page"]}>
+                <ProductHero />
+                <ProductInformation />
+            </div>
+        </ProductContext.Provider>
     );
 }
