4e880137f1705fc2981fcb559966a35da09dadaf
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-26 17:59:31 +0000

    Set up mock API request for product data

--- packages/frontend/src/pages/Product/index.tsx
+++ packages/frontend/src/pages/Product/index.tsx
@@ -1,7 +1,9 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useState, useEffect, useRef, useMemo } from "react";
+import { useParams } from "react-router-dom";
 import { ProductHero } from "@/features/ProductHero";
 import { ProductInformation } from "@/features/ProductInformation";
 import { Product as ProductDataType } from "@/utils/products/product";
+import { mockGetProductDataFromSlug } from "@/api/product";
 import styles from "./index.module.css";
 
 export interface IProductContext {
@@ -15,9 +17,49 @@ const defaultProductContext: IProductContext = {
 export const ProductContext = createContext<IProductContext>(defaultProductContext);
 
 export function Product() {
+    const params = useParams();
+    const { productSlug } = params;
+
     const [product, setProduct] = useState<IProductContext["product"]>(
         defaultProductContext.product,
     );
+
+    const getProductDataTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+    useEffect(() => {
+        const fetchCart = async (slug: string) => {
+            // Simulate API request delay
+            await new Promise((resolve) => {
+                getProductDataTimeoutRef.current = setTimeout(resolve, 1000);
+            });
+
+            getProductDataTimeoutRef.current = null;
+
+            const response = {
+                data: mockGetProductDataFromSlug(slug),
+                awaiting: false,
+                status: 200,
+                message: "Success",
+            };
+
+            setProduct(response);
+        };
+
+        if (productSlug) {
+            fetchCart(productSlug);
+        } else {
+            if (getProductDataTimeoutRef.current) {
+                clearTimeout(getProductDataTimeoutRef.current);
+                getProductDataTimeoutRef.current = null;
+            }
+            setProduct(defaultProductContext.product);
+        }
+
+        return () => {
+            if (getProductDataTimeoutRef.current) {
+                clearTimeout(getProductDataTimeoutRef.current);
+            }
+        };
+    }, [productSlug]);
 
     return (
         <ProductContext.Provider value={useMemo(() => ({ product }), [product])}>
