10737a5824f80fe682615bf6bdefed90b0a89a08
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-26 17:37:37 +0000

    Render ProductInformation component on Product page

--- packages/frontend/src/pages/Product/index.tsx
+++ packages/frontend/src/pages/Product/index.tsx
@@ -1,10 +1,12 @@
 import { ProductHero } from "@/features/ProductHero";
+import { ProductInformation } from "@/features/ProductInformation";
 import styles from "./index.module.css";
 
 export function Product() {
     return (
         <div className={styles["page"]}>
             <ProductHero />
+            <ProductInformation />
         </div>
     );
 }
