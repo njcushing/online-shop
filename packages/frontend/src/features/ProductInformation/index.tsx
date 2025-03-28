cbff59bbb449637f039b412491380054c98ff1f3
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-28 11:12:36 +0000

    Set max-width on ProductInformation component

--- packages/frontend/src/features/ProductInformation/index.tsx
+++ packages/frontend/src/features/ProductInformation/index.tsx
@@ -23,21 +23,25 @@ const segments = [
 export function ProductInformation() {
     return (
         <section className={styles["product-information"]}>
-            <Accordion defaultValue="Description">
-                {segments.map((segment) => {
-                    const { value, text } = segment;
-                    return (
-                        <Accordion.Item key={value} value={value}>
-                            <Accordion.Control classNames={{ label: styles["accordion-label"] }}>
-                                {value}
-                            </Accordion.Control>
-                            <Accordion.Panel className={styles["accordion-panel"]}>
-                                {text}
-                            </Accordion.Panel>
-                        </Accordion.Item>
-                    );
-                })}
-            </Accordion>
+            <div className={styles["product-information-width-controller"]}>
+                <Accordion defaultValue="Description">
+                    {segments.map((segment) => {
+                        const { value, text } = segment;
+                        return (
+                            <Accordion.Item key={value} value={value}>
+                                <Accordion.Control
+                                    classNames={{ label: styles["accordion-label"] }}
+                                >
+                                    {value}
+                                </Accordion.Control>
+                                <Accordion.Panel className={styles["accordion-panel"]}>
+                                    {text}
+                                </Accordion.Panel>
+                            </Accordion.Item>
+                        );
+                    })}
+                </Accordion>
+            </div>
         </section>
     );
 }
