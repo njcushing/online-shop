665e6d56a6c9e05998af912709f4ea7649c09d95
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-29 10:27:04 +0000

    Add delivery information to ProductInformation component

--- packages/frontend/src/features/ProductInformation/index.tsx
+++ packages/frontend/src/features/ProductInformation/index.tsx
@@ -1,4 +1,5 @@
 import { Accordion } from "@mantine/core";
+import { freeDeliveryThreshold, expressDeliveryCost } from "@/utils/products/cart";
 import styles from "./index.module.css";
 
 const segments = [
@@ -12,7 +13,37 @@ const segments = [
     },
     {
         value: "Delivery Information",
-        text: "",
+        text: (
+            <div className={styles["delivery-information"]}>
+                <div className={styles["delivery-information-segment"]}>
+                    <p className={styles["delivery-information-segment-title"]}>
+                        Standard Delivery
+                    </p>
+                    <div className={styles["delivery-information-segment-information"]}>
+                        <span>
+                            Free delivery on all orders of at least £
+                            <strong>
+                                {+parseFloat(`${freeDeliveryThreshold / 100}`).toFixed(2)}.
+                            </strong>
+                        </span>
+                        <span>
+                            We aim to ship all orders within 48h of time of purchase; this is
+                            guaranteed if the order is placed before 5pm.
+                        </span>
+                    </div>
+                </div>
+                <div className={styles["delivery-information-segment"]}>
+                    <p className={styles["delivery-information-segment-title"]}>
+                        Express Delivery (£{+parseFloat(`${expressDeliveryCost / 100}`).toFixed(2)})
+                    </p>
+                    <div className={styles["delivery-information-segment-information"]}>
+                        <span>
+                            Guaranteed next-day delivery on all orders if placed before 5pm.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        ),
     },
     {
         value: "Customer Reviews",
@@ -30,6 +61,7 @@ export function ProductInformation() {
                         root: styles["accordion-root"],
                         item: styles["accordion-item"],
                         control: styles["accordion-control"],
+                        content: styles["accordion-content"],
                     }}
                 >
                     {segments.map((segment) => {
