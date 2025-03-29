e6009f7d1fccb91fdcbc9c6be77d0c342d40b2c4
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-29 11:22:49 +0000

    Display actual product variant details in table

--- packages/frontend/src/features/ProductInformation/index.tsx
+++ packages/frontend/src/features/ProductInformation/index.tsx
@@ -1,94 +1,106 @@
+import { useContext, useMemo } from "react";
+import { ProductContext } from "@/pages/Product";
 import { Accordion, Table } from "@mantine/core";
+import dayjs from "dayjs";
 import { freeDeliveryThreshold, expressDeliveryCost } from "@/utils/products/cart";
 import styles from "./index.module.css";
 
-const segments = [
-    {
-        value: "Description",
-        content: "",
-    },
-    {
-        value: "Product Details",
-        content: (
-            <Table
-                variant="vertical"
-                layout="fixed"
-                withTableBorder
-                classNames={{
-                    table: styles["details-table"],
-                    th: styles["details-table-th"],
-                }}
-            >
-                <Table.Tbody>
-                    <Table.Tr>
-                        <Table.Th>Example Row 1</Table.Th>
-                        <Table.Td>Example Row 1 Content</Table.Td>
-                    </Table.Tr>
-
-                    <Table.Tr>
-                        <Table.Th>Example Row 2</Table.Th>
-                        <Table.Td>Example Row 2 Content</Table.Td>
-                    </Table.Tr>
-
-                    <Table.Tr>
-                        <Table.Th>Example Row 3</Table.Th>
-                        <Table.Td>Example Row 3 Content</Table.Td>
-                    </Table.Tr>
-
-                    <Table.Tr>
-                        <Table.Th>Example Row 4</Table.Th>
-                        <Table.Td>Example Row 4 Content</Table.Td>
-                    </Table.Tr>
-
-                    <Table.Tr>
-                        <Table.Th>Example Row 5</Table.Th>
-                        <Table.Td>Example Row 5 Content</Table.Td>
-                    </Table.Tr>
-                </Table.Tbody>
-            </Table>
-        ),
-    },
-    {
-        value: "Delivery Information",
-        content: (
-            <div className={styles["delivery-information"]}>
-                <div className={styles["delivery-information-segment"]}>
-                    <p className={styles["delivery-information-segment-title"]}>
-                        Standard Delivery
-                    </p>
-                    <div className={styles["delivery-information-segment-information"]}>
-                        <span>
-                            Free delivery on all orders of at least £
-                            <strong>
-                                {+parseFloat(`${freeDeliveryThreshold / 100}`).toFixed(2)}.
-                            </strong>
-                        </span>
-                        <span>
-                            We aim to ship all orders within 48h of time of purchase; this is
-                            guaranteed if the order is placed before 5pm.
-                        </span>
-                    </div>
-                </div>
-                <div className={styles["delivery-information-segment"]}>
-                    <p className={styles["delivery-information-segment-title"]}>
-                        Express Delivery (£{+parseFloat(`${expressDeliveryCost / 100}`).toFixed(2)})
-                    </p>
-                    <div className={styles["delivery-information-segment-information"]}>
-                        <span>
-                            Guaranteed next-day delivery on all orders if placed before 5pm.
-                        </span>
-                    </div>
-                </div>
-            </div>
-        ),
-    },
-    {
-        value: "Customer Reviews",
-        content: "",
-    },
-];
-
 export function ProductInformation() {
+    const { variant } = useContext(ProductContext);
+    const { sku, details, releaseDate } = variant || {
+        sku: "null",
+        details: [],
+        releaseDate: null,
+    };
+
+    const segments = useMemo(() => {
+        return [
+            {
+                value: "Description",
+                content: "",
+            },
+            {
+                value: "Product Details",
+                content: (
+                    <Table
+                        variant="vertical"
+                        layout="fixed"
+                        withTableBorder
+                        classNames={{
+                            table: styles["details-table"],
+                            th: styles["details-table-th"],
+                        }}
+                    >
+                        <Table.Tbody>
+                            <Table.Tr>
+                                <Table.Th>SKU</Table.Th>
+                                <Table.Td>{sku}</Table.Td>
+                            </Table.Tr>
+
+                            {details.map((detail) => {
+                                const { name, value } = detail;
+                                return (
+                                    <Table.Tr key={name}>
+                                        <Table.Th>{name}</Table.Th>
+                                        <Table.Td>{value}</Table.Td>
+                                    </Table.Tr>
+                                );
+                            })}
+
+                            <Table.Tr>
+                                <Table.Th>Release Date</Table.Th>
+                                <Table.Td>
+                                    {releaseDate
+                                        ? dayjs(releaseDate).format("MMMM D, YYYY")
+                                        : "null"}
+                                </Table.Td>
+                            </Table.Tr>
+                        </Table.Tbody>
+                    </Table>
+                ),
+            },
+            {
+                value: "Delivery Information",
+                content: (
+                    <div className={styles["delivery-information"]}>
+                        <div className={styles["delivery-information-segment"]}>
+                            <p className={styles["delivery-information-segment-title"]}>
+                                Standard Delivery
+                            </p>
+                            <div className={styles["delivery-information-segment-information"]}>
+                                <span>
+                                    Free delivery on all orders of at least £
+                                    <strong>
+                                        {+parseFloat(`${freeDeliveryThreshold / 100}`).toFixed(2)}.
+                                    </strong>
+                                </span>
+                                <span>
+                                    We aim to ship all orders within 48h of time of purchase; this
+                                    is guaranteed if the order is placed before 5pm.
+                                </span>
+                            </div>
+                        </div>
+                        <div className={styles["delivery-information-segment"]}>
+                            <p className={styles["delivery-information-segment-title"]}>
+                                Express Delivery (£
+                                {+parseFloat(`${expressDeliveryCost / 100}`).toFixed(2)})
+                            </p>
+                            <div className={styles["delivery-information-segment-information"]}>
+                                <span>
+                                    Guaranteed next-day delivery on all orders if placed before 5pm.
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                ),
+            },
+            {
+                value: "Customer Reviews",
+                content: "",
+            },
+        ];
+    }, [sku, details, releaseDate]);
+
     return (
         <section className={styles["product-information"]}>
             <div className={styles["product-information-width-controller"]}>
