834f18bdc5fb1afc0f3307021ae0d7297d1dc587
njcushing <129661299+njcushing@users.noreply.github.com>
2025-03-31 21:52:31 +0100

    Create SearchBar component

---
+++ packages/frontend/src/features/Header/components/SearchBar/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Input, CloseButton } from "@mantine/core";
+import styles from "./index.module.css";
+
+export type TSearchBar = {
+    opened?: boolean;
+    onClose?: () => unknown;
+};
+
+export function SearchBar({ opened = false, onClose }: TSearchBar) {
+    const [value, setValue] = useState<string>("");
+
+    return opened ? (
+        <Input
+            placeholder="Search for a product"
+            value={value}
+            onChange={(event) => setValue(event.currentTarget.value)}
+            rightSectionPointerEvents="all"
+            mt="md"
+            rightSection={
+                <CloseButton
+                    aria-label="Clear input"
+                    onClick={() => onClose && onClose()}
+                    style={{ display: value ? undefined : "none" }}
+                />
+            }
+            classNames={{ wrapper: styles["input-wrapper"], input: styles["input"] }}
+        />
+    ) : null;
+}
