b0ca03c97d9d8883135e97ef26eb09de5da62a09
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-02 23:14:16 +0100

    Allow ref forwarding to SearchBar component

--- packages/frontend/src/features/Header/components/SearchBar/index.tsx
+++ packages/frontend/src/features/Header/components/SearchBar/index.tsx
@@ -1,5 +1,6 @@
-import { useRef, useState } from "react";
+import { forwardRef, useRef, useState } from "react";
 import { Input, CloseButton, Collapse } from "@mantine/core";
+import { mergeRefs } from "@/utils/mergeRefs";
 import styles from "./index.module.css";
 
 export type TSearchBar = {
@@ -7,38 +8,42 @@ export type TSearchBar = {
     onClose?: () => unknown;
 };
 
-export function SearchBar({ opened = false, onClose }: TSearchBar) {
-    const [value, setValue] = useState<string>("");
-    const inputRef = useRef<HTMLInputElement>(null);
+export const SearchBar = forwardRef<HTMLInputElement, TSearchBar>(
+    ({ opened = false, onClose }: TSearchBar, ref) => {
+        const [value, setValue] = useState<string>("");
+        const inputRef = useRef<HTMLInputElement>(null);
 
-    return (
-        <Collapse
-            in={opened}
-            animateOpacity={false}
-            onTransitionEnd={() => {
-                const { current } = inputRef;
-                if (!current) return;
-                if (opened) current.focus();
-                else current.blur();
-            }}
-            className={styles["search-bar"]}
-        >
-            <Input
-                placeholder="Search for a product"
-                value={value}
-                onChange={(event) => setValue(event.currentTarget.value)}
-                rightSectionPointerEvents="all"
-                mt="md"
-                rightSection={
-                    <CloseButton
-                        aria-label="Clear input"
-                        onClick={() => setValue("")}
-                        style={{ display: value ? undefined : "none" }}
-                    />
-                }
-                classNames={{ wrapper: styles["input-wrapper"], input: styles["input"] }}
-                ref={inputRef}
-            />
-        </Collapse>
-    );
-}
+        return (
+            <Collapse
+                in={opened}
+                animateOpacity={false}
+                onTransitionEnd={() => {
+                    const { current } = inputRef;
+                    if (!current) return;
+                    if (opened) current.focus();
+                    else current.blur();
+                }}
+                className={styles["search-bar"]}
+            >
+                <Input
+                    placeholder="Search for a product"
+                    value={value}
+                    onChange={(event) => setValue(event.currentTarget.value)}
+                    rightSectionPointerEvents="all"
+                    mt="md"
+                    rightSection={
+                        <CloseButton
+                            aria-label="Clear input"
+                            onClick={() => setValue("")}
+                            style={{ display: value ? undefined : "none" }}
+                        />
+                    }
+                    classNames={{ wrapper: styles["input-wrapper"], input: styles["input"] }}
+                    ref={mergeRefs(ref, inputRef)}
+                />
+            </Collapse>
+        );
+    },
+);
+
+SearchBar.displayName = "HeaderSearchBar";
