b60502cba1a2576cdbb1c9c2c33433ee03ec9241
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-03 00:01:19 +0100

    Add copyright message to Footer component

--- packages/frontend/src/features/Footer/index.tsx
+++ packages/frontend/src/features/Footer/index.tsx
@@ -6,8 +6,11 @@ export function Footer() {
     return (
         <footer className={styles["footer"]}>
             <div className={styles["footer-width-controller"]}>
-                <Logo size="l" />
-                <Divider size="lg" orientation="vertical" color="black" />
+                <div className={styles["column-1"]}>
+                    <Logo size="l" />
+                    <p className={styles["copyright-message"]}>© njcushing 2025</p>
+                </div>
+                <Divider size="sm" orientation="vertical" color="black" />
             </div>
         </footer>
     );
