887b0bd69bf2d5cfab76bb931e84fbaf65d7e26c
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-03 09:54:19 +0100

    Add sections and placeholder links within Footer component

--- packages/frontend/src/features/Footer/index.tsx
+++ packages/frontend/src/features/Footer/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Divider } from "@mantine/core";
 import { Logo } from "../Logo";
 import styles from "./index.module.css";
@@ -11,6 +12,56 @@ export function Footer() {
                     <p className={styles["copyright-message"]}>© njcushing 2025</p>
                 </div>
                 <Divider size="sm" orientation="vertical" color="black" />
+                <div className={styles["sections-container"]}>
+                    <div className={styles["section"]}>
+                        <h3 className={styles["section-heading"]}>Legal</h3>
+                        <Link to="/" className={styles["link"]}>
+                            Terms & Conditions
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            Privacy Policy
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            Rewards
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            Delivery
+                        </Link>
+                    </div>
+                    <div className={styles["section"]}>
+                        <h3 className={styles["section-heading"]}>Help</h3>
+                        <Link to="/" className={styles["link"]}>
+                            FAQs
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            Email
+                        </Link>
+                    </div>
+                    <div className={styles["section"]}>
+                        <h3 className={styles["section-heading"]}>Contact</h3>
+                        <Link to="/" className={styles["link"]}>
+                            Email
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            Address
+                        </Link>
+                    </div>
+                    <div className={styles["section"]}>
+                        <h3 className={styles["section-heading"]}>Socials</h3>
+                        <Link to="/" className={styles["link"]}>
+                            Instagram
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            X
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            Facebook
+                        </Link>
+                        <Link to="/" className={styles["link"]}>
+                            GitHub
+                        </Link>
+                    </div>
+                </div>
             </div>
         </footer>
     );
