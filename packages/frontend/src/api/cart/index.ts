fda61d6b3983b26c75c3326a7b2bccbf36184a38
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-18 15:15:16 +0100

    Write 'addProductToCart' API request function

--- packages/frontend/src/api/cart/index.ts
+++ packages/frontend/src/api/cart/index.ts
@@ -1,12 +1,12 @@
-import { products } from "@/utils/products/product";
-import { PopulatedCartItemData, mockCart } from "@/utils/products/cart";
+import { products as productData } from "@/utils/products/product";
+import { CartItemData, PopulatedCartItemData, mockCart } from "@/utils/products/cart";
 import * as HTTPMethodTypes from "../types";
 import { saveTokenFromAPIResponse } from "../utils/saveTokenFromAPIResponse";
 
 export const getPopulatedCartItemData: HTTPMethodTypes.GET<
     undefined,
     { cartData: PopulatedCartItemData[] }
-> = async (data, abortController = null) => {
+> = async (data, abortController) => {
     const token = localStorage.getItem(import.meta.env.VITE_TOKEN_LOCAL_LOCATION);
     if (!token) return { status: 400, message: "No token provided for query", data: null };
 
@@ -24,7 +24,7 @@ export const getPopulatedCartItemData: HTTPMethodTypes.GET<
             return {
                 status: responseJSON.status,
                 message: responseJSON.message,
-                data: responseJSON.data ? responseJSON.data._id : null,
+                data: responseJSON.data,
             };
         })
         .catch((error) => {
@@ -40,10 +40,50 @@ export const getPopulatedCartItemData: HTTPMethodTypes.GET<
 export const mockGetPopulatedCartItemData = (): PopulatedCartItemData[] => {
     return mockCart.flatMap((cartItem) => {
         const { productId, variantId, quantity } = cartItem;
-        const matchedProduct = products.find((product) => product.id === productId);
+        const matchedProduct = productData.find((product) => product.id === productId);
         if (!matchedProduct) return [];
         const matchedVariant = matchedProduct.variants.find((variant) => variant.id === variantId);
         if (!matchedVariant) return [];
         return { product: matchedProduct, variant: matchedVariant, quantity };
     });
+};
+
+export const addProductToCart: HTTPMethodTypes.PUT<
+    undefined,
+    { products: CartItemData[] },
+    { cartData: PopulatedCartItemData[] }
+> = async (data, abortController) => {
+    const token = localStorage.getItem(import.meta.env.VITE_TOKEN_LOCAL_LOCATION);
+    if (!token) return { status: 400, message: "No token provided for query", data: null };
+
+    const { products } = data.body || { products: [] };
+    if (products.length === 0) {
+        return { status: 400, message: "No products provided for query", data: null };
+    }
+
+    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/api/cart`, {
+        signal: abortController ? abortController.signal : null,
+        method: "PUT",
+        mode: "cors",
+        headers: { Authorization: token },
+        body: JSON.stringify({ products, populated: true }),
+    })
+        .then(async (response) => {
+            const responseJSON = await response.json();
+            saveTokenFromAPIResponse(responseJSON);
+
+            return {
+                status: responseJSON.status,
+                message: responseJSON.message,
+                data: responseJSON.data,
+            };
+        })
+        .catch((error) => {
+            return {
+                status: error.status ? error.status : 500,
+                message: error.message,
+                data: null,
+            };
+        });
+    return result;
 };
