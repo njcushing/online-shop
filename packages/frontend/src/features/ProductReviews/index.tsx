be1c86151c705ff404f77b5ad6e692500b9ef5ba
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-19 16:18:00 +0100

    Add rating stars and value and review quantity to overview

--- packages/frontend/src/features/ProductReviews/index.tsx
+++ packages/frontend/src/features/ProductReviews/index.tsx
@@ -1,5 +1,33 @@
+import { useContext } from "react";
+import { ProductContext } from "@/pages/Product";
+import { Rating } from "@mantine/core";
 import styles from "./index.module.css";
 
 export function ProductReviews() {
-    return <div className={styles["product-reviews"]}></div>;
+    const { product } = useContext(ProductContext);
+    const { data: productData, awaiting } = product;
+
+    if (awaiting || !productData) return null;
+
+    const { rating } = productData;
+
+    return (
+        <div className={styles["product-reviews"]}>
+            <div className={styles["overview"]}>
+                <Rating
+                    className={styles["product-rating"]}
+                    readOnly
+                    count={5}
+                    fractions={10}
+                    value={rating.value}
+                    color="gold"
+                    size="lg"
+                />
+                <div className={styles["product-rating-description"]}>
+                    <strong>{rating.value.toFixed(2)}</strong> out of <strong>5</strong> from{" "}
+                    <strong>{rating.quantity}</strong> reviews
+                </div>
+            </div>
+        </div>
+    );
 }
