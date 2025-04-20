459accc74847f329e2674f15c249dd7f72a8ed58
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-20 15:55:14 +0100

    Display review comment in Review component

--- packages/frontend/src/features/ProductReviews/components/Review/index.tsx
+++ packages/frontend/src/features/ProductReviews/components/Review/index.tsx
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { ProductReview } from "@/utils/products/product";
 import styles from "./index.module.css";
 
@@ -6,5 +8,15 @@ export type TReview = {
 };
 
 export function Review({ data }: TReview) {
-    return <div className={styles["review"]}></div>;
+    const { comment } = data;
+
+    return (
+        <div className={styles["review"]}>
+            <div className={styles["comment"]}>
+                <div className={styles["markdown-container"]}>
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>{comment}</ReactMarkdown>
+                </div>
+            </div>
+        </div>
+    );
 }
