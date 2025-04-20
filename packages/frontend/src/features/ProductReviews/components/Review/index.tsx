ac3443fc2c04af93aafae5fc1335dd455e4931bd
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-20 14:53:29 +0100

    Add 'data' prop to Review component

--- packages/frontend/src/features/ProductReviews/components/Review/index.tsx
+++ packages/frontend/src/features/ProductReviews/components/Review/index.tsx
@@ -1,5 +1,10 @@
+import { ProductReview } from "@/utils/products/product";
 import styles from "./index.module.css";
 
-export function Review() {
+export type TReview = {
+    data: ProductReview;
+};
+
+export function Review({ data }: TReview) {
     return <div className={styles["review"]}></div>;
 }
