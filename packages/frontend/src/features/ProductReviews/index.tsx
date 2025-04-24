d8fdc93d1f5c61d32c450be05490ce1750b47dac
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-24 12:05:24 +0100

    Add select elements for review filter and sort options

--- packages/frontend/src/features/ProductReviews/index.tsx
+++ packages/frontend/src/features/ProductReviews/index.tsx
@@ -7,6 +7,9 @@ import { v4 as uuid } from "uuid";
 import { Review } from "./components/Review";
 import styles from "./index.module.css";
 
+const filterOptions = ["All", "5", "4", "3", "2", "1"] as const;
+const sortOptions = ["Most Recent", "Highest Rating", "Lowest Rating"] as const;
+
 const reviewsPerPage = 10;
 
 export function ProductReviews() {
@@ -17,6 +20,8 @@ export function ProductReviews() {
     const { data: productData, awaiting } = product;
 
     const [page, setPage] = useState<number>(0);
+    const [filter, setFilter] = useState<(typeof filterOptions)[number]>("All");
+    const [sort, setSort] = useState<(typeof sortOptions)[number]>("Most Recent");
 
     const forceCloseId = useRef<string>(uuid());
     const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>({
@@ -41,56 +46,119 @@ export function ProductReviews() {
     return (
         <div className={styles["product-reviews"]}>
             <div
-                className={`${styles["overview"]} ${styles[headerInfo.open ? "shifted" : ""]}`}
+                className={`${styles["sticky-panel"]} ${styles[headerInfo.open ? "shifted" : ""]}`}
                 style={{
                     top: !headerInfo.open
                         ? "16px"
                         : `calc(max(${16}px, ${headerInfo.height + 16}px))`,
                 }}
             >
-                <div className={styles["product-reviews-rating-container"]}>
-                    <Rating
-                        classNames={{ starSymbol: styles["rating-star-symbol"] }}
-                        readOnly
-                        count={5}
-                        fractions={10}
-                        value={rating.meanValue}
-                        color="gold"
-                        size="lg"
-                    />
-                    <div className={styles["product-rating-description"]}>
-                        <strong>{rating.meanValue.toFixed(2)}</strong> out of <strong>5</strong>{" "}
-                        from <strong>{reviewIds.length}</strong> reviews
+                <div className={styles["overview"]}>
+                    <div className={styles["product-reviews-rating-container"]}>
+                        <Rating
+                            classNames={{ starSymbol: styles["rating-star-symbol"] }}
+                            readOnly
+                            count={5}
+                            fractions={10}
+                            value={rating.meanValue}
+                            color="gold"
+                            size="lg"
+                        />
+                        <div className={styles["product-rating-description"]}>
+                            <strong>{rating.meanValue.toFixed(2)}</strong> out of <strong>5</strong>{" "}
+                            from <strong>{reviewIds.length}</strong> reviews
+                        </div>
+                    </div>
+                    <div className={styles["product-reviews-rating-bars"]}>
+                        {Object.entries(rating.quantities)
+                            .reverse()
+                            .map((entry) => {
+                                const [key, value] = entry;
+
+                                return (
+                                    <div
+                                        className={styles["product-reviews-rating-bar"]}
+                                        key={`product-reviews-tier-${key}-progress-bar`}
+                                    >
+                                        <p className={styles["product-reviews-rating-tier-key"]}>
+                                            {key}
+                                        </p>
+                                        <Progress
+                                            value={(value * 100) / rating.totalQuantity}
+                                            color="gold"
+                                            left={key}
+                                            size="0.8rem"
+                                            style={{ width: "100%" }}
+                                            className={styles["progress"]}
+                                        />
+                                        <p
+                                            className={
+                                                styles["product-reviews-rating-tier-percentage"]
+                                            }
+                                        >
+                                            {Math.floor((value * 100) / rating.totalQuantity + 0.5)}
+                                            %
+                                        </p>
+                                    </div>
+                                );
+                            })}
                     </div>
                 </div>
-                <div className={styles["product-reviews-rating-bars"]}>
-                    {Object.entries(rating.quantities)
-                        .reverse()
-                        .map((entry) => {
-                            const [key, value] = entry;
-
-                            return (
-                                <div
-                                    className={styles["product-reviews-rating-bar"]}
-                                    key={`product-reviews-tier-${key}-progress-bar`}
-                                >
-                                    <p className={styles["product-reviews-rating-tier-key"]}>
-                                        {key}
-                                    </p>
-                                    <Progress
-                                        value={(value * 100) / rating.totalQuantity}
-                                        color="gold"
-                                        left={key}
-                                        size="0.8rem"
-                                        style={{ width: "100%" }}
-                                        className={styles["progress"]}
-                                    />
-                                    <p className={styles["product-reviews-rating-tier-percentage"]}>
-                                        {Math.floor((value * 100) / rating.totalQuantity + 0.5)}%
-                                    </p>
-                                </div>
-                            );
-                        })}
+                <div className={styles["filter-and-sort-options-container"]}>
+                    <label htmlFor="filter-reviews" className={styles["label"]}>
+                        Rating
+                        <select
+                            className={styles["select"]}
+                            id="filter-reviews"
+                            name="filter-reviews"
+                            aria-label="filter-reviews"
+                            defaultValue="All"
+                            onChange={(e) => {
+                                const { value } = e.target;
+                                setFilter(value as (typeof filterOptions)[number]);
+                            }}
+                            key="sort-options"
+                        >
+                            {filterOptions.map((option) => {
+                                return (
+                                    <option
+                                        className={styles["filter-reviews-option"]}
+                                        value={option}
+                                        key={`filter-reviews-option-${option}`}
+                                    >
+                                        {option}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </label>
+                    <label htmlFor="sort-reviews" className={styles["label"]}>
+                        Sort by
+                        <select
+                            className={styles["select"]}
+                            id="sort-reviews"
+                            name="sort-reviews"
+                            aria-label="sort-reviews"
+                            defaultValue="All"
+                            onChange={(e) => {
+                                const { value } = e.target;
+                                setSort(value as (typeof sortOptions)[number]);
+                            }}
+                            key="sort-options"
+                        >
+                            {sortOptions.map((option) => {
+                                return (
+                                    <option
+                                        className={styles["sort-reviews-option"]}
+                                        value={option}
+                                        key={`sort-reviews-option-${option}`}
+                                    >
+                                        {option}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </label>
                 </div>
             </div>
             <div className={styles["reviews"]} ref={targetRef}>
