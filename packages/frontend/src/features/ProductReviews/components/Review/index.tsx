48979ce776d79c559edc6b6e4490b178cf3d441b
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-25 12:58:45 +0100

    Add 'awaiting' prop and Skeleton components to Review component

--- packages/frontend/src/features/ProductReviews/components/Review/index.tsx
+++ packages/frontend/src/features/ProductReviews/components/Review/index.tsx
@@ -1,4 +1,4 @@
-import { Rating } from "@mantine/core";
+import { Rating, Skeleton } from "@mantine/core";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import dayjs from "dayjs";
@@ -7,22 +7,29 @@ import styles from "./index.module.css";
 
 export type TReview = {
     data: ProductReview;
+    awaiting?: boolean;
 };
 
-export function Review({ data }: TReview) {
+export function Review({ data, awaiting = false }: TReview) {
     const { rating, comment, datePosted } = data;
 
     return (
         <div className={styles["review"]}>
-            <Rating readOnly count={rating} value={rating} color="gold" size="md" />
-            <span className={styles["date-posted"]}>
-                Posted by username on {dayjs(datePosted).format("MMMM D, YYYY")}
-            </span>
-            <div className={styles["comment"]}>
-                <div className={styles["markdown-container"]}>
-                    <ReactMarkdown remarkPlugins={[remarkGfm]}>{comment}</ReactMarkdown>
+            <Skeleton visible={awaiting} width="min-content">
+                <Rating readOnly count={rating} value={rating} color="gold" size="md" />
+            </Skeleton>
+            <Skeleton visible={awaiting} width="20rem">
+                <span className={styles["date-posted"]}>
+                    Posted by username on {dayjs(datePosted).format("MMMM D, YYYY")}
+                </span>
+            </Skeleton>
+            <Skeleton visible={awaiting} width="100%">
+                <div className={styles["comment"]}>
+                    <div className={styles["markdown-container"]}>
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{comment}</ReactMarkdown>
+                    </div>
                 </div>
-            </div>
+            </Skeleton>
         </div>
     );
 }
