8a263125c40abeb21c8b4bc0d6b0a5bc018697a9
njcushing <129661299+njcushing@users.noreply.github.com>
2025-04-26 10:06:57 +0100

    Create 'UnwrapPromise' utility type

--- packages/frontend/src/utils/types/index.ts
+++ packages/frontend/src/utils/types/index.ts
@@ -1,3 +1,5 @@
 export type RecursivePartial<T> = {
     [P in keyof T]?: T[P] extends object ? RecursivePartial<T[P]> : T[P];
 };
+
+export type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
