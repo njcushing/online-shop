7f9d7dc1c0353157dd171a8a3efdb5580fdf2033
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-07 23:15:18 +0100

    Correctly handle start dates being after end dates
    The function will still generate a random time between those two dates.

--- packages/frontend/src/utils/dates/index.ts
+++ packages/frontend/src/utils/dates/index.ts
@@ -1,3 +1,6 @@
 export const generateDateWithinRandomRange = (start: Date, end: Date): Date => {
-    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    const startTime = start.getTime();
+    const endTime = end.getTime();
+
+    return new Date(Math.min(startTime, endTime) + Math.random() * Math.abs(endTime - startTime));
 };
