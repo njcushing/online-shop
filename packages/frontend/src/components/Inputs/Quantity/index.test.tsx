d765cee2246ea928584e5e178329ada8e7bde3f7
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-15 11:03:49 +0100

    Update unit tests for Quantity component

--- packages/frontend/src/components/Inputs/Quantity/index.test.tsx
+++ packages/frontend/src/components/Inputs/Quantity/index.test.tsx
@@ -5,12 +5,19 @@ import { Quantity } from ".";
 
 describe("The Quantity component...", () => {
     describe("Should render an <input> element...", () => {
-        test("With a 'type' attribute equal to 'number'", () => {
+        test("With a 'role' attribute equal to 'spinbutton'", () => {
             render(<Quantity />);
 
             const input = screen.getByRole("spinbutton");
             expect(input).toBeInTheDocument();
-            expect(input.getAttribute("type")).toBe("number");
+        });
+
+        test("With a 'type' attribute equal to 'text'", () => {
+            render(<Quantity />);
+
+            const input = screen.getByRole("spinbutton");
+            expect(input).toBeInTheDocument();
+            expect(input.getAttribute("type")).toBe("text");
         });
 
         test("With an initial value equal to the value of the 'defaultValue' prop", () => {
@@ -20,34 +27,18 @@ describe("The Quantity component...", () => {
             expect(input.value).toBe("3");
         });
 
-        test("That, when changed, should call the callback function passed to the 'onChange' prop", async () => {
-            const callback = vi.fn();
-
-            render(<Quantity defaultValue={3} onChange={callback} />);
-
-            // Called on mount/initial render
-            expect(callback).toHaveBeenCalledTimes(1);
+        test("Which should, on blur, set the input's value to the current value of the internal quantity state", async () => {
+            render(<Quantity defaultValue={3} min={1} max={10} />);
 
             const input = screen.getByRole("spinbutton") as HTMLInputElement;
 
+            expect(input.value).toBe("3");
+
+            await act(async () => userEvent.clear(input));
             await act(async () => userEvent.type(input, "0"));
+            await act(async () => fireEvent.blur(input));
 
-            expect(callback).toHaveBeenCalledTimes(2);
-        });
-
-        test("Passing the new value as an argument", async () => {
-            const callback = vi.fn();
-
-            render(<Quantity defaultValue={3} onChange={callback} />);
-
-            // Called on mount/initial render
-            expect(callback).toHaveBeenCalledWith(3);
-
-            const input = screen.getByRole("spinbutton") as HTMLInputElement;
-
-            await act(async () => userEvent.type(input, "0"));
-
-            expect(callback).toHaveBeenCalledWith(30);
+            expect(input.value).toBe("1");
         });
     });
 
@@ -83,6 +74,20 @@ describe("The Quantity component...", () => {
 
             expect(input.value).toBe("3");
         });
+
+        test("That should be disabled when the value of the 'disabled' prop is 'true'", async () => {
+            render(<Quantity disabled />);
+
+            const decrementButton = screen.getByRole("button", { name: "Reduce quantity" });
+            expect(decrementButton).toBeDisabled();
+        });
+
+        test("Or if the current value of the input is equal to the value of the 'min' prop", async () => {
+            render(<Quantity defaultValue={3} min={3} />);
+
+            const decrementButton = screen.getByRole("button", { name: "Reduce quantity" });
+            expect(decrementButton).toBeDisabled();
+        });
     });
 
     describe("Should render an 'increment' <button> element...", () => {
@@ -94,7 +99,7 @@ describe("The Quantity component...", () => {
             expect(incrementButton.getAttribute("type")).toBe("button");
         });
 
-        test("That, when clicked, should decrement the value of the input by 1", async () => {
+        test("That, when clicked, should increment the value of the input by 1", async () => {
             render(<Quantity defaultValue={3} />);
 
             const input = screen.getByRole("spinbutton") as HTMLInputElement;
@@ -116,6 +121,84 @@ describe("The Quantity component...", () => {
             await act(() => fireEvent.click(incrementButton));
 
             expect(input.value).toBe("3");
+        });
+
+        test("That should be disabled when the value of the 'disabled' prop is 'true'", async () => {
+            render(<Quantity disabled />);
+
+            const incrementButton = screen.getByRole("button", { name: "Increase quantity" });
+            expect(incrementButton).toBeDisabled();
+        });
+
+        test("Or if the current value of the input is equal to the value of the 'max' prop", async () => {
+            render(<Quantity defaultValue={3} max={3} />);
+
+            const incrementButton = screen.getByRole("button", { name: "Increase quantity" });
+            expect(incrementButton).toBeDisabled();
+        });
+    });
+
+    describe("Should take an 'onChange' callback function prop...", () => {
+        test("That, when changed, should call the callback function passed to the 'onChange' prop", async () => {
+            const callback = vi.fn();
+
+            render(<Quantity defaultValue={3} onChange={callback} />);
+
+            // Called on mount/initial render
+            expect(callback).toHaveBeenCalledTimes(1);
+
+            const input = screen.getByRole("spinbutton") as HTMLInputElement;
+
+            await act(async () => userEvent.type(input, "0"));
+
+            expect(callback).toHaveBeenCalledTimes(2);
+        });
+
+        test("Passing the new value as an argument", async () => {
+            const callback = vi.fn();
+
+            render(<Quantity defaultValue={3} onChange={callback} />);
+
+            // Called on mount/initial render
+            expect(callback).toHaveBeenCalledWith("3");
+
+            const input = screen.getByRole("spinbutton") as HTMLInputElement;
+
+            await act(async () => userEvent.type(input, "0"));
+
+            expect(callback).toHaveBeenCalledWith("30");
+        });
+    });
+
+    describe("Should take an 'onQuantityChange' callback function prop...", () => {
+        test("That should be invoked whenever the internal quantity state is changed", async () => {
+            const callback = vi.fn();
+
+            render(<Quantity defaultValue={1} onQuantityChange={callback} />);
+
+            // Called on mount/initial render
+            expect(callback).toHaveBeenCalledTimes(1);
+
+            const incrementButton = screen.getByRole("button", { name: "Increase quantity" });
+
+            await act(() => fireEvent.click(incrementButton));
+
+            expect(callback).toHaveBeenCalledTimes(2);
+        });
+
+        test("With the current quantity state value being passed as an argument", async () => {
+            const callback = vi.fn();
+
+            render(<Quantity defaultValue={1} onQuantityChange={callback} />);
+
+            // Called on mount/initial render
+            expect(callback).toHaveBeenCalledWith(1);
+
+            const incrementButton = screen.getByRole("button", { name: "Increase quantity" });
+
+            await act(() => fireEvent.click(incrementButton));
+
+            expect(callback).toHaveBeenCalledWith(2);
         });
     });
 });
