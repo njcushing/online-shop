fed12512f5ff3a5bd9405a0bfa8d4107f29ca3d6
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-21 23:28:51 +0100

    Wrap components being tested in BrowserRouter component

--- packages/frontend/test-utils.tsx
+++ packages/frontend/test-utils.tsx
@@ -3,15 +3,18 @@ import { render as testingLibraryRender } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MantineProvider } from "@mantine/core";
 import { DatesProvider } from "@mantine/dates";
+import { BrowserRouter } from "react-router-dom";
 import { theme } from "./mantine-config";
 
 export * from "@testing-library/react";
 export function render(ui: React.ReactNode) {
     return testingLibraryRender(ui, {
         wrapper: ({ children }: { children: React.ReactNode }) => (
-            <MantineProvider theme={theme}>
-                <DatesProvider settings={{ locale: "en" }}>{children}</DatesProvider>
-            </MantineProvider>
+            <BrowserRouter>
+                <MantineProvider theme={theme}>
+                    <DatesProvider settings={{ locale: "en" }}>{children}</DatesProvider>
+                </MantineProvider>
+            </BrowserRouter>
         ),
     });
 }
