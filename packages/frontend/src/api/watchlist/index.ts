6b419b6407408f2f984556575b1c183dd6eeee58
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-21 22:47:44 +0100

    Utilise getTokenFromStorage and fetcher functions in API query functions

--- packages/frontend/src/api/watchlist/index.ts
+++ packages/frontend/src/api/watchlist/index.ts
@@ -1,34 +1,22 @@
 import { UserWatchlist, watchlists } from "@/utils/products/watchlist";
 import * as HTTPMethodTypes from "../types";
-import { saveTokenFromAPIResponse } from "../utils/saveTokenFromAPIResponse";
+import { getTokenFromStorage } from "../utils/getTokenFromStorage";
+import { fetcher } from "../utils/fetcher";
 
 export const getWatchlist: HTTPMethodTypes.GET<undefined, UserWatchlist> = async (data) => {
-    const token = localStorage.getItem(import.meta.env.VITE_TOKEN_LOCAL_LOCATION);
+    const token = getTokenFromStorage();
     if (!token) return { status: 400, message: "No token provided for query", data: null };
 
-    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/api/watchlist`, {
-        signal: data.abortController ? data.abortController.signal : null,
-        method: "GET",
-        mode: "cors",
-        headers: { Authorization: token },
-    })
-        .then(async (response) => {
-            const responseJSON = await response.json();
-            saveTokenFromAPIResponse(responseJSON);
+    const result = await fetcher<UserWatchlist>(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/api/watchlist`,
+        {
+            signal: data.abortController ? data.abortController.signal : null,
+            method: "GET",
+            mode: "cors",
+            headers: { Authorization: token },
+        },
+    );
 
-            return {
-                status: responseJSON.status,
-                message: responseJSON.message,
-                data: responseJSON.data,
-            };
-        })
-        .catch((error) => {
-            return {
-                status: error.status ? error.status : 500,
-                message: error.message,
-                data: null,
-            };
-        });
     return result;
 };
 
@@ -54,35 +42,22 @@ export const updateWatchlist: HTTPMethodTypes.PUT<
     { variant: UserWatchlist[number] },
     UserWatchlist[]
 > = async (data) => {
-    const token = localStorage.getItem(import.meta.env.VITE_TOKEN_LOCAL_LOCATION);
+    const token = getTokenFromStorage();
     if (!token) return { status: 400, message: "No token provided for query", data: null };
 
     const { variant } = data.body || { variant: null };
     if (!variant) return { status: 400, message: "No variant provided for query", data: null };
 
-    const result = await fetch(`${import.meta.env.VITE_SERVER_DOMAIN}/api/watchlist`, {
-        signal: data.abortController ? data.abortController.signal : null,
-        method: "PUT",
-        mode: "cors",
-        headers: { Authorization: token },
-        body: JSON.stringify({ variant }),
-    })
-        .then(async (response) => {
-            const responseJSON = await response.json();
-            saveTokenFromAPIResponse(responseJSON);
+    const result = await fetcher<UserWatchlist[]>(
+        `${import.meta.env.VITE_SERVER_DOMAIN}/api/watchlist`,
+        {
+            signal: data.abortController ? data.abortController.signal : null,
+            method: "PUT",
+            mode: "cors",
+            headers: { Authorization: token },
+            body: JSON.stringify({ variant }),
+        },
+    );
 
-            return {
-                status: responseJSON.status,
-                message: responseJSON.message,
-                data: responseJSON.data,
-            };
-        })
-        .catch((error) => {
-            return {
-                status: error.status ? error.status : 500,
-                message: error.message,
-                data: null,
-            };
-        });
     return result;
 };
