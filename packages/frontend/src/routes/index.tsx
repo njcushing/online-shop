a908c026aaea6cd50dad9f3faf9ef3d32e8766cb
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-22 12:11:48 +0100

    Add future flags to Router

--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -2,34 +2,48 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Pages, Routes } from "@/pages";
 
 export function Router() {
-    const browserRouter = createBrowserRouter([
-        {
-            path: "/create-account",
-            element: <Pages.CreateAccount />,
-            errorElement: <div></div>,
-        },
-        {
-            path: "/login",
-            element: <Pages.Login />,
-            errorElement: <div></div>,
-        },
-        {
-            path: "/terms-and-conditions",
-            element: <Pages.TermsAndConditions />,
-            errorElement: <div></div>,
-        },
-        {
-            path: "/privacy-policy",
-            element: <Pages.PrivacyPolicy />,
-            errorElement: <div></div>,
-        },
-        {
-            path: "/",
-            element: <Pages.Root />,
-            children: Routes.Root,
-            errorElement: <div></div>,
-        },
-    ]);
-
-    return <RouterProvider router={browserRouter} />;
+    return (
+        <RouterProvider
+            router={createBrowserRouter(
+                [
+                    {
+                        path: "/create-account",
+                        element: <Pages.CreateAccount />,
+                        errorElement: <div></div>,
+                    },
+                    {
+                        path: "/login",
+                        element: <Pages.Login />,
+                        errorElement: <div></div>,
+                    },
+                    {
+                        path: "/terms-and-conditions",
+                        element: <Pages.TermsAndConditions />,
+                        errorElement: <div></div>,
+                    },
+                    {
+                        path: "/privacy-policy",
+                        element: <Pages.PrivacyPolicy />,
+                        errorElement: <div></div>,
+                    },
+                    {
+                        path: "/",
+                        element: <Pages.Root />,
+                        children: Routes.Root,
+                        errorElement: <div></div>,
+                    },
+                ],
+                {
+                    future: {
+                        v7_fetcherPersist: true,
+                        v7_relativeSplatPath: true,
+                        v7_partialHydration: true,
+                        v7_normalizeFormMethod: true,
+                        v7_skipActionErrorRevalidation: true,
+                    },
+                },
+            )}
+            future={{ v7_startTransition: true }}
+        />
+    );
 }
