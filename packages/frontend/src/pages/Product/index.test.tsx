ecab318a39635c4ae4a439a78f790a7a1f95b6dd
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-25 16:34:48 +0100

    Write additional unit tests for Product page component

--- packages/frontend/src/pages/Product/index.test.tsx
+++ packages/frontend/src/pages/Product/index.test.tsx
@@ -91,6 +91,17 @@ vi.mock("@/api/product", async (importOriginal) => {
     };
 });
 
+// Args not required for mock function - disabling linter error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const mockFindVariantFromOptions = vi.fn((...args: unknown[]) => mockProduct.variants![0]);
+vi.mock("@/utils/products/product", async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+        ...(actual || {}),
+        findVariantFromOptions: (...args: unknown[]) => mockFindVariantFromOptions(args),
+    };
+});
+
 vi.mock("@/features/ProductHero", () => ({
     ProductHero: () => <div aria-label="ProductHero component"></div>,
 }));
@@ -104,8 +115,15 @@ vi.mock("@/features/RecommendedProducts", () => ({
 }));
 
 describe("The Product component...", () => {
+    afterEach(() => {
+        // The Product component updates URL search params internally (e.g. - when setting
+        // 'selectedVariantOptions' state), so I'm ensuring each test starts from the same URL path
+        // for the sake of consistency.
+        window.history.pushState({}, "", "/");
+    });
+
     describe("Should provide context to all its descendant components...", () => {
-        describe("Including the 'product' object...", () => {
+        describe("Including the 'product' field...", () => {
             test("Which should initially have the 'data' field set to 'null'", async () => {
                 const { getProductContextValue } = await renderFunc({ initRender: true });
                 const ProductContextValue = getProductContextValue();
@@ -129,7 +147,7 @@ describe("The Product component...", () => {
             });
         });
 
-        describe("Including the 'variant' object...", () => {
+        describe("Including the 'variant' field...", () => {
             test("Which should initially be set to 'null'", async () => {
                 const { getProductContextValue } = await renderFunc({ initRender: true });
                 const ProductContextValue = getProductContextValue();
@@ -141,6 +159,112 @@ describe("The Product component...", () => {
                 await waitFor(async () => {
                     expect(variant).toBeNull();
                 });
+            });
+
+            test("Which should be populated by the return value of the 'findVariantFromOptions' function", async () => {
+                const { getProductContextValue } = await renderFunc();
+                const ProductContextValue = getProductContextValue();
+
+                const { variant } = ProductContextValue;
+
+                expect(variant).toStrictEqual(mockFindVariantFromOptions());
+            });
+        });
+
+        describe("Including the 'selectedVariantOptions' field...", () => {
+            test("Which should initially be set to an object containing the URL search params", async () => {
+                const mockURLParamValues = {
+                    option1Name: "option1Value1",
+                    option2Name: "option2Value1",
+                    option3Name: "option3Value1",
+                };
+                const mockURLParams = new URLSearchParams();
+                Object.entries(mockURLParamValues).forEach(([key, value]) => {
+                    mockURLParams.append(key, `${value}`);
+                });
+
+                window.history.pushState({}, "", `?${mockURLParams}`);
+
+                const { getProductContextValue } = await renderFunc({ initRender: true });
+                const ProductContextValue = getProductContextValue();
+                expect(ProductContextValue).toBeDefined();
+
+                const { selectedVariantOptions } = ProductContextValue;
+                expect(selectedVariantOptions).toBeDefined();
+
+                await waitFor(async () => {
+                    expect(selectedVariantOptions).toStrictEqual(mockURLParamValues);
+                });
+            });
+
+            test("Which should be populated by the internal 'variant' state's 'options' field", async () => {
+                const { getProductContextValue } = await renderFunc();
+                const ProductContextValue = getProductContextValue();
+
+                const { selectedVariantOptions } = ProductContextValue;
+
+                expect(selectedVariantOptions).toStrictEqual(mockFindVariantFromOptions()?.options);
+            });
+
+            test("And should update the URL search params accordingly", async () => {
+                await renderFunc();
+
+                const urlSearchParamsEntries = Object.fromEntries(
+                    new URLSearchParams(window.location.search).entries(),
+                );
+
+                expect(urlSearchParamsEntries).toStrictEqual(mockFindVariantFromOptions()?.options);
+            });
+        });
+
+        describe("Including a setter function for updating the 'selectedVariantOptions' field...", async () => {
+            test("Which, when called, should cause the 'findVariantFromOptions' to be called with the new state value", async () => {
+                const { getProductContextValue } = await renderFunc();
+                const ProductContextValue = getProductContextValue();
+
+                const { setSelectedVariantOptions } = ProductContextValue;
+                expect(setSelectedVariantOptions).toBeDefined();
+
+                mockFindVariantFromOptions.mockRestore();
+
+                const mockSelectedVariantOptions = {
+                    option1Name: "option1Value1",
+                    option2Name: "option2Value1",
+                    option3Name: "option3Value1",
+                };
+                await act(async () => setSelectedVariantOptions(mockSelectedVariantOptions));
+
+                const args = mockFindVariantFromOptions.mock.calls[0][0];
+                const selectedVariantOptionsArg = (args as unknown[])[1];
+
+                expect(selectedVariantOptionsArg).toStrictEqual(mockSelectedVariantOptions);
+            });
+        });
+
+        describe("Which, when consumed by a component other than the Product component...", () => {
+            test("Should match the shape of the expected context, but have fields containing default values", async () => {
+                let ProductContextValue!: IProductContext;
+
+                render(
+                    <div>
+                        <ProductContext.Consumer>
+                            {(value) => {
+                                ProductContextValue = value;
+                                return null;
+                            }}
+                        </ProductContext.Consumer>
+                    </div>,
+                );
+
+                expect(ProductContextValue).toBeDefined();
+
+                const { product, variant, selectedVariantOptions, setSelectedVariantOptions } =
+                    ProductContextValue;
+
+                expect(product).toBeDefined();
+                expect(variant).toBeDefined();
+                expect(selectedVariantOptions).toBeDefined();
+                expect(() => setSelectedVariantOptions({})).not.toThrow();
             });
         });
     });
