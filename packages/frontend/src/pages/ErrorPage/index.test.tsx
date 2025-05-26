24146c8361340e0d71f5d2115d867b2a2e07b1f1
njcushing <129661299+njcushing@users.noreply.github.com>
2025-05-26 14:21:09 +0100

    Write unit tests for ErrorPage component

---
+++ packages/frontend/src/pages/ErrorPage/index.test.tsx
@@ -0,0 +1,46 @@
+import { screen, render, userEvent } from "@test-utils";
+import { BrowserRouter } from "react-router-dom";
+import { act } from "react";
+import { ErrorPage } from ".";
+
+const renderFunc = async () => {
+    const component = (
+        // Using BrowserRouter for Link component
+        <BrowserRouter
+            future={{
+                v7_startTransition: true,
+                v7_relativeSplatPath: true,
+            }}
+        >
+            <ErrorPage />
+        </BrowserRouter>
+    );
+
+    const { rerender } = render(component);
+
+    return { rerender, component };
+};
+
+describe("The ErrorPage component...", () => {
+    test("Should render a heading element explaining to the user that they are on an error page", async () => {
+        renderFunc();
+
+        const heading = screen.getByRole("heading");
+        expect(heading).toBeInTheDocument();
+    });
+
+    test("Should render a Link component that directs the user to '/' on click", async () => {
+        window.history.pushState({}, "", "/some-path");
+
+        renderFunc();
+
+        const LinkComponent = screen.getByRole("link");
+        expect(LinkComponent).toBeInTheDocument();
+
+        expect(window.location.pathname).toBe("/some-path");
+
+        await act(() => userEvent.click(LinkComponent));
+
+        expect(window.location.pathname).toBe("/");
+    });
+});
