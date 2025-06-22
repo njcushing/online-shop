944909c51a7aa44f28c3c668e125f7eb79b97871
njcushing <129661299+njcushing@users.noreply.github.com>
2025-06-22 10:43:23 +0100

    Render AccountSettings component within Account page component

--- packages/frontend/src/pages/Account/index.tsx
+++ packages/frontend/src/pages/Account/index.tsx
@@ -1,5 +1,10 @@
+import { AccountSettings } from "@/features/AccountSettings";
 import styles from "./index.module.css";
 
 export function Account() {
-    return <div className={styles["page"]}></div>;
+    return (
+        <div className={styles["page"]}>
+            <AccountSettings />
+        </div>
+    );
 }
