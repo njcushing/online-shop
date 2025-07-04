1bd10b4018620eafdd7f438bc7b4dca674230afc
njcushing <129661299+njcushing@users.noreply.github.com>
2025-07-04 11:00:49 +0100

    Create Security component

---
+++ packages/frontend/src/features/AccountDetails/components/Security/index.tsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+import { UserContext } from "@/pages/Root";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormBuilder } from "@/features/AccountDetails/components/FormBuilder";
+import { PasswordsFormData, passwordsFormDataSchema } from "./schemas/passwordsSchema";
+import styles from "./index.module.css";
+
+export function Security() {
+    const { accountDetails } = useContext(UserContext);
+    const { awaiting } = accountDetails;
+
+    return (
+        <div className={styles["account-settings-content"]}>
+            <h1 className={styles["header"]}>Security</h1>
+
+            <div className={styles["forms-container"]}>
+                <FormBuilder<PasswordsFormData>
+                    fieldsets={[
+                        {
+                            legend: "Password",
+                            fields: [
+                                {
+                                    type: "text",
+                                    name: "newPassword",
+                                    label: "New password",
+                                    mode: "onTouched",
+                                },
+                                {
+                                    type: "text",
+                                    name: "confirmNewPassword",
+                                    label: "Confirm new password",
+                                    mode: "onTouched",
+                                },
+                            ],
+                            fullElement: (
+                                <div className={styles["update-password-message"]}>
+                                    Update your password
+                                </div>
+                            ),
+                        },
+                    ]}
+                    ariaLabel="Password"
+                    defaultValues={{ newPassword: "", confirmNewPassword: "" }}
+                    resolver={zodResolver(passwordsFormDataSchema)}
+                    disabled={awaiting}
+                />
+            </div>
+        </div>
+    );
+}
---
+++ packages/frontend/src/features/AccountDetails/components/Security/schemas/passwordsSchema.ts
@@ -0,0 +1,14 @@
+/* c8 ignore start */
+
+import { z } from "zod";
+import { Password, password } from "@/utils/schemas/user";
+
+export type PasswordsFormData = {
+    newPassword: Password;
+    confirmNewPassword: Password;
+};
+
+export const passwordsFormDataSchema: z.ZodType<PasswordsFormData> = z.object({
+    newPassword: password,
+    confirmNewPassword: password,
+});
