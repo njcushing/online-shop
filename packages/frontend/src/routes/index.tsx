0214edf2bafb6b4beb7ee5e3de0d14e66f412d89
njcushing <129661299+njcushing@users.noreply.github.com>
2025-07-20 17:31:19 +0100

    Render ScrollToTop component alongside the content for each main route

--- packages/frontend/src/routes/index.tsx
+++ packages/frontend/src/routes/index.tsx
@@ -1,49 +1,72 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Pages, Routes } from "@/pages";
+import { ScrollToTop } from "@/features/ScrollToTop";
 
 export function Router() {
-    return (
-        <RouterProvider
-            router={createBrowserRouter(
-                [
-                    {
-                        path: "/create-account",
-                        element: <Pages.CreateAccount />,
-                        errorElement: <Pages.ErrorPage />,
-                    },
-                    {
-                        path: "/login",
-                        element: <Pages.Login />,
-                        errorElement: <Pages.ErrorPage />,
-                    },
-                    {
-                        path: "/terms-and-conditions",
-                        element: <Pages.TermsAndConditions />,
-                        errorElement: <Pages.ErrorPage />,
-                    },
-                    {
-                        path: "/privacy-policy",
-                        element: <Pages.PrivacyPolicy />,
-                        errorElement: <Pages.ErrorPage />,
-                    },
-                    {
-                        path: "/",
-                        element: <Pages.Root />,
-                        children: Routes.Root,
-                        errorElement: <Pages.ErrorPage />,
-                    },
-                ],
-                {
-                    future: {
-                        v7_fetcherPersist: true,
-                        v7_relativeSplatPath: true,
-                        v7_partialHydration: true,
-                        v7_normalizeFormMethod: true,
-                        v7_skipActionErrorRevalidation: true,
-                    },
-                },
-            )}
-            future={{ v7_startTransition: true }}
-        />
+    const browserRouter = createBrowserRouter(
+        [
+            {
+                path: "/create-account",
+                element: (
+                    <>
+                        <ScrollToTop />
+                        <Pages.CreateAccount />
+                    </>
+                ),
+                errorElement: <Pages.ErrorPage />,
+            },
+            {
+                path: "/login",
+                element: (
+                    <>
+                        <ScrollToTop />
+                        <Pages.Login />
+                    </>
+                ),
+                errorElement: <Pages.ErrorPage />,
+            },
+            {
+                path: "/terms-and-conditions",
+                element: (
+                    <>
+                        <ScrollToTop />
+                        <Pages.TermsAndConditions />
+                    </>
+                ),
+                errorElement: <Pages.ErrorPage />,
+            },
+            {
+                path: "/privacy-policy",
+                element: (
+                    <>
+                        <ScrollToTop />
+                        <Pages.PrivacyPolicy />
+                    </>
+                ),
+                errorElement: <Pages.ErrorPage />,
+            },
+            {
+                path: "/",
+                element: (
+                    <>
+                        <ScrollToTop />
+                        <Pages.Root />
+                    </>
+                ),
+                children: Routes.Root,
+                errorElement: <Pages.ErrorPage />,
+            },
+        ],
+        {
+            future: {
+                v7_fetcherPersist: true,
+                v7_relativeSplatPath: true,
+                v7_partialHydration: true,
+                v7_normalizeFormMethod: true,
+                v7_skipActionErrorRevalidation: true,
+            },
+        },
     );
+
+    return <RouterProvider router={browserRouter} future={{ v7_startTransition: true }} />;
 }
