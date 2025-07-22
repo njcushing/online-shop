e04258d49efd0efd278bac24bafc91d84df4c8b9
njcushing <129661299+njcushing@users.noreply.github.com>
2025-07-22 18:08:09 +0100

    Add 'classNames' prop to Price component

--- packages/frontend/src/features/Price/index.tsx
+++ packages/frontend/src/features/Price/index.tsx
@@ -6,21 +6,29 @@ export type TPrice = {
     current: number;
     multiply?: number;
     size?: "xs" | "sm" | "md" | "lg" | "xl";
+    classNames?: {
+        container?: string;
+        current?: string;
+        base?: string;
+        discountPercentage?: string;
+    };
 };
 
-export function Price({ base = 0, current = 0, multiply = 1, size = "md" }: TPrice) {
+export function Price({ base = 0, current = 0, multiply = 1, size = "md", classNames }: TPrice) {
     return (
-        <div className={styles["price-container"]} data-size={size}>
-            <span className={styles["price-current"]}>
+        <div className={`${styles["price-container"]} ${classNames?.container}`} data-size={size}>
+            <span className={`${styles["price-current"]} ${classNames?.current}`}>
                 £{((current * multiply) / 100).toFixed(2)}
             </span>
 
             {current !== base && (
                 <>
-                    <span className={styles["price-base"]}>
+                    <span className={`${styles["price-base"]} ${classNames?.base}`}>
                         £{((base * multiply) / 100).toFixed(2)}
                     </span>
-                    <span className={styles["price-discount-percentage"]}>
+                    <span
+                        className={`${styles["price-discount-percentage"]} ${classNames?.discountPercentage}`}
+                    >
                         {createPriceAdjustmentString(current * multiply, base * multiply)}
                     </span>
                 </>
