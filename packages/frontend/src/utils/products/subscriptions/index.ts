de15484b50c2ee391013360735696b9c6bd83950
njcushing <129661299+njcushing@users.noreply.github.com>
2025-07-29 10:11:20 +0100

    Create mock subscriptions

--- packages/frontend/src/utils/products/subscriptions/index.ts
+++ packages/frontend/src/utils/products/subscriptions/index.ts
@@ -1,4 +1,5 @@
 import { ProductVariant, Product } from "@/utils/products/product";
+import { defaultAccountDetails } from "@/utils/schemas/account";
 import { Address } from "@/utils/schemas/address";
 
 const frequencies = [
@@ -29,3 +30,72 @@ export type PopulatedSubscriptionData = SubscriptionDataBase & {
     product: Product;
     variant: ProductVariant;
 };
+
+export const mockSubscriptions: SubscriptionData[] = [
+    {
+        id: "1",
+        userId: "1",
+        subscriptionDate: new Date().toISOString(),
+        deliveryAddress: defaultAccountDetails.addresses.delivery,
+        billingAddress: defaultAccountDetails.addresses.billing,
+        frequency: "one_week",
+
+        productId: "1",
+        variantId: "1-1",
+    },
+    {
+        id: "2",
+        userId: "1",
+        subscriptionDate: new Date().toISOString(),
+        deliveryAddress: defaultAccountDetails.addresses.delivery,
+        billingAddress: defaultAccountDetails.addresses.billing,
+        frequency: "two_weeks",
+
+        productId: "1",
+        variantId: "1-2",
+    },
+    {
+        id: "3",
+        userId: "1",
+        subscriptionDate: new Date().toISOString(),
+        deliveryAddress: defaultAccountDetails.addresses.delivery,
+        billingAddress: defaultAccountDetails.addresses.billing,
+        frequency: "one_month",
+
+        productId: "1",
+        variantId: "1-3",
+    },
+    {
+        id: "4",
+        userId: "1",
+        subscriptionDate: new Date().toISOString(),
+        deliveryAddress: defaultAccountDetails.addresses.delivery,
+        billingAddress: defaultAccountDetails.addresses.billing,
+        frequency: "three_months",
+
+        productId: "2",
+        variantId: "2-1",
+    },
+    {
+        id: "5",
+        userId: "1",
+        subscriptionDate: new Date().toISOString(),
+        deliveryAddress: defaultAccountDetails.addresses.delivery,
+        billingAddress: defaultAccountDetails.addresses.billing,
+        frequency: "six_months",
+
+        productId: "2",
+        variantId: "2-2",
+    },
+    {
+        id: "6",
+        userId: "1",
+        subscriptionDate: new Date().toISOString(),
+        deliveryAddress: defaultAccountDetails.addresses.delivery,
+        billingAddress: defaultAccountDetails.addresses.billing,
+        frequency: "one_year",
+
+        productId: "3",
+        variantId: "3-2",
+    },
+];
