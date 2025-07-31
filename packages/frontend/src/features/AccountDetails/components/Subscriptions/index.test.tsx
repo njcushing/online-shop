35377dd60b71912733abc320979691bfd7390fd8
njcushing <129661299+njcushing@users.noreply.github.com>
2025-07-31 19:51:17 +0100

    Create test suite for Subscriptions component

---
+++ packages/frontend/src/features/AccountDetails/components/Subscriptions/index.test.tsx
@@ -0,0 +1,164 @@
+import { vi } from "vitest";
+import { screen, render, within } from "@test-utils";
+import _ from "lodash";
+import { act } from "react";
+import { RecursivePartial } from "@/utils/types";
+import { IUserContext, UserContext } from "@/pages/Root";
+import { Subscriptions } from ".";
+
+const getProps = (component: HTMLElement) => {
+    return JSON.parse(component.getAttribute("data-props")!);
+};
+
+// Mock dependencies
+// Mock contexts are only using fields relevant to component being tested
+const mockSubscriptions: RecursivePartial<IUserContext["subscriptions"]["data"]> = [
+    { id: "1" },
+    { id: "2" },
+    { id: "3" },
+];
+
+const mockUserContext: RecursivePartial<IUserContext> = {
+    subscriptions: {
+        data: mockSubscriptions as IUserContext["subscriptions"]["data"],
+        status: 200,
+        message: "Success",
+        awaiting: false,
+    },
+
+    defaultData: { subscriptions: [] },
+};
+
+type renderFuncArgs = {
+    UserContextOverride?: IUserContext;
+    initRender?: boolean;
+};
+const renderFunc = async (args: renderFuncArgs = {}) => {
+    const { UserContextOverride, initRender = false } = args;
+
+    let UserContextValue!: IUserContext;
+
+    function Component({
+        context,
+    }: {
+        context?: { User?: renderFuncArgs["UserContextOverride"] };
+    }) {
+        const mergedUserContext = _.merge(_.cloneDeep(mockUserContext), context?.User);
+
+        return (
+            <UserContext.Provider value={mergedUserContext}>
+                <UserContext.Consumer>
+                    {(value) => {
+                        UserContextValue = value;
+                        return null;
+                    }}
+                </UserContext.Consumer>
+                <Subscriptions />
+            </UserContext.Provider>
+        );
+    }
+
+    // When using initRender, must wrap 'expect' in 'await waitFor'
+    const { rerender } = initRender
+        ? render(<Component context={{ User: UserContextOverride }} />)
+        : await act(() => render(<Component context={{ User: UserContextOverride }} />));
+
+    return {
+        rerenderFunc: (newArgs: renderFuncArgs) => {
+            rerender(<Component context={{ User: newArgs.UserContextOverride }} />);
+        },
+        getUserContextValue: () => UserContextValue,
+        component: <Component context={{ User: UserContextOverride }} />,
+    };
+};
+
+vi.mock(
+    "@/features/AccountDetails/components/Subscriptions/components/SubscriptionSummary",
+    () => ({
+        SubscriptionSummary: vi.fn((props: unknown & { data: { id: string } }) => {
+            const { data } = props;
+            const { id } = data;
+
+            return (
+                <div
+                    aria-label="SubscriptionSummary component"
+                    data-props={JSON.stringify(props)}
+                >{`SubscriptionSummary id: ${id}`}</div>
+            );
+        }),
+    }),
+);
+
+describe("The Subscriptions component...", () => {
+    describe("Should render a <ul> element...", () => {
+        test("If the UserContext's 'subscriptions.data' array field contains at least one entry", () => {
+            renderFunc();
+
+            const ulElement = screen.getByRole("list");
+            expect(ulElement).toBeInTheDocument();
+        });
+
+        test("Unless the UserContext's 'subscriptions.data' and 'defaultData.subscriptions' array fields are falsy or empty", () => {
+            renderFunc({ UserContextOverride: { subscriptions: { data: null } } as IUserContext });
+
+            const ulElement = screen.queryByRole("list");
+            expect(ulElement).not.toBeInTheDocument();
+        });
+
+        describe("That renders an SubscriptionSummary component...", () => {
+            test("For each entry in the UserContext's 'subscriptions.data' array field", () => {
+                renderFunc();
+
+                const ulElement = screen.getByRole("list");
+
+                mockSubscriptions.forEach((subscription) => {
+                    const { id } = subscription!;
+
+                    const SubscriptionSummaryComponent = within(ulElement).getByText(
+                        `SubscriptionSummary id: ${id}`,
+                    );
+                    expect(SubscriptionSummaryComponent).toBeInTheDocument();
+                });
+            });
+
+            test("For each entry in the UserContext's 'defaultData.subscriptions' array field if the UserContext's 'subscriptions.data' array field is falsy or empty", () => {
+                renderFunc({
+                    UserContextOverride: {
+                        subscriptions: { data: null },
+                        defaultData: { subscriptions: [{ id: "4" }] },
+                    } as IUserContext,
+                });
+
+                const ulElement = screen.getByRole("list");
+
+                const SubscriptionSummaryComponent = within(ulElement).getByText(
+                    "SubscriptionSummary id: 4",
+                );
+                expect(SubscriptionSummaryComponent).toBeInTheDocument();
+            });
+
+            test("Passing the correct props", () => {
+                renderFunc();
+
+                const ulElement = screen.getByRole("list");
+
+                mockSubscriptions.forEach((subscription, i) => {
+                    const { id } = subscription!;
+
+                    const SubscriptionSummaryComponent = within(ulElement).getByText(
+                        `SubscriptionSummary id: ${id}`,
+                    );
+                    const props = getProps(SubscriptionSummaryComponent);
+                    expect(props).toStrictEqual({ data: mockUserContext.subscriptions!.data![i] });
+                });
+            });
+        });
+    });
+
+    test("Should render a relevant message if the UserContext's 'subscriptions.data' and 'defaultData.subscriptions' array fields are falsy or empty", () => {
+        renderFunc({ UserContextOverride: { subscriptions: { data: null } } as IUserContext });
+
+        const messageElement = screen.getByText("Nothing to show!");
+        expect(messageElement).toBeInTheDocument();
+    });
+});
