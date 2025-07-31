44f6939cf57da6657e3d4587f939a37107aa240d
njcushing <129661299+njcushing@users.noreply.github.com>
2025-07-31 22:32:41 +0100

    Render product information in SubscriptionSummary component

--- packages/frontend/src/features/AccountDetails/components/Subscriptions/components/SubscriptionSummary/index.tsx
+++ packages/frontend/src/features/AccountDetails/components/Subscriptions/components/SubscriptionSummary/index.tsx
@@ -1,4 +1,10 @@
+import { useContext } from "react";
+import { UserContext } from "@/pages/Root";
+import { Link } from "react-router-dom";
+import { Skeleton, Image } from "@mantine/core";
+import { variantOptions } from "@/utils/products/product";
 import { PopulatedSubscriptionData } from "@/utils/products/subscriptions";
+import { Price } from "@/features/Price";
 import styles from "./index.module.css";
 
 export type TSubscriptionSummary = {
@@ -6,5 +12,81 @@ export type TSubscriptionSummary = {
 };
 
 export function SubscriptionSummary({ data }: TSubscriptionSummary) {
-    return <li className={styles["subscription-summary"]}></li>;
+    const { subscriptions } = useContext(UserContext);
+    const { awaiting } = subscriptions;
+
+    const { product, variant } = data;
+
+    const { id: productId, slug, name, images } = product;
+    const { price, options, image } = variant;
+    const { current } = price;
+
+    const usedImage = image || images.thumb;
+    const { src, alt } = usedImage;
+
+    const variantUrlParams = new URLSearchParams();
+    Object.entries(options).forEach(([key, value]) => variantUrlParams.append(key, `${value}`));
+
+    return (
+        <li className={styles["subscription-summary"]}>
+            <div className={styles["subscription-product"]}>
+                <Skeleton visible={awaiting}>
+                    <Image
+                        className={styles["product-thumbnail-image"]}
+                        src={src}
+                        alt={alt}
+                        style={{ visibility: awaiting ? "hidden" : "initial" }}
+                    />
+                </Skeleton>
+
+                <div className={styles["product-information"]}>
+                    <Skeleton visible={awaiting}>
+                        <Link
+                            to={`/p/${productId}/${slug}?${variantUrlParams}`}
+                            className={styles["product-full-name"]}
+                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                        >
+                            {name.full}
+                        </Link>
+                    </Skeleton>
+
+                    <div className={styles["product-variant-options"]}>
+                        {Object.entries(options).map((option) => {
+                            const [key, value] = option;
+                            const variantOption = variantOptions.find((vOpt) => vOpt.id === key);
+                            const variantOptionValue = variantOption?.values.find(
+                                (vOptVal) => vOptVal.id === value,
+                            );
+                            return (
+                                <Skeleton visible={awaiting} key={`${key}-skeleton`}>
+                                    <div
+                                        className={styles["product-variant-option-info"]}
+                                        key={key}
+                                        style={{ visibility: awaiting ? "hidden" : "initial" }}
+                                    >
+                                        <p className={styles["product-variant-option-name"]}>
+                                            {variantOption?.name || key}:{" "}
+                                        </p>
+                                        <p className={styles["product-variant-option-value"]}>
+                                            {variantOptionValue?.name || value}
+                                        </p>
+                                    </div>
+                                </Skeleton>
+                            );
+                        })}
+                    </div>
+
+                    <Skeleton visible={awaiting}>
+                        <div style={{ visibility: awaiting ? "hidden" : "initial" }}>
+                            <Price
+                                base={current}
+                                current={current}
+                                classNames={{ current: styles["price-container"] }}
+                            />
+                        </div>
+                    </Skeleton>
+                </div>
+            </div>
+        </li>
+    );
 }
