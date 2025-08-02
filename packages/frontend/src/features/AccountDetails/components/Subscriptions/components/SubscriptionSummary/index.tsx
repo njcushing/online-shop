e91b7f2d5d66783fa49124aa2e272046dad3212c
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-02 13:38:34 +0100

    Adjust layout of SubscriptionSummary component

--- packages/frontend/src/features/AccountDetails/components/Subscriptions/components/SubscriptionSummary/index.tsx
+++ packages/frontend/src/features/AccountDetails/components/Subscriptions/components/SubscriptionSummary/index.tsx
@@ -49,121 +49,139 @@ export function SubscriptionSummary({ data }: TSubscriptionSummary) {
 
     return (
         <li className={styles["subscription-summary"]}>
-            <div className={styles["subscription-product"]}>
-                <Skeleton visible={awaiting}>
-                    <Image
-                        className={styles["product-thumbnail-image"]}
-                        src={src}
-                        alt={alt}
-                        style={{ visibility: awaiting ? "hidden" : "initial" }}
-                    />
-                </Skeleton>
-
-                <div className={styles["product-information"]}>
-                    <Skeleton visible={awaiting}>
-                        <Link
-                            to={`/p/${productId}/${slug}?${variantUrlParams}`}
-                            className={styles["product-full-name"]}
-                            style={{ visibility: awaiting ? "hidden" : "initial" }}
-                        >
-                            {name.full}
-                        </Link>
+            <div className={styles["top-bar"]}>
+                <div className={styles["frequency"]}>
+                    <Skeleton visible={awaiting} width="min-content">
+                        <strong style={{ visibility: awaiting ? "hidden" : "initial" }}>
+                            Delivery Frequency
+                        </strong>
                     </Skeleton>
 
-                    <div className={styles["product-variant-options"]}>
-                        {Object.entries(options).map((option) => {
-                            const [key, value] = option;
-                            const variantOption = variantOptions.find((vOpt) => vOpt.id === key);
-                            const variantOptionValue = variantOption?.values.find(
-                                (vOptVal) => vOptVal.id === value,
-                            );
-                            return (
-                                <Skeleton visible={awaiting} key={`${key}-skeleton`}>
-                                    <div
-                                        className={styles["product-variant-option-info"]}
-                                        key={key}
-                                        style={{ visibility: awaiting ? "hidden" : "initial" }}
-                                    >
-                                        <p className={styles["product-variant-option-name"]}>
-                                            {variantOption?.name || key}:{" "}
-                                        </p>
-                                        <p className={styles["product-variant-option-value"]}>
-                                            {variantOptionValue?.name || value}
-                                        </p>
-                                    </div>
-                                </Skeleton>
-                            );
-                        })}
-                    </div>
+                    <Skeleton visible={awaiting} width="min-content">
+                        <p
+                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                        >{`${count} unit${count !== 1 ? "s" : ""} every ${subscriptionFrequencyMessage(frequency)}`}</p>
+                    </Skeleton>
+                </div>
 
-                    <div className={styles["price-container"]}>
-                        <Skeleton visible={awaiting}>
-                            <div style={{ visibility: awaiting ? "hidden" : "initial" }}>
-                                <Price
-                                    base={base}
-                                    current={current * (1 - subscriptionDiscountPercentage / 100)}
-                                    classNames={{ current: styles["price-current"] }}
-                                />
-                            </div>
-                        </Skeleton>
+                <div className={styles["next-delivery-date"]}>
+                    <Skeleton visible={awaiting} width="min-content">
+                        <strong style={{ visibility: awaiting ? "hidden" : "initial" }}>
+                            Next Delivery Date
+                        </strong>
+                    </Skeleton>
 
-                        {!awaiting && subscriptionDiscountPercentage > 0 && (
-                            <p className={styles["discount-percentage-message"]}>
-                                The above unit cost includes a{" "}
-                                <strong>{subscriptionDiscountPercentage}%</strong> discount for
-                                subscriptions to this product.
-                            </p>
-                        )}
-                    </div>
+                    <Skeleton visible={awaiting} width="min-content">
+                        <p
+                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                        >{`${dayjs(nextDate).format("MMMM D, YYYY")}`}</p>
+                    </Skeleton>
                 </div>
             </div>
 
-            <div className={styles["details"]}>
-                <Skeleton visible={awaiting}>
-                    <div
-                        className={styles["frequency"]}
-                        style={{ visibility: awaiting ? "hidden" : "initial" }}
-                    >{`${count} unit${count !== 1 ? "s" : ""} every ${subscriptionFrequencyMessage(frequency)}`}</div>
-                </Skeleton>
+            <div className={styles["content"]}>
+                <div className={styles["subscription-product"]}>
+                    <Skeleton visible={awaiting}>
+                        <Image
+                            className={styles["product-thumbnail-image"]}
+                            src={src}
+                            alt={alt}
+                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                        />
+                    </Skeleton>
 
-                <Skeleton visible={awaiting}>
-                    <p
-                        className={styles["next-delivery-date"]}
-                        style={{ visibility: awaiting ? "hidden" : "initial" }}
-                    >
-                        Next delivery: {`${dayjs(nextDate).format("MMMM D, YYYY")}`}
-                    </p>
-                </Skeleton>
-            </div>
+                    <div className={styles["product-information"]}>
+                        <Skeleton visible={awaiting}>
+                            <Link
+                                to={`/p/${productId}/${slug}?${variantUrlParams}`}
+                                className={styles["product-full-name"]}
+                                style={{ visibility: awaiting ? "hidden" : "initial" }}
+                            >
+                                {name.full}
+                            </Link>
+                        </Skeleton>
 
-            <div className={styles["options"]}>
-                <Skeleton visible={awaiting} width="min-content">
-                    <Button
-                        onClick={() => {}}
-                        color="rgb(241, 202, 168)"
-                        variant="filled"
-                        radius={9999}
-                        className={styles["button"]}
-                        disabled={awaiting}
-                        style={{ visibility: awaiting ? "hidden" : "initial" }}
-                    >
-                        Change delivery schedule
-                    </Button>
-                </Skeleton>
+                        <div className={styles["product-variant-options"]}>
+                            {Object.entries(options).map((option) => {
+                                const [key, value] = option;
+                                const variantOption = variantOptions.find(
+                                    (vOpt) => vOpt.id === key,
+                                );
+                                const variantOptionValue = variantOption?.values.find(
+                                    (vOptVal) => vOptVal.id === value,
+                                );
+                                return (
+                                    <Skeleton visible={awaiting} key={`${key}-skeleton`}>
+                                        <div
+                                            className={styles["product-variant-option-info"]}
+                                            key={key}
+                                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                                        >
+                                            <p className={styles["product-variant-option-name"]}>
+                                                {variantOption?.name || key}:{" "}
+                                            </p>
+                                            <p className={styles["product-variant-option-value"]}>
+                                                {variantOptionValue?.name || value}
+                                            </p>
+                                        </div>
+                                    </Skeleton>
+                                );
+                            })}
+                        </div>
 
-                <Skeleton visible={awaiting} width="min-content">
-                    <Button
-                        onClick={() => {}}
-                        color="rgb(241, 202, 168)"
-                        variant="filled"
-                        radius={9999}
-                        className={styles["button"]}
-                        disabled={awaiting}
-                        style={{ visibility: awaiting ? "hidden" : "initial" }}
-                    >
-                        Cancel subscription
-                    </Button>
-                </Skeleton>
+                        <div className={styles["price-container"]}>
+                            <Skeleton visible={awaiting}>
+                                <div style={{ visibility: awaiting ? "hidden" : "initial" }}>
+                                    <Price
+                                        base={base}
+                                        current={
+                                            current * (1 - subscriptionDiscountPercentage / 100)
+                                        }
+                                        classNames={{ current: styles["price-current"] }}
+                                    />
+                                </div>
+                            </Skeleton>
+
+                            {!awaiting && subscriptionDiscountPercentage > 0 && (
+                                <p className={styles["discount-percentage-message"]}>
+                                    The above unit cost includes a{" "}
+                                    <strong>{subscriptionDiscountPercentage}%</strong> discount for
+                                    subscriptions to this product.
+                                </p>
+                            )}
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles["options"]}>
+                    <Skeleton visible={awaiting} width="min-content">
+                        <Button
+                            onClick={() => {}}
+                            color="rgb(241, 202, 168)"
+                            variant="filled"
+                            radius={9999}
+                            className={styles["button"]}
+                            disabled={awaiting}
+                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                        >
+                            Change delivery schedule
+                        </Button>
+                    </Skeleton>
+
+                    <Skeleton visible={awaiting} width="min-content">
+                        <Button
+                            onClick={() => {}}
+                            color="rgb(241, 202, 168)"
+                            variant="filled"
+                            radius={9999}
+                            className={styles["button"]}
+                            disabled={awaiting}
+                            style={{ visibility: awaiting ? "hidden" : "initial" }}
+                        >
+                            Cancel subscription
+                        </Button>
+                    </Skeleton>
+                </div>
             </div>
         </li>
     );
