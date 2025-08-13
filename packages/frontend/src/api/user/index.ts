8021de9018185fda0c3441020f5e4f6d05d8973d
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-13 21:01:19 +0100

    Write 'getUser' API query function

--- packages/frontend/src/api/user/index.ts
+++ packages/frontend/src/api/user/index.ts
@@ -1,6 +1,22 @@
+import { User } from "@/utils/schemas/user";
 import { AccountCreationFormData } from "@/features/AccountCreationForm/utils/zodSchema";
 import * as HTTPMethodTypes from "../types";
+import { getTokenFromStorage } from "../utils/getTokenFromStorage";
 import { fetcher } from "../utils/fetcher";
+
+export const getUser: HTTPMethodTypes.GET<undefined, User> = async (data) => {
+    const token = getTokenFromStorage();
+    if (!token) return { status: 400, message: "No token provided for query", data: null };
+
+    const result = await fetcher<User>(`${import.meta.env.VITE_SERVER_DOMAIN}/api`, {
+        signal: data.abortController ? data.abortController.signal : null,
+        method: "GET",
+        mode: "cors",
+        headers: { Authorization: token },
+    });
+
+    return result;
+};
 
 export const createAccount: HTTPMethodTypes.POST<undefined, AccountCreationFormData, null> = async (
     data,
