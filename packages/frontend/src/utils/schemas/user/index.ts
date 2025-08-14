2f90f922d9353d0f6ac5ef21b9613da308c2c5f8
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-14 10:55:47 +0100

    Include 'cart' field on User type

--- packages/frontend/src/utils/schemas/user/index.ts
+++ packages/frontend/src/utils/schemas/user/index.ts
@@ -1,16 +1,19 @@
 import { OrderDataBase } from "@/utils/products/orders";
+import { CartItemData, mockCart } from "@/utils/products/cart";
 import { SubscriptionDataBase } from "@/utils/products/subscriptions";
 import { DeepRequired } from "react-hook-form";
 import { defaultProfile, Profile } from "../profile";
 
 export type User = {
     profile: Profile;
+    cart: CartItemData[];
     orders: OrderDataBase["id"][];
     subscriptions: SubscriptionDataBase["id"][];
 };
 
 export const defaultUser: DeepRequired<User> = {
     profile: defaultProfile,
+    cart: mockCart,
     orders: ["1", "2", "3", "4", "5"],
     subscriptions: ["1", "2", "3", "4", "5", "6"],
 };
