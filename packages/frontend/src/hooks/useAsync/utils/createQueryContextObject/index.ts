24e09a36fa9cfd590a4f660212473c6101684938
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-15 10:54:21 +0100

    Add optional parameter for setting fields' initial values
    I also changed 'awaiting' to be false by default.

--- packages/frontend/src/hooks/useAsync/utils/createQueryContextObject/index.ts
+++ packages/frontend/src/hooks/useAsync/utils/createQueryContextObject/index.ts
@@ -1,15 +1,18 @@
 import { UseAsyncReturnType } from "@/hooks/useAsync";
+import { RecursivePartial } from "@/utils/types";
+import _ from "lodash";
 
-export const createQueryContextObject = <FuncParams, FuncBody, FuncResponse>(): UseAsyncReturnType<
-    FuncParams,
-    FuncBody,
-    FuncResponse
-> => {
-    return {
-        response: { status: 200, message: "Success", data: null },
-        setParams: () => {},
-        attempt: () => {},
-        abort: () => {},
-        awaiting: true,
-    };
+export const createQueryContextObject = <FuncParams, FuncBody, FuncResponse>(
+    initial: RecursivePartial<UseAsyncReturnType<FuncParams, FuncBody, FuncResponse>> = {},
+): UseAsyncReturnType<FuncParams, FuncBody, FuncResponse> => {
+    return _.merge(
+        {
+            response: { status: 200, message: "Success", data: null },
+            setParams: () => {},
+            attempt: () => {},
+            abort: () => {},
+            awaiting: false,
+        },
+        initial,
+    );
 };
