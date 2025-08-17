d187b3eb55e52b542839283118b32c2e2cd2c21f
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-17 13:12:24 +0100

    Write boilerplate for CheckoutContent component

---
+++ packages/frontend/src/features/CheckoutContent/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export function CheckoutContent() {
+    return (
+        <section className={styles["checkout-content"]}>
+            <div className={styles["checkout-content-width-controller"]}></div>
+        </section>
+    );
+}
