ab4f3ffb41f46213dede702d754464d0059e7b02
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-19 23:38:57 +0100

    Render cart price breakdown in CheckoutContent component

--- packages/frontend/src/features/CheckoutContent/index.tsx
+++ packages/frontend/src/features/CheckoutContent/index.tsx
@@ -1,15 +1,22 @@
 import { useContext } from "react";
-import { UserContext } from "@/pages/Root";
+import { IUserContext, UserContext } from "@/pages/Root";
 import { Divider } from "@mantine/core";
-import styles from "./index.module.css";
+import { calculateCartSubtotal } from "@/utils/products/utils/calculateCartSubtotal";
 import { CartItem } from "../Cart/components/CartItem";
+import styles from "./index.module.css";
 
 export function CheckoutContent() {
-    const { cart } = useContext(UserContext);
+    const { cart, defaultData } = useContext(UserContext);
     const { response, awaiting } = cart;
     const { data } = response;
 
-    return (
+    const cartItems = !awaiting
+        ? data
+        : (defaultData.cart as NonNullable<IUserContext["cart"]["response"]["data"]>);
+
+    const { cost, discount } = calculateCartSubtotal(cartItems || []);
+
+    return cartItems ? (
         <section className={styles["checkout-content"]}>
             <div className={styles["checkout-content-width-controller"]}>
                 <div className={styles["checkout-content-left"]}>
@@ -28,7 +35,7 @@ export function CheckoutContent() {
                         <Divider className={styles["divider"]} />
 
                         <ul className={styles["cart-items"]}>
-                            {data?.map((item) => {
+                            {cartItems.map((item) => {
                                 return (
                                     <CartItem
                                         data={item}
@@ -53,9 +60,55 @@ export function CheckoutContent() {
                                 );
                             })}
                         </ul>
+
+                        <Divider className={styles["divider"]} />
+
+                        <div className={styles["cost-breakdown-group"]}>
+                            <div className={styles["cost-breakdown-line"]}>
+                                <span>Item(s) Subtotal:</span>
+                                <span>£{(cost.products / 100).toFixed(2)}</span>
+                            </div>
+
+                            {discount.products !== 0 && (
+                                <div className={styles["cost-breakdown-line"]}>
+                                    <span>Item Discounts:</span>
+                                    <span>-£{(discount.products / 100).toFixed(2)}</span>
+                                </div>
+                            )}
+
+                            {discount.subscriptions !== 0 && (
+                                <div className={styles["cost-breakdown-line"]}>
+                                    <span>Subscriptions:</span>
+                                    <span>-£{(discount.subscriptions / 100).toFixed(2)}</span>
+                                </div>
+                            )}
+
+                            {discount.promotions !== 0 && (
+                                <div className={styles["cost-breakdown-line"]}>
+                                    <span>Promotions</span>
+                                    <span>-£{(discount.promotions / 100).toFixed(2)}</span>
+                                </div>
+                            )}
+                        </div>
+
+                        <Divider className={styles["divider"]} />
+
+                        <div className={styles["cost-breakdown-line"]}>
+                            <span>Postage:</span>
+                            <span>
+                                {`${cost.postage !== 0 ? `£${(cost.postage / 100).toFixed(2)}` : "FREE"}`}
+                            </span>
+                        </div>
+
+                        <Divider className={styles["divider"]} />
+
+                        <div className={styles["cost-breakdown-line"]}>
+                            <span>Total:</span>
+                            <span>£{(cost.total / 100).toFixed(2)}</span>
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-    );
+    ) : null;
 }
