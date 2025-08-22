86233384939eeaa524d286c2d6f12303586637c3
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-22 10:46:56 +0100

    Change type of 'cart' field on User type

--- packages/frontend/src/utils/schemas/user/index.ts
+++ packages/frontend/src/utils/schemas/user/index.ts
@@ -1,19 +1,18 @@
-import { CartItemData, mockCart } from "@/utils/products/cart";
+import { Cart, mockCart } from "@/utils/products/cart";
 import { ProductVariant } from "@/utils/products/product";
 import { OrderDataBase } from "@/utils/products/orders";
 import { SubscriptionDataBase } from "@/utils/products/subscriptions";
-import { DeepRequired } from "react-hook-form";
 import { defaultProfile, Profile } from "../profile";
 
 export type User = {
     profile: Profile;
-    cart: CartItemData[];
+    cart: Cart;
     watchlist: ProductVariant["id"][];
     orders: OrderDataBase["id"][];
     subscriptions: SubscriptionDataBase["id"][];
 };
 
-export const defaultUser: DeepRequired<User> = {
+export const defaultUser: User = {
     profile: defaultProfile,
     cart: mockCart,
     watchlist: ["1-1", "1-2", "2-2", "3-1", "3-3"],
