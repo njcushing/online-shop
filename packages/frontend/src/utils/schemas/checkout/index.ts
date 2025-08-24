cdb8cfdd6c55e11f1875ad6206ab86647e1de118
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-24 15:15:31 +0100

    Create schema for address form

--- packages/frontend/src/utils/schemas/checkout/index.ts
+++ packages/frontend/src/utils/schemas/checkout/index.ts
@@ -2,6 +2,7 @@
 
 import { z } from "zod";
 import { Profile } from "@/utils/schemas/profile";
+import { Address, address } from "@/utils/schemas/address";
 import { name, email, phone } from "@/utils/schemas/personal";
 import { DeepPick } from "ts-deep-pick";
 
@@ -10,8 +11,16 @@ export type CheckoutPersonalFormData = DeepPick<
     "personal.firstName" | "personal.lastName" | "personal.email" | "personal.phone"
 >;
 
+export type CheckoutAddressFormData = {
+    address: {
+        delivery: Address;
+        billing: Address;
+    };
+};
+
 export type CheckoutFormData = {
     personal: CheckoutPersonalFormData;
+    address: CheckoutAddressFormData;
 };
 
 export const checkoutPersonalFormDataSchema: z.ZodType<CheckoutPersonalFormData> = z.object({
@@ -20,5 +29,12 @@ export const checkoutPersonalFormDataSchema: z.ZodType<CheckoutPersonalFormData>
         lastName: name,
         email,
         phone: phone.optional(),
+    }),
+});
+
+export const checkoutAddressFormDataSchema: z.ZodType<CheckoutAddressFormData> = z.object({
+    address: z.object({
+        delivery: address,
+        billing: address,
     }),
 });
