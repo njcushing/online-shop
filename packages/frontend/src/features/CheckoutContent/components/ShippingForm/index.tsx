dd0b4f5575d989312df7cba1af5fd25e9a98db52
njcushing <129661299+njcushing@users.noreply.github.com>
2025-08-25 16:29:44 +0100

    Create ShippingForm component

---
+++ packages/frontend/src/features/CheckoutContent/components/ShippingForm/index.tsx
@@ -0,0 +1,337 @@
+import { useContext, useState, useEffect, useCallback, useRef, useMemo } from "react";
+import { UserContext } from "@/pages/Root";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CheckoutAddressFormData, checkoutAddressFormDataSchema } from "@/utils/schemas/checkout";
+import { useForm, useWatch, Controller, SubmitHandler } from "react-hook-form";
+import { TextInput, Button, Checkbox, Collapse } from "@mantine/core";
+import { createInputError } from "@/utils/createInputError";
+import _ from "lodash";
+import styles from "./index.module.css";
+
+const inputProps = {
+    classNames: {
+        input: styles["form-field-input"],
+        label: styles["form-field-label"],
+    },
+};
+
+export type TShipping = {
+    onReturn?: () => void;
+    onSubmit?: SubmitHandler<CheckoutAddressFormData>;
+};
+
+const emptyBillingFields: CheckoutAddressFormData["address"]["billing"] = {
+    line1: "",
+    line2: "",
+    townCity: "",
+    county: "",
+    postcode: "",
+};
+
+export function ShippingForm({ onReturn, onSubmit }: TShipping) {
+    const { user, cart } = useContext(UserContext);
+
+    const { response, awaiting: userAwaiting } = user;
+    const { data } = response;
+    const { profile } = data || {};
+    const { addresses } = profile || {};
+    const { delivery } = addresses || {};
+    const {
+        line1: dLine1,
+        line2: dLine2,
+        townCity: dTownCity,
+        county: dCounty,
+        postcode: dPostcode,
+    } = delivery || {};
+
+    const { awaiting: cartAwaiting } = cart;
+
+    const defaultValues = useMemo(() => {
+        return {
+            address: {
+                delivery: {
+                    line1: dLine1,
+                    line2: dLine2,
+                    townCity: dTownCity,
+                    county: dCounty,
+                    postcode: dPostcode,
+                },
+                billing: emptyBillingFields,
+            },
+        };
+    }, [dLine1, dLine2, dTownCity, dCounty, dPostcode]);
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        getValues,
+        setValue,
+    } = useForm<CheckoutAddressFormData>({
+        defaultValues,
+        mode: "onTouched",
+        resolver: zodResolver(checkoutAddressFormDataSchema),
+    });
+
+    useEffect(() => reset(defaultValues), [defaultValues, reset]);
+
+    const getError = useCallback(
+        (name: string) => {
+            const fieldError = _.get(errors, `${name}.message`);
+            return createInputError(typeof fieldError === "string" ? fieldError : undefined);
+        },
+        [errors],
+    );
+
+    const [billingIsDelivery, setBillingIsDelivery] = useState<boolean>(true);
+    const currentDeliveryValues = useWatch({ control, name: "address.delivery" });
+    const cachedBilling = useRef<CheckoutAddressFormData["address"]["billing"]>(emptyBillingFields);
+    useEffect(() => {
+        if (billingIsDelivery) {
+            cachedBilling.current = getValues("address.billing");
+            setValue("address.billing", currentDeliveryValues, { shouldValidate: true });
+        } else {
+            setValue("address.billing", cachedBilling.current);
+        }
+    }, [getValues, setValue, billingIsDelivery, currentDeliveryValues]);
+
+    return (
+        <form
+            className={styles["form"]}
+            aria-label="checkout shipping"
+            onSubmit={onSubmit && handleSubmit(onSubmit)}
+            noValidate
+        >
+            <fieldset>
+                <legend className={styles["legend"]}>Delivery address</legend>
+
+                <div className={styles["fields-container"]}>
+                    <Controller
+                        control={control}
+                        name="address.delivery.line1"
+                        render={({ field }) => {
+                            return (
+                                <TextInput
+                                    {...field}
+                                    {...inputProps}
+                                    value={field.value ?? ""}
+                                    label="Line 1"
+                                    autoComplete="delivery address-line1"
+                                    required
+                                    error={getError("address.delivery.line1")}
+                                    disabled={userAwaiting || cartAwaiting}
+                                />
+                            );
+                        }}
+                    />
+
+                    <Controller
+                        control={control}
+                        name="address.delivery.line2"
+                        render={({ field }) => {
+                            return (
+                                <TextInput
+                                    {...field}
+                                    {...inputProps}
+                                    value={field.value ?? ""}
+                                    label="Line 2"
+                                    autoComplete="delivery address-line2"
+                                    error={getError("address.delivery.line2")}
+                                    disabled={userAwaiting || cartAwaiting}
+                                />
+                            );
+                        }}
+                    />
+
+                    <Controller
+                        control={control}
+                        name="address.delivery.townCity"
+                        render={({ field }) => {
+                            return (
+                                <TextInput
+                                    {...field}
+                                    {...inputProps}
+                                    value={field.value ?? ""}
+                                    label="Town/City"
+                                    autoComplete="delivery address-level2"
+                                    required
+                                    error={getError("address.delivery.townCity")}
+                                    disabled={userAwaiting || cartAwaiting}
+                                />
+                            );
+                        }}
+                    />
+
+                    <Controller
+                        control={control}
+                        name="address.delivery.county"
+                        render={({ field }) => {
+                            return (
+                                <TextInput
+                                    {...field}
+                                    {...inputProps}
+                                    value={field.value ?? ""}
+                                    label="County"
+                                    error={getError("address.delivery.county")}
+                                    disabled={userAwaiting || cartAwaiting}
+                                />
+                            );
+                        }}
+                    />
+
+                    <Controller
+                        control={control}
+                        name="address.delivery.postcode"
+                        render={({ field }) => {
+                            return (
+                                <TextInput
+                                    {...field}
+                                    {...inputProps}
+                                    value={field.value ?? ""}
+                                    label="Postcode"
+                                    autoComplete="delivery postal-code"
+                                    required
+                                    error={getError("address.delivery.postcode")}
+                                    disabled={userAwaiting || cartAwaiting}
+                                />
+                            );
+                        }}
+                    />
+                </div>
+            </fieldset>
+
+            <Checkbox
+                label="My billing address is the same as my delivery address"
+                checked={billingIsDelivery}
+                onChange={() => setBillingIsDelivery(!billingIsDelivery)}
+                classNames={{ input: styles["checkbox-input"], label: styles["checkbox-label"] }}
+            />
+
+            <Collapse in={!billingIsDelivery} animateOpacity={false}>
+                <fieldset>
+                    <legend className={styles["legend"]}>Billing address</legend>
+
+                    <div className={styles["fields-container"]}>
+                        <Controller
+                            control={control}
+                            name="address.billing.line1"
+                            render={({ field }) => {
+                                return (
+                                    <TextInput
+                                        {...field}
+                                        {...inputProps}
+                                        value={field.value ?? ""}
+                                        label="Line 1"
+                                        autoComplete="billing address-line1"
+                                        required
+                                        error={getError("address.billing.line1")}
+                                        disabled={userAwaiting || cartAwaiting || billingIsDelivery}
+                                    />
+                                );
+                            }}
+                        />
+
+                        <Controller
+                            control={control}
+                            name="address.billing.line2"
+                            render={({ field }) => {
+                                return (
+                                    <TextInput
+                                        {...field}
+                                        {...inputProps}
+                                        value={field.value ?? ""}
+                                        label="Line 2"
+                                        autoComplete="billing address-line2"
+                                        error={getError("address.billing.line2")}
+                                        disabled={userAwaiting || cartAwaiting || billingIsDelivery}
+                                    />
+                                );
+                            }}
+                        />
+
+                        <Controller
+                            control={control}
+                            name="address.billing.townCity"
+                            render={({ field }) => {
+                                return (
+                                    <TextInput
+                                        {...field}
+                                        {...inputProps}
+                                        value={field.value ?? ""}
+                                        label="Town/City"
+                                        autoComplete="billing address-level2"
+                                        required
+                                        error={getError("address.billing.townCity")}
+                                        disabled={userAwaiting || cartAwaiting || billingIsDelivery}
+                                    />
+                                );
+                            }}
+                        />
+
+                        <Controller
+                            control={control}
+                            name="address.billing.county"
+                            render={({ field }) => {
+                                return (
+                                    <TextInput
+                                        {...field}
+                                        {...inputProps}
+                                        value={field.value ?? ""}
+                                        label="County"
+                                        error={getError("address.billing.county")}
+                                        disabled={userAwaiting || cartAwaiting || billingIsDelivery}
+                                    />
+                                );
+                            }}
+                        />
+
+                        <Controller
+                            control={control}
+                            name="address.billing.postcode"
+                            render={({ field }) => {
+                                return (
+                                    <TextInput
+                                        {...field}
+                                        {...inputProps}
+                                        value={field.value ?? ""}
+                                        label="Postcode"
+                                        autoComplete="billing postal-code"
+                                        required
+                                        error={getError("address.billing.postcode")}
+                                        disabled={userAwaiting || cartAwaiting || billingIsDelivery}
+                                    />
+                                );
+                            }}
+                        />
+                    </div>
+                </fieldset>
+            </Collapse>
+
+            <div className={styles["button-container"]}>
+                <Button
+                    type="button"
+                    color="rgb(48, 48, 48)"
+                    variant="filled"
+                    radius={9999}
+                    onClick={() => onReturn && onReturn()}
+                    disabled={userAwaiting || cartAwaiting}
+                    className={styles["previous-stage-button"]}
+                >
+                    Return to personal
+                </Button>
+
+                <Button
+                    type="submit"
+                    color="rgb(48, 48, 48)"
+                    variant="filled"
+                    radius={9999}
+                    disabled={userAwaiting || cartAwaiting}
+                    className={styles["next-stage-button"]}
+                >
+                    Continue to payment
+                </Button>
+            </div>
+        </form>
+    );
+}
