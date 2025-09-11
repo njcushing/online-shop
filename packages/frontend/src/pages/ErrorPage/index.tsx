de5c3fb3fcac2f8b85fb1f97fb2562a2f8915812
njcushing <129661299+njcushing@users.noreply.github.com>
2025-09-11 11:43:27 +0100

    Design ErrorPage component

--- packages/frontend/src/pages/ErrorPage/index.tsx
+++ packages/frontend/src/pages/ErrorPage/index.tsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
+import { Header } from "@/features/Header";
+import { Footer } from "@/features/Footer";
 import styles from "./index.module.css";
 
 /* Breaking normal naming convention to prevent clash with Error API */
 export function ErrorPage() {
     return (
-        <div className={styles["page"]}>
-            <div className={styles["page-content"]}>
-                <h1>{`Oh no! There doesn't seem to be anything here`}</h1>
-                <Link to="/">Return to home</Link>
+        <div className={styles["error-page"]}>
+            <Header disableActivity reduced />
+            <div className={styles["error-page-width-controller"]}>
+                <h1
+                    className={styles["header"]}
+                >{`Oh no! There doesn't seem to be anything here`}</h1>
+                <Link to="/" className={styles["link"]}>
+                    Return to home
+                </Link>
             </div>
+            <Footer reduced />
         </div>
     );
 }
