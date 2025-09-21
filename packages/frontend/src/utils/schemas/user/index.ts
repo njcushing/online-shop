89d95f0056ad30e9108e37b634355cd97961d76c
njcushing <129661299+njcushing@users.noreply.github.com>
2025-09-21 18:24:51 +0100

    Add 'consent' field to User type

--- packages/frontend/src/utils/schemas/user/index.ts
+++ packages/frontend/src/utils/schemas/user/index.ts
@@ -1,3 +1,4 @@
+import { Consent } from "@/utils/schemas/consent";
 import { Cart, mockCart } from "@/utils/products/cart";
 import { ProductVariant } from "@/utils/products/product";
 import { OrderDataBase } from "@/utils/products/orders";
@@ -5,6 +6,7 @@ import { SubscriptionDataBase } from "@/utils/products/subscriptions";
 import { defaultProfile, Profile } from "../profile";
 
 export type User = {
+    consent: Consent;
     profile: Profile;
     cart: Cart;
     watchlist: ProductVariant["id"][];
@@ -13,6 +15,7 @@ export type User = {
 };
 
 export const defaultUser: User = {
+    consent: { cookies: false, timestamp: null },
     profile: defaultProfile,
     cart: mockCart,
     watchlist: ["1-1", "1-2", "2-2", "3-1", "3-3"],
