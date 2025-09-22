d45dcf9e322461ccda4dbb1bfa58c3a09b760514
njcushing <129661299+njcushing@users.noreply.github.com>
2025-09-22 21:05:18 +0100

    Adjust position, size and colour for ConsentBanner component

--- packages/frontend/src/features/ConsentBanner/index.tsx
+++ packages/frontend/src/features/ConsentBanner/index.tsx
@@ -2,5 +2,20 @@ import { Modal } from "@mantine/core";
 import styles from "./index.module.css";
 
 export function ConsentBanner() {
-    return <Modal opened onClose={() => {}} className={styles["consent-banner"]}></Modal>;
+    return (
+        <Modal
+            opened
+            size="100%"
+            withCloseButton={false}
+            closeOnClickOutside={false}
+            onClose={() => {}}
+            classNames={{
+                inner: styles["modal-inner"],
+                content: styles["modal-content"],
+                header: styles["modal-header"],
+                body: styles["modal-body"],
+                close: styles["modal-close"],
+            }}
+        ></Modal>
+    );
 }
