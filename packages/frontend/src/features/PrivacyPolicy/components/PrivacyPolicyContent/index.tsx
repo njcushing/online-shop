c048639e117d5db0a2690809edf449a5b7592baa
njcushing <129661299+njcushing@users.noreply.github.com>
2025-09-23 19:38:04 +0100

    Add header and last updated date to PrivacyPolicyContent component

--- packages/frontend/src/features/PrivacyPolicy/components/PrivacyPolicyContent/index.tsx
+++ packages/frontend/src/features/PrivacyPolicy/components/PrivacyPolicyContent/index.tsx
@@ -1,5 +1,15 @@
 import styles from "./index.module.css";
 
 export function PrivacyPolicyContent() {
-    return <section className={styles["privacy-policy-content"]}></section>;
+    return (
+        <section className={styles["privacy-policy-content"]}>
+            <div className={styles["privacy-policy-content-width-controller"]}>
+                <h1 className={styles["header"]}>Privacy & Cookie Policy</h1>
+
+                <p className={styles["last-updated"]}>
+                    Last updated: <strong>September 2025</strong>
+                </p>
+            </div>
+        </section>
+    );
 }
