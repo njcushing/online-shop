b7489014e4385af2bfe8d53ab0b481b2f42c56bd
njcushing <129661299+njcushing@users.noreply.github.com>
2025-09-23 22:50:26 +0100

    Add Accordion with multiple items to PrivacyPolicyContent component

--- packages/frontend/src/features/PrivacyPolicy/components/PrivacyPolicyContent/index.tsx
+++ packages/frontend/src/features/PrivacyPolicy/components/PrivacyPolicyContent/index.tsx
@@ -1,4 +1,9 @@
+import { Divider, Accordion, AccordionPanelProps } from "@mantine/core";
 import styles from "./index.module.css";
+
+const defaultAccordionPanelProps: AccordionPanelProps = {
+    style: { opacity: 1 }, // Override default opacity transition
+};
 
 export function PrivacyPolicyContent() {
     return (
@@ -9,6 +14,61 @@ export function PrivacyPolicyContent() {
                 <p className={styles["last-updated"]}>
                     Last updated: <strong>September 2025</strong>
                 </p>
+
+                <Accordion
+                    classNames={{
+                        root: styles["accordion-root"],
+                        item: styles["accordion-item"],
+                        control: styles["accordion-control"],
+                        content: styles["accordion-content"],
+                        label: styles["accordion-label"],
+                        panel: styles["accordion-panel"],
+                    }}
+                >
+                    <Divider className={styles["divider"]} />
+
+                    <Accordion.Item value="introduction">
+                        <Accordion.Control>Introduction</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="your-rights">
+                        <Accordion.Control>Your Rights</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="cookie-policy">
+                        <Accordion.Control>Cookie Policy</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="data-collection-and-usage">
+                        <Accordion.Control>Data Collection and Usage</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="data-storage">
+                        <Accordion.Control>Data Storage</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="data-security">
+                        <Accordion.Control>Data Security</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+
+                    <Accordion.Item value="getting-in-touch">
+                        <Accordion.Control>Getting in Touch</Accordion.Control>
+
+                        <Accordion.Panel {...defaultAccordionPanelProps}></Accordion.Panel>
+                    </Accordion.Item>
+                </Accordion>
             </div>
         </section>
     );
