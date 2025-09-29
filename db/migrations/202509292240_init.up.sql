d56fed65dffc4cc73f0a4c0fd02f182c751ab374
njcushing <129661299+njcushing@users.noreply.github.com>
2025-09-29 22:52:16 +0100

    Set up initial database migration files for creating products table

---
+++ db/migrations/202509292240_init.up.sql
@@ -0,0 +1,11 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE products (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    description TEXT,
+    slug TEXT NOT NULL UNIQUE,
+    allowance INT NOT NULL DEFAULT 0,
+    tags TEXT[] DEFAULT '{}',
+    release_date TIMESTAMPTZ
+);
