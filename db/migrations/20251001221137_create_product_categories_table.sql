ad09986fb608036ab84ab67f82d36e415640dc05
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-01 23:12:41 +0100

    Add database migration for creating 'product_categories' table

---
+++ db/migrations/20251001221137_create_product_categories_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE product_categories (
+    product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    category_id UUID NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+    PRIMARY KEY (product_id, category_id)
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS product_categories;
+
