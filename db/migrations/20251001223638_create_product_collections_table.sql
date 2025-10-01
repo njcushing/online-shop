4a0c228c3c3c137de945393f38a057991204f0f9
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-01 23:37:27 +0100

    Add database migration for creating 'product_collections' table

---
+++ db/migrations/20251001223638_create_product_collections_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE product_collections (
+    collection_id UUID NOT NULL REFERENCES collections(id) ON DELETE CASCADE,
+    product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    PRIMARY KEY (collection_id, product_id)
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS product_collections;
+
