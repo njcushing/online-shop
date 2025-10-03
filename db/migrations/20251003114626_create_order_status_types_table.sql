8d2d3fa14ccc7b17e0a2fc729e1e363104792c11
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-03 23:26:44 +0100

    Add database migration for creating 'order_status_types' table

---
+++ db/migrations/20251003114626_create_order_status_types_table.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+CREATE TABLE order_status_types (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text NOT NULL UNIQUE,
+    label text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS order_status_types;
