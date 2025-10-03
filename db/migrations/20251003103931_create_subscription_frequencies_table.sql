d529107c9f08771dcbd3bdf9e2c0aa57ce25d0c7
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-03 12:35:51 +0100

    Add database migration for creating 'subscription_frequencies' table

---
+++ db/migrations/20251003103931_create_subscription_frequencies_table.sql
@@ -0,0 +1,11 @@
+-- migrate:up
+CREATE TABLE subscription_frequencies (
+    id uuid PRIMARY LEY DEFAULT uuid_generate_v4(),
+    code text NOT NULL UNIQUE,
+    label text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS subscription_frequencies;
