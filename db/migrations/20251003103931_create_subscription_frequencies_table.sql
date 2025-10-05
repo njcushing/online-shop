52b2dd468b7fb0dd625907abcfbb339e9e2cba07
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-05 19:52:37 +0100

    Fix incorrect keyword spelling in database migration file

--- db/migrations/20251003103931_create_subscription_frequencies_table.sql
+++ db/migrations/20251003103931_create_subscription_frequencies_table.sql
@@ -1,6 +1,6 @@
 -- migrate:up
 CREATE TABLE subscription_frequencies (
-    id uuid PRIMARY LEY DEFAULT uuid_generate_v4(),
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     code text NOT NULL UNIQUE,
     label text NOT NULL,
     created_at timestamptz NOT NULL DEFAULT now(),
