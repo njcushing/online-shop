42c3b15755039e80068bc117f69cf800fcdedf5c
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-05 19:50:11 +0100

    Add database migration for creating 'order_addresses' table

--- db/migrations/20251005180142_create_order_addresses_table.sql
+++ db/migrations/20251005180142_create_order_addresses_table.sql
@@ -6,6 +6,9 @@
  * wrong. Addresses for orders must be immutable, and having this separate table for addresses
  * associated with orders seems better than having some sort of 'immutable' field set to 'true' for
  * addresses associated with orders in the 'addresses' table to prevent their mutation.
+ *
+ * Postcode RegEx from:
+ * https://stackoverflow.com/questions/164979/regex-for-matching-uk-postcodes
  */
 
 -- migrate:up
@@ -17,9 +20,7 @@ CREATE TABLE order_addresses (
     line2 text,
     town_city text NOT NULL CHECK (char_length(town_city) >= 1),
     county text,
-    postcode text NOT NULL CHECK (
-        postcode ~ '^([Gg][Ii][Rr]0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})$'
-    ),
+    postcode text NOT NULL CHECK (postcode ~ '^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$'),
     created_at timestamptz NOT NULL DEFAULT now(),
     UNIQUE (order_id, address_type_id)
 );
