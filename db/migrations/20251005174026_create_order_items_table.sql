41ab11aa1d6387a193100c89c79730222fc931c8
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-05 19:29:14 +0100

    Add database migration for creating 'order_items' table

---
+++ db/migrations/20251005174026_create_order_items_table.sql
@@ -0,0 +1,18 @@
+-- migrate:up
+CREATE TABLE order_items (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    order_id uuid NOT NULL REFERENCES orders(id) ON DELETE RESTRICT,
+    product_variant_id uuid NOT NULL REFERENCES product_variants(id) ON DELETE RESTRICT,
+    unit_price_base numeric(10,2) NOT NULL,
+    unit_price_paid numeric(10,2) NOT NULL,
+    quantity int NOT NULL CHECK (quantity > 0),
+    subscription_frequency_id REFERENCES subscription_frequencies(id) ON DELETE RESTRICT,
+    subscription_discount numeric(10,2),
+    created_at timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_order_items_order_id ON order_items(order_id);
+
+-- migrate:down
+DROP TABLE IF EXISTS order_items;
+
