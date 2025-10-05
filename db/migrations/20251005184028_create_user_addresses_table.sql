d8b3565b48e3ca68a0b8fe9c54917ae8e0501eee
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-05 19:50:32 +0100

    Add database migration for creating 'user_addresses' table

---
+++ db/migrations/20251005184028_create_user_addresses_table.sql
@@ -0,0 +1,22 @@
+/**
+ * Postcode RegEx from:
+ * https://stackoverflow.com/questions/164979/regex-for-matching-uk-postcodes
+ */
+
+ -- migrate:up
+CREATE TABLE user_addresses (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id uuid NOT NULL REFERENCES users(id) ON DELETE RESTRICT,
+    address_type_id uuid NOT NULL REFERENCES address_types(id) ON DELETE RESTRICT,
+    line1 text NOT NULL CHECK (char_length(line1) >= 1),
+    line2 text,
+    town_city text NOT NULL CHECK (char_length(town_city) >= 1),
+    county text,
+    postcode text NOT NULL CHECK (postcode ~ '^([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})$'),
+    created_at timestamptz NOT NULL DEFAULT now(),
+    updated_at timestamptz
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS user_addresses;
+
