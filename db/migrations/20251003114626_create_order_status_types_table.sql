4b382114027af7c0ec1a5aa774ff95b7d8f9a432
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-07 19:57:49 +0100

    Remove 'NOT NULL' from tables' 'label' columns
    Again, I'm just editing the original migrations' files here as I haven't yet seeded any data into the development database and I can just reapply each migration from the start.

--- db/migrations/20251003114626_create_order_status_types_table.sql
+++ db/migrations/20251003114626_create_order_status_types_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE order_status_types (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name text NOT NULL UNIQUE,
-    label text NOT NULL,
+    label text,
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz
 );
