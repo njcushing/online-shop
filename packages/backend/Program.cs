1bfe41faa6c7bfb19a2ef0161b27b05cfc43afdf
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-09 12:26:11 +0100

    Enable CORS for localhost port running client

--- packages/backend/Program.cs
+++ packages/backend/Program.cs
@@ -1,22 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using Cafree.Api.Data;
 
+var AllowSpecificOrigins = "";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowSpecificOrigins, builder =>
+    {
+        builder.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.MapControllers();
+app.UseCors(AllowSpecificOrigins);
 
+app.MapControllers();
 app.MapFallbackToFile("index.html");
 
 app.Run();
