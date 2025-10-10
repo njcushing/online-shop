0d6e76775467f0171d1387b69bc4aea3e975bef9
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-10 12:35:58 +0100

    Enable support for non-nullable reference types in Swagger gen

--- packages/backend/Program.cs
+++ packages/backend/Program.cs
@@ -6,7 +6,7 @@ var AllowSpecificOrigins = "";
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SupportNonNullableReferenceTypes());
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
