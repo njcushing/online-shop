fb72f1e05ad618efc94853e82974aff8698b4e8d
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-14 19:03:49 +0100

    Write migration for creating 'settings' table schema

---
+++ db/migrations/20251014184905_create_settings_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE settings (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    base_express_delivery_cost numeric(10,2) NOT NULL,
+    free_express_delivery_threshold numeric(10,2) NOT NULL,
+    updated_at timestamptz NOT NULL DEFAULT now()
+);
+
+-- migrate:down
+DROP TABLE IF EXISTS settings;
