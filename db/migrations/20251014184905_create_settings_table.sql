948107df5638fa5d6fb8bbe29eaf3d17bf4d7d43
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-16 17:58:39 +0100

    Adjust migration file for creating 'settings' table
    To add a 'low_stock_threshold' int field. Again, I'm changing the original migration file instead of writing a new one because I can just rebuild the database at this stage, and it's more convenient for such a small adjustment.

--- db/migrations/20251014184905_create_settings_table.sql
+++ db/migrations/20251014184905_create_settings_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE settings (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     base_express_delivery_cost numeric(10,2) NOT NULL,
     free_express_delivery_threshold numeric(10,2) NOT NULL,
+    low_stock_threshold int NOT NULL,
     updated_at timestamptz NOT NULL DEFAULT now()
 );
 
