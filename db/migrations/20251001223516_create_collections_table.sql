ef2f3d4465a0bd897f951c3ddfe20ac976ea803a
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-24 12:12:39 +0100

    Add unique 'slug' field to 'collections' table in database
    I originally had the 'name' field as unique but as I'm considering adding pages for collections, they'll need unique slugs for the URL path anyway, and this approach enforces consistency with categories and products.

--- db/migrations/20251001223516_create_collections_table.sql
+++ db/migrations/20251001223516_create_collections_table.sql
@@ -3,9 +3,9 @@ CREATE TABLE collections (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name text NOT NULL,
     description text,
+    slug text NOT NULL UNIQUE,
     created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz,
-    UNIQUE (name)
+    updated_at timestamptz
 );
 
 -- migrate:down
