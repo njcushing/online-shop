8165e0ef2558f23036f83f6ccd5519d9871af3fd
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-24 12:55:40 +0100

    Remove unnecessary code comment from seed file

--- db/seeds/009_product_categories.sql
+++ db/seeds/009_product_categories.sql
@@ -1,16 +1,3 @@
-/*
--- migrate:up
-CREATE TABLE product_categories (
-    product_id uuid NOT NULL REFERENCES products(id) ON DELETE CASCADE,
-    category_id uuid NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
-    created_at timestamptz NOT NULL DEFAULT now(),
-    updated_at timestamptz,
-    PRIMARY KEY (product_id, category_id)
-);
-
--- migrate:down
-DROP TABLE IF EXISTS product_categories;
-*/
 BEGIN;
 
 INSERT INTO product_categories
