6c2fa9e3a2266e81dc47692c4effc54684babdfe
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-25 15:30:12 +0100

    Add more 'Blend' attribute values to database

--- db/seeds/014_product_attribute_values.sql
+++ db/seeds/014_product_attribute_values.sql
@@ -1,8 +1,19 @@
 BEGIN;
 
 INSERT INTO product_attribute_values (product_attribute_id, code, name) VALUES
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'HO', 'House'),
     ((SELECT id FROM product_attributes WHERE name='Blend'), 'LT', 'Light'),
     ((SELECT id FROM product_attributes WHERE name='Blend'), 'MD', 'Medium'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'DK', 'Dark');
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'DK', 'Dark'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'XD', 'Extra Dark'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'BK', 'Breakfast'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'PK', 'Pumpkin Spice'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'MO', 'Mocha'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'ES', 'Espresso'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'VA', 'Vanilla'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'CA', 'Caramel'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'IN', 'Intense'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'FR', 'French Roast'),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'IR', 'Italian Roast');
 
 COMMIT;
