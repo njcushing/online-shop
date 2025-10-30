6a993474ae61e11b72fb974eb723c58e5af1f5f5
njcushing <129661299+njcushing@users.noreply.github.com>
2025-10-30 11:32:15 +0000

    Update getProductBySlug API query function to use API

--- packages/frontend/src/api/product/index.ts
+++ packages/frontend/src/api/product/index.ts
@@ -1,23 +1,40 @@
-import { Product } from "@/utils/products/product";
 import * as HTTPMethodTypes from "../types";
+import { paths, components } from "../schema";
 import { fetcher } from "../utils/fetcher";
 
-export const getProduct: HTTPMethodTypes.GET<{ productId?: string }, Product> = async (data) => {
-    const { productId } = data.params || { productId: null };
-    if (!productId) return { status: 400, message: "No product id provided for query", data: null };
+type RequestParams = {
+    slug: components["schemas"]["Products._Slug.GET.GetProductBySlugResponseDto"]["slug"];
+};
 
-    const result = await fetcher<Product>(
-        `${import.meta.env.VITE_SERVER_DOMAIN}/api/product/${productId}`,
-        {
-            signal: data.abortController ? data.abortController.signal : null,
-            method: "GET",
-            mode: "cors",
-            headers: {
-                Authorization:
-                    localStorage.getItem(import.meta.env.VITE_TOKEN_LOCAL_LOCATION) || "",
-            },
-        },
-    );
+const endpoint = "/api/products/{slug}";
+const method = "get";
+const code = 200;
+const contentType = "application/json";
+type Responses = paths[typeof endpoint][typeof method]["responses"];
+type ResponseBody = Responses[typeof code]["content"][typeof contentType];
+
+export const getProductBySlug: HTTPMethodTypes.GET<RequestParams, ResponseBody> = async (data) => {
+    const { params } = data;
+
+    if (!params || !params.slug) {
+        return {
+            success: false as const,
+            status: 400,
+            message: "Could not make request: no product slug provided.",
+            error: undefined,
+        };
+    }
+
+    const { slug } = params;
+
+    const apiUrl = import.meta.env.VITE_API_BASE_URL || "/api";
+
+    const result = await fetcher<ResponseBody>(`${apiUrl}/products/${slug}`, {
+        signal: data.abortController ? data.abortController.signal : null,
+        method: method.toUpperCase(),
+        mode: "cors",
+        headers: { "Content-Type": contentType },
+    });
 
     return result;
 };
