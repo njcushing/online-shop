1c060fec7236b8691d93b25caf55c88c5d63c454
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-01 12:06:52 +0000

    Add 'position' int field to 'product_attribute_values' table

--- db/migrations/20251101115400_create_product_attribute_values_table.sql
+++ db/migrations/20251101115400_create_product_attribute_values_table.sql
@@ -3,9 +3,11 @@ CREATE TABLE product_attribute_values (
     product_attribute_id uuid NOT NULL REFERENCES product_attributes(id) ON DELETE CASCADE,
     code text NOT NULL,
     name text NOT NULL,
+    position int NOT NULL CHECK (position >= 0),
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz,
-    PRIMARY KEY (product_attribute_id, code)
+    PRIMARY KEY (product_attribute_id, code),
+    UNIQUE (product_attribute_id, position)
 );
 
 -- migrate:down
--- db/seeds/015_product_attribute_values.sql
+++ db/seeds/015_product_attribute_values.sql
@@ -1,19 +1,19 @@
 BEGIN;
 
-INSERT INTO product_attribute_values (product_attribute_id, code, name) VALUES
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'HO', 'House'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'LT', 'Light'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'MD', 'Medium'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'DK', 'Dark'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'XD', 'Extra Dark'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'BK', 'Breakfast'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'PK', 'Pumpkin Spice'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'MO', 'Mocha'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'ES', 'Espresso'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'VA', 'Vanilla'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'CA', 'Caramel'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'IN', 'Intense'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'FR', 'French Roast'),
-    ((SELECT id FROM product_attributes WHERE name='Blend'), 'IR', 'Italian Roast');
+INSERT INTO product_attribute_values (product_attribute_id, code, name, position) VALUES
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'HO', 'House', 0),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'LT', 'Light', 1),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'MD', 'Medium', 2),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'DK', 'Dark', 3),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'XD', 'Extra Dark', 4),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'BK', 'Breakfast', 5),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'PK', 'Pumpkin Spice', 6),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'MO', 'Mocha', 7),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'ES', 'Espresso', 8),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'VA', 'Vanilla', 9),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'CA', 'Caramel', 10),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'IN', 'Intense', 11),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'FR', 'French Roast', 12),
+    ((SELECT id FROM product_attributes WHERE name='Blend'), 'IR', 'Italian Roast', 13);
 
 COMMIT;
