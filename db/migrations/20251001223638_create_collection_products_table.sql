709d49ef8f6cdf410d01240a5e3496e33232d373
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-08 18:45:04 +0000

    Add 'position' field to 'collection_products' database table

--- db/migrations/20251001223638_create_collection_products_table.sql
+++ db/migrations/20251001223638_create_collection_products_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE collection_products (
     collection_id uuid NOT NULL REFERENCES collections(id) ON DELETE CASCADE,
     product_id uuid NOT NULL REFERENCES products(id) ON DELETE CASCADE,
     name text,
+    position int NOT NULL CHECK (position >= 0),
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz,
     PRIMARY KEY (collection_id, product_id)
--- db/seeds/012_collection_products.sql
+++ db/seeds/012_collection_products.sql
@@ -1,20 +1,23 @@
 BEGIN;
 
-INSERT INTO collection_products (collection_id, product_id, name) VALUES
+INSERT INTO collection_products (collection_id, product_id, name, position) VALUES
     (
         (SELECT id FROM collections WHERE slug='coffee-whole-bean'),
         (SELECT id FROM products WHERE slug='coffee-whole-bean-250g'),
-        '250g bag'
+        '250g bag',
+        0
     ),
     (
         (SELECT id FROM collections WHERE slug='coffee-whole-bean'),
         (SELECT id FROM products WHERE slug='coffee-whole-bean-500g'),
-        '500g bag'
+        '500g bag',
+        1
     ),
     (
         (SELECT id FROM collections WHERE slug='coffee-whole-bean'),
         (SELECT id FROM products WHERE slug='coffee-whole-bean-1kg'),
-        '1kg bag'
+        '1kg bag',
+        2
     );
 
 COMMIT;
