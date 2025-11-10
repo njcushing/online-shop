46d1fd07eb621096fd91c2e5a68263dfe5e0c232
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-10 23:26:24 +0000

    Replace getCategoryByName API query function with getCategoryBySlug

--- packages/frontend/src/api/category/index.ts
+++ packages/frontend/src/api/category/index.ts
@@ -3,33 +3,33 @@ import { paths, components } from "../schema";
 import { fetcher } from "../utils/fetcher";
 
 type RequestParams = {
-    name: components["schemas"]["Categories._Name.GET.GetCategoryByNameResponseDto"]["name"];
+    slug: components["schemas"]["Categories._Slug.GET.GetCategoryBySlugResponseDto"]["slug"];
 };
 
-const endpoint = "/api/categories/{name}";
+const endpoint = "/api/categories/{slug}";
 const method = "get";
 const code = 200;
 const contentType = "application/json";
 type Responses = paths[typeof endpoint][typeof method]["responses"];
 export type ResponseBody = Responses[typeof code]["content"][typeof contentType];
 
-export const getCategoryByName: HTTPMethodTypes.GET<RequestParams, ResponseBody> = async (data) => {
+export const getCategoryBySlug: HTTPMethodTypes.GET<RequestParams, ResponseBody> = async (data) => {
     const { params } = data;
 
-    if (!params || !params.name) {
+    if (!params || !params.slug) {
         return {
             success: false as const,
             status: 400,
-            message: "Could not make request: no category name provided.",
+            message: "Could not make request: no category slug provided.",
             error: undefined,
         };
     }
 
-    const { name } = params;
+    const { slug } = params;
 
     const apiUrl = import.meta.env.VITE_API_BASE_URL || "/api";
 
-    const result = await fetcher<ResponseBody>(`${apiUrl}/categories/${name}`, {
+    const result = await fetcher<ResponseBody>(`${apiUrl}/categories/${slug}`, {
         signal: data.abortController ? data.abortController.signal : null,
         method: method.toUpperCase(),
         mode: "cors",
