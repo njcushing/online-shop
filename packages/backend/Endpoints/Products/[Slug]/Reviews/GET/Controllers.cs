a820d6bf15d03cff6ac8bf4dea067e92f2a445d1
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-10 23:06:28 +0000

    Create /api/products/{slug}/reviews endpoint

---
+++ packages/backend/Endpoints/Products/[Slug]/Reviews/GET/Controllers.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cafree.Api.Data;
+
+namespace Cafree.Api.Endpoints.Products._Slug.Reviews.GET
+{
+    [ApiController]
+    [Route("api/products/{slug}/reviews")]
+    public class GetReviewsByProductSlugController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<GetReviewsByProductSlugResponseDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProductBySlugReviews(string slug, [FromQuery] GetReviewsByProductSlugRequestDto query)
+        {
+            query.PageSize ??= 10;
+
+            var product = await _context.Products
+                .AnyAsync(p => p.Active && p.Slug == slug);
+
+            if (!product) return Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Product not found",
+                detail: $"No product with the specified slug '{slug}' could be located."
+            );
+
+            var reviewQuery = _context.ProductReviews
+                .Where(pr => pr.Product.Slug == slug)
+                .AsNoTracking();
+
+            reviewQuery = query.Filter switch
+            {
+                "rating_5" => reviewQuery.Where(pr => pr.Rating == 5),
+                "rating_4" => reviewQuery.Where(pr => pr.Rating == 4),
+                "rating_3" => reviewQuery.Where(pr => pr.Rating == 3),
+                "rating_2" => reviewQuery.Where(pr => pr.Rating == 2),
+                "rating_1" => reviewQuery.Where(pr => pr.Rating == 1),
+                _ => reviewQuery,
+            };
+
+            reviewQuery = query.Sort switch
+            {
+                "rating_desc" => reviewQuery.OrderByDescending(pr => pr.Rating),
+                "rating_asc" => reviewQuery.OrderBy(pr => pr.Rating),
+                "created_asc" => reviewQuery.OrderBy(pr => pr.CreatedAt),
+                "created_desc" or _ => reviewQuery.OrderByDescending(pr => pr.CreatedAt),
+            };
+
+            int pageSize = query.PageSize ?? 10;
+
+            var reviews = await reviewQuery
+                .Skip((query.Page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(pr => new GetReviewsByProductSlugResponseDto
+                {
+                    Title = pr.Title,
+                    Description = pr.Description,
+                    Rating = pr.Rating,
+                    CreatedAt = pr.CreatedAt,
+                    UpdatedAt = pr.UpdatedAt,
+                    Variant = pr.ProductVariant == null
+                        ? null
+                        : new GetReviewsByProductSlugResponseDto.ProductVariant
+                        {
+                            Name = pr.ProductVariant.Name,
+                            Sku = pr.ProductVariant.Sku,
+                        }
+
+                })
+                .ToListAsync();
+
+            if (reviews.Count == 0) return Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Reviews not found",
+                detail: $"No reviews for the product with the specified slug '{slug}' could be located."
+            );
+
+            return Ok(reviews);
+        }
+    }
+}
---
+++ packages/backend/Endpoints/Products/[Slug]/Reviews/GET/Dtos.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace Cafree.Api.Endpoints.Products._Slug.Reviews.GET
+{
+    public class GetReviewsByProductSlugRequestDto
+    {
+        [FromQuery(Name = "page")]
+        public required int Page { get; set; }
+
+        [FromQuery(Name = "pageSize")]
+        public int? PageSize { get; set; }
+
+        [FromQuery(Name = "sort")]
+        public string? Sort { get; set; }
+
+        [FromQuery(Name = "filter")]
+        public string? Filter { get; set; }
+    }
+
+    public class GetReviewsByProductSlugResponseDto
+    {
+        public class ProductVariant
+        {
+            [JsonIgnore]
+            public Guid Id { get; set; }
+
+            [JsonIgnore]
+            public Guid ProductId { get; set; }
+
+            public required string Name { get; set; }
+
+            public required string Sku { get; set; }
+        }
+
+        [JsonIgnore]
+        public Guid Id { get; set; }
+
+        [JsonIgnore]
+        public Guid ProductId { get; set; }
+
+        [JsonIgnore]
+        public Guid? ProductVariantId { get; set; }
+
+        public string? Title { get; set; }
+
+        public required string Description { get; set; }
+
+        public short Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual ProductVariant? Variant { get; set; }
+    }
+}
