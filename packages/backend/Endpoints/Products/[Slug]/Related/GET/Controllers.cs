58154642d1eb3945a1c6bd4e53c805a74e883e7b
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-16 16:51:17 +0000

    Create /api/products/{slug}/related endpoint

---
+++ packages/backend/Endpoints/Products/[Slug]/Related/GET/Controllers.cs
@@ -0,0 +1,120 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cafree.Api.Data;
+
+namespace Cafree.Api.Endpoints.Products._Slug.Related.GET
+{
+    [ApiController]
+    [Route("api/products/{slug}/related")]
+    public class GetRelatedProductsBySlugController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<GetRelatedProductsBySlugResponseDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProductBySlugReviews(string slug)
+        {
+            var product = await _context.Products
+                .Include(p => p.CategoryProducts)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(p => p.Slug == slug);
+
+            if (product == null) return Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Product not found",
+                detail: $"No product with the specified slug '{slug}' could be located."
+            );
+
+            var categoryIds = product.CategoryProducts.Select(c => c.CategoryId).ToList();
+            var productTags = product.Tags ?? [];
+
+            var relatedProducts = await _context.Products
+                .Where(p => p.Slug != slug)
+                .Select(p => new GetRelatedProductsBySlugResponseDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Slug = p.Slug,
+                    Allowance = p.Allowance,
+                    Tags = p.Tags,
+                    ReleaseDate = p.ReleaseDate,
+                    Details = p.ProductDetails.Select(pi => new GetRelatedProductsBySlugResponseDto.Detail
+                    {
+                        Id = pi.Id,
+                        Name = pi.Name,
+                        Value = pi.Value,
+                    }).ToList(),
+                    Images = p.ProductImages.Select(pi => new GetRelatedProductsBySlugResponseDto.Image
+                    {
+                        Id = pi.Id,
+                        Src = pi.Src,
+                        Alt = pi.Alt,
+                        Position = pi.Position,
+                    }).ToList(),
+                    Rating = new GetRelatedProductsBySlugResponseDto.ProductRating
+                    {
+                        Average = p.ProductRating!.Average,
+                        Total = p.ProductRating.Total,
+                        Quantities = new GetRelatedProductsBySlugResponseDto.ProductRating.RatingQuantities
+                        {
+                            Rating5 = p.ProductRating.Rating5,
+                            Rating4 = p.ProductRating.Rating4,
+                            Rating3 = p.ProductRating.Rating3,
+                            Rating2 = p.ProductRating.Rating2,
+                            Rating1 = p.ProductRating.Rating1,
+                        }
+                    },
+                    Variants = p.ProductVariants.Select(pv => new GetRelatedProductsBySlugResponseDto.Variant
+                    {
+                        Id = pv.Id,
+                        Name = pv.Name,
+                        Sku = pv.Sku,
+                        CanSubscribe = pv.CanSubscribe,
+                        PriceCurrent = pv.PriceCurrent,
+                        PriceBase = pv.PriceBase,
+                        SubscriptionDiscountPercentage = pv.SubscriptionDiscountPercentage,
+                        Stock = pv.Stock,
+                        AllowanceOverride = pv.AllowanceOverride,
+                        Active = pv.Active,
+                        ReleaseDate = pv.ReleaseDate,
+                        Attributes = pv.ProductVariantAttributes.Select(pva => new GetRelatedProductsBySlugResponseDto.Variant.Attribute
+                        {
+                            Type = new GetRelatedProductsBySlugResponseDto.Variant.Attribute.AttributeType
+                            {
+                                Id = pva.ProductAttribute.Id,
+                                Name = pva.ProductAttribute.Name,
+                                Title = pva.ProductAttribute.Title,
+                            },
+                            Value = new GetRelatedProductsBySlugResponseDto.Variant.Attribute.AttributeValue
+                            {
+                                Code = pva.ProductAttributeValue.Code,
+                                Name = pva.ProductAttributeValue.Name,
+                                Position = pva.ProductAttributeValue.Position,
+                            },
+                        }).ToList(),
+                        Images = pv.ProductVariantImages.Select(pvi => new GetRelatedProductsBySlugResponseDto.Variant.Image
+                        {
+                            Id = pvi.Id,
+                            Src = pvi.Src,
+                            Alt = pvi.Alt,
+                            Position = pvi.Position,
+                        }).ToList(),
+                    }).ToList(),
+                    Score =
+                        5 * p.CategoryProducts.Count(c => categoryIds.Contains(c.CategoryId)) +
+                        3 * (p.Tags != null ? p.Tags.Count(tag => productTags.Contains(tag)) : 0) +
+                        EF.Functions.ToTsVector("english", p.SearchText ?? "")
+                            .Rank(EF.Functions.PlainToTsQuery(product.Name))
+                })
+                .OrderByDescending(p => p.Score)
+                .ThenBy(p => p.Name)
+                .Take(10)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(relatedProducts);
+        }
+    }
+}
---
+++ packages/backend/Endpoints/Products/[Slug]/Related/GET/Dtos.cs
@@ -0,0 +1,183 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace Cafree.Api.Endpoints.Products._Slug.Related.GET
+{
+    public class GetRelatedProductsBySlugRequestDto
+    {
+        [FromQuery(Name = "slug")]
+        public required string Slug { get; set; }
+    }
+
+    public class GetRelatedProductsBySlugResponseDto
+    {
+        public class Detail
+        {
+            public Guid Id { get; set; }
+
+            [JsonIgnore]
+            public Guid ProductId { get; set; }
+
+            public required string Name { get; set; }
+
+            public required string Value { get; set; }
+        }
+
+        public class Image
+        {
+            public Guid Id { get; set; }
+
+            [JsonIgnore]
+            public Guid ProductId { get; set; }
+
+            public required string Src { get; set; }
+
+            public required string Alt { get; set; }
+
+            public required int Position { get; set; }
+        }
+
+        public class ProductRating
+        {
+            public class RatingQuantities
+            {
+                [JsonPropertyName("5")] public int Rating5 { get; set; }
+
+                [JsonPropertyName("4")] public int Rating4 { get; set; }
+
+                [JsonPropertyName("3")] public int Rating3 { get; set; }
+
+                [JsonPropertyName("2")] public int Rating2 { get; set; }
+
+                [JsonPropertyName("1")] public int Rating1 { get; set; }
+            }
+
+            [JsonIgnore]
+            public Guid ProductId { get; set; }
+
+            public decimal Average { get; set; }
+
+            public int Total { get; set; }
+
+            public RatingQuantities Quantities { get; set; } = new();
+        }
+
+        public class Variant
+        {
+            public class Attribute
+            {
+                public class AttributeType
+                {
+                    public Guid Id { get; set; }
+
+                    public required string Name { get; set; }
+
+                    public required string Title { get; set; }
+                }
+
+                public class AttributeValue
+                {
+                    [JsonIgnore]
+                    public Guid ProductAttributeId { get; set; }
+
+                    public required string Code { get; set; }
+
+                    public required string Name { get; set; }
+
+                    public required int Position { get; set; }
+                }
+
+                [JsonIgnore]
+                public Guid ProductVariantId { get; set; }
+
+                [JsonIgnore]
+                public Guid ProductAttributeId { get; set; }
+
+                public required virtual AttributeType Type { get; set; }
+
+                public required virtual AttributeValue Value { get; set; }
+            }
+
+            public class Detail
+            {
+                public Guid Id { get; set; }
+
+                [JsonIgnore]
+                public Guid ProductVariantId { get; set; }
+
+                public required string Name { get; set; }
+
+                public required string Value { get; set; }
+            }
+
+            public class Image
+            {
+                public Guid Id { get; set; }
+
+                [JsonIgnore]
+                public Guid ProductId { get; set; }
+
+                public required string Src { get; set; }
+
+                public required string Alt { get; set; }
+
+                public required int Position { get; set; }
+            }
+
+            public Guid Id { get; set; }
+
+            [JsonIgnore]
+            public Guid ProductId { get; set; }
+
+            public required string Name { get; set; }
+
+            public required string Sku { get; set; }
+
+            public bool? CanSubscribe { get; set; }
+
+            public decimal PriceCurrent { get; set; }
+
+            public decimal PriceBase { get; set; }
+
+            public decimal? SubscriptionDiscountPercentage { get; set; }
+
+            public int Stock { get; set; }
+
+            public int? AllowanceOverride { get; set; }
+
+            public bool Active { get; set; }
+
+            public DateTime ReleaseDate { get; set; }
+
+            public virtual ICollection<Attribute> Attributes { get; set; } = new List<Attribute>();
+
+            public virtual ICollection<Detail> Details { get; set; } = new List<Detail>();
+
+            public virtual ICollection<Image> Images { get; set; } = new List<Image>();
+        }
+
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public required string Slug { get; set; }
+
+        public int Allowance { get; set; }
+
+        public List<string>? Tags { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public virtual ICollection<Detail> Details { get; set; } = new List<Detail>();
+
+        public virtual ICollection<Image> Images { get; set; } = new List<Image>();
+
+        public required virtual ProductRating Rating { get; set; }
+
+        public virtual ICollection<Variant> Variants { get; set; } = new List<Variant>();
+
+        public required float Score { get; set; }
+    }
+}
