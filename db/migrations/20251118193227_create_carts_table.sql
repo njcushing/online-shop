66e1b9eca481c69cf682bd9c89b3e49aa0322b8a
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-18 19:37:37 +0000

    Add 'cart'-related tables to database after 'refresh_tokens' table
    Due to the fact the 'cart' table references the 'refresh_tokens' table.

--- db/migrations/20251118193227_create_carts_table.sql
+++ db/migrations/20251118193227_create_carts_table.sql
@@ -1,7 +1,8 @@
 -- migrate:up
 CREATE TABLE carts (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    token uuid NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    refresh_token_id uuid NOT NULL REFERENCES refresh_tokens(id) ON DELETE CASCADE,
+    user_id uuid REFERENCES users(id) ON DELETE CASCADE,
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz
 );
