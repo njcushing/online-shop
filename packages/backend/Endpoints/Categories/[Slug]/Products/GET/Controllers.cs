b501c6e2d98d06ab7b80a31a9e2d094ad9e4a2f0
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-19 23:40:41 +0000

    Create /api/categories/{slug}/products endpoint

---
+++ packages/backend/Endpoints/Categories/[Slug]/Products/GET/Controllers.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cafree.Api.Data;
+
+namespace Cafree.Api.Endpoints.Categories._Slug.Products.GET
+{
+    [ApiController]
+    [Route("api/categories/{slug}/products")]
+    public class GetCategoryBySlugProductsController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<GetCategoryBySlugProductsResponseDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetCategoryBySlugProducts(string slug, [FromQuery] GetCategoryBySlugProductsRequestDto query)
+        {
+            var category = await _context.Categories
+                .Where(c => c.Slug == slug)
+                .AsNoTracking()
+                .AsSplitQuery()
+                .FirstOrDefaultAsync();
+
+            if (category == null) return Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Category not found",
+                detail: $"No category with the specified slug '{slug}' could be located."
+            );
+
+            var productQuery = _context.CategoryProducts
+                .Where(cp => cp.CategoryId == category.Id)
+                .AsNoTracking();
+
+            int pageSize = query.PageSize ?? 10;
+
+            var products = await productQuery
+                .Skip((query.Page - 1) * pageSize)
+                .Take(pageSize)
+                .Select(cp => new GetCategoryBySlugProductsResponseDto
+                {
+                    Id = cp.Product.Id,
+                    Name = cp.Product.Name,
+                    Slug = cp.Product.Slug,
+                    Allowance = cp.Product.Allowance,
+                    Tags = cp.Product.Tags,
+                    ReleaseDate = cp.Product.ReleaseDate,
+                    Rating = new GetCategoryBySlugProductsResponseDto.ProductRating
+                    {
+                        Average = cp.Product.ProductRating!.Average,
+                        Total = cp.Product.ProductRating.Total,
+                        Quantities = new GetCategoryBySlugProductsResponseDto.ProductRating.RatingQuantities
+                        {
+                            Rating5 = cp.Product.ProductRating.Rating5,
+                            Rating4 = cp.Product.ProductRating.Rating4,
+                            Rating3 = cp.Product.ProductRating.Rating3,
+                            Rating2 = cp.Product.ProductRating.Rating2,
+                            Rating1 = cp.Product.ProductRating.Rating1,
+                        }
+                    },
+                    Attributes = cp.Product.ProductAttributeOrders.Select(pao => new GetCategoryBySlugProductsResponseDto.AttributeOrder
+                    {
+                        Position = pao.Position,
+                        Name = pao.ProductAttribute.Name,
+                        Title = pao.ProductAttribute.Title,
+                    }).ToList(),
+                    Images = cp.Product.ProductImages.Select(pi => new GetCategoryBySlugProductsResponseDto.Image
+                    {
+                        Id = pi.Id,
+                        Src = pi.Src,
+                        Alt = pi.Alt,
+                        Position = pi.Position,
+                    }).ToList(),
+                    Variants = cp.Product.ProductVariants.Select(pv => new GetCategoryBySlugProductsResponseDto.Variant
+                    {
+                        Id = pv.Id,
+                        Name = pv.Name,
+                        Sku = pv.Sku,
+                        CanSubscribe = pv.CanSubscribe,
+                        PriceCurrent = pv.PriceCurrent,
+                        PriceBase = pv.PriceBase,
+                        SubscriptionDiscountPercentage = pv.SubscriptionDiscountPercentage,
+                        Stock = pv.Stock,
+                        AllowanceOverride = pv.AllowanceOverride,
+                        Active = pv.Active,
+                        ReleaseDate = pv.ReleaseDate,
+                        Attributes = pv.ProductVariantAttributes.Select(pva => new GetCategoryBySlugProductsResponseDto.Variant.Attribute
+                        {
+                            Type = new GetCategoryBySlugProductsResponseDto.Variant.Attribute.AttributeType
+                            {
+                                Id = pva.ProductAttribute.Id,
+                                Name = pva.ProductAttribute.Name,
+                                Title = pva.ProductAttribute.Title,
+                            },
+                            Value = new GetCategoryBySlugProductsResponseDto.Variant.Attribute.AttributeValue
+                            {
+                                Code = pva.ProductAttributeValue.Code,
+                                Name = pva.ProductAttributeValue.Name,
+                                Position = pva.ProductAttributeValue.Position,
+                            },
+                        }).ToList(),
+                        Images = pv.ProductVariantImages.Select(pvi => new GetCategoryBySlugProductsResponseDto.Variant.Image
+                        {
+                            Id = pvi.Id,
+                            Src = pvi.Src,
+                            Alt = pvi.Alt,
+                            Position = pvi.Position,
+                        }).ToList(),
+                    }).ToList(),
+                })
+                .ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
