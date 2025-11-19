bf00fcd19b4e9add28234b9c060ec405aee49b25
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-19 23:22:47 +0000

    No longer return a category's products from /api/categories/{slug} GET

---
+++ packages/backend/Endpoints/Categories/[Slug]/GET/Controllers.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cafree.Api.Data;
+
+namespace Cafree.Api.Endpoints.Categories._Slug.GET
+{
+    [ApiController]
+    [Route("api/categories/{slug}")]
+    public class GetCategoryBySlugController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        [ProducesResponseType(typeof(GetCategoryBySlugResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetCategoryBySlug(string slug)
+        {
+            var category = await _context.Categories
+                .Where(c => c.Slug == slug)
+                .AsNoTracking()
+                .AsSplitQuery()
+                .Select(c => new GetCategoryBySlugResponseDto
+                {
+                    Name = c.Name,
+                    Slug = c.Slug,
+                    Description = c.Description,
+                    Subcategories = c.InverseParent.Select(ip => new GetCategoryBySlugResponseDto.Subcategory
+                    {
+                        Name = ip.Name,
+                        Slug = ip.Slug,
+                        Description = ip.Description,
+                    }).ToList(),
+                })
+                .FirstOrDefaultAsync();
+
+            if (category == null) return Problem(
+                statusCode: StatusCodes.Status404NotFound,
+                title: "Category not found",
+                detail: $"No category with the specified slug '{slug}' could be located."
+            );
+
+            return Ok(category);
+        }
+    }
+}
---
+++ packages/backend/Endpoints/Categories/[Slug]/GET/Dtos.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+namespace Cafree.Api.Endpoints.Categories._Slug.GET
+{
+    public class GetCategoryBySlugResponseDto
+    {
+        public class Subcategory
+        {
+            [JsonIgnore]
+            public Guid Id { get; set; }
+
+            public string Name { get; set; } = null!;
+
+            public string Slug { get; set; } = null!;
+
+            public string? Description { get; set; }
+        }
+
+        [JsonIgnore]
+        public Guid Id { get; set; }
+
+        [JsonIgnore]
+        public Guid? ParentId { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Slug { get; set; }
+
+        public string? Description { get; set; }
+
+        public virtual ICollection<Subcategory> Subcategories { get; set; } = new List<Subcategory>();
+    }
+}
