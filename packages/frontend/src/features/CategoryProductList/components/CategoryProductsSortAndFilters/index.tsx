cfd18418107d5586faf872db7e121a96a7054ef9
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-24 23:14:17 +0000

    Write JSX and styles for listing color-type cetegory filters

--- packages/frontend/src/features/CategoryProductList/components/CategoryProductsSortAndFilters/index.tsx
+++ packages/frontend/src/features/CategoryProductList/components/CategoryProductsSortAndFilters/index.tsx
@@ -1,5 +1,57 @@
+import { useContext } from "react";
+import { CategoryContext } from "@/pages/Category";
+import { skeletonCategory } from "@/utils/products/categories";
+import { useQueryContexts } from "@/hooks/useQueryContexts";
+import { ResponseBody as GetCategoryBySlugResponseDto } from "@/api/categories/[slug]/GET";
 import styles from "./index.module.css";
 
 export function CategoryProductsSortAndFilters() {
-    return <div className={styles["category-products-sort-and-filters"]}></div>;
+    const { categoryData } = useContext(CategoryContext);
+
+    let category = skeletonCategory as GetCategoryBySlugResponseDto;
+
+    const { data, awaitingAny: contextAwaitingAny } = useQueryContexts({
+        contexts: [{ name: "category", context: categoryData }],
+    });
+
+    if (!contextAwaitingAny) {
+        if (data.category) category = data.category;
+    }
+
+    const { filters } = category;
+
+    return (
+        <div className={styles["category-products-sort-and-filters"]}>
+            {filters.map((filter) => {
+                const { name: filterName, values } = filter;
+
+                return (
+                    <div className={styles["filter"]} key={filterName}>
+                        <p className={styles["filter-name"]}>{filterName}</p>
+                        <ul className={styles["filter-colors"]}>
+                            {values.map((value) => {
+                                const { code, name: valueName, value: valueString, count } = value;
+
+                                return (
+                                    <button
+                                        type="button"
+                                        className={styles["filter-value-color"]}
+                                        key={code}
+                                    >
+                                        <div
+                                            className={styles["filter-value-color-box"]}
+                                            data-valid-color={!!valueString}
+                                            style={{ backgroundColor: valueString }}
+                                        ></div>
+                                        <p className={styles["filter-value-name"]}>{valueName}</p>
+                                        <p className={styles["filter-value-count"]}>({count})</p>
+                                    </button>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
