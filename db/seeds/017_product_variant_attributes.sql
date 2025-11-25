c6f003248a5bb31eabefd55c87fa5e1882196938
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-25 21:08:52 +0000

    Add more seed product variants to development database

--- db/seeds/017_product_variant_attributes.sql
+++ db/seeds/017_product_variant_attributes.sql
@@ -17,7 +17,25 @@ VALUES
     ),
     (
         (SELECT id FROM products WHERE slug='coffee-whole-bean-250g'),
+        (SELECT id FROM product_variants WHERE sku='COF-WB-250G-XD'),
+        (SELECT id FROM product_attributes WHERE name='Blend'),
+        (SELECT get_product_attribute_value_id('Blend', 'XD'))
+    ),
+    (
+        (SELECT id FROM products WHERE slug='coffee-whole-bean-250g'),
         (SELECT id FROM product_variants WHERE sku='COF-WB-250G-DK'),
+        (SELECT id FROM product_attributes WHERE name='Blend'),
+        (SELECT get_product_attribute_value_id('Blend', 'DK'))
+    ),
+    (
+        (SELECT id FROM products WHERE slug='coffee-whole-bean-250g'),
+        (SELECT id FROM product_variants WHERE sku='COF-WB-250G-MO'),
+        (SELECT id FROM product_attributes WHERE name='Blend'),
+        (SELECT get_product_attribute_value_id('Blend', 'MO'))
+    ),
+    (
+        (SELECT id FROM products WHERE slug='coffee-whole-bean-500g'),
+        (SELECT id FROM product_variants WHERE sku='COF-WB-500G-DK'),
         (SELECT id FROM product_attributes WHERE name='Blend'),
         (SELECT get_product_attribute_value_id('Blend', 'DK'))
     ),
@@ -35,15 +53,21 @@ VALUES
     ),
     (
         (SELECT id FROM products WHERE slug='coffee-whole-bean-500g'),
-        (SELECT id FROM product_variants WHERE sku='COF-WB-500G-DK'),
+        (SELECT id FROM product_variants WHERE sku='COF-WB-500G-IN'),
         (SELECT id FROM product_attributes WHERE name='Blend'),
-        (SELECT get_product_attribute_value_id('Blend', 'DK'))
+        (SELECT get_product_attribute_value_id('Blend', 'IN'))
     ),
     (
         (SELECT id FROM products WHERE slug='coffee-whole-bean-1kg'),
         (SELECT id FROM product_variants WHERE sku='COF-WB-1KG-LT'),
         (SELECT id FROM product_attributes WHERE name='Blend'),
         (SELECT get_product_attribute_value_id('Blend', 'LT'))
+    ),
+    (
+        (SELECT id FROM products WHERE slug='coffee-whole-bean-1kg'),
+        (SELECT id FROM product_variants WHERE sku='COF-WB-1KG-PK'),
+        (SELECT id FROM product_attributes WHERE name='Blend'),
+        (SELECT get_product_attribute_value_id('Blend', 'PK'))
     ),
     (
         (SELECT id FROM products WHERE slug='coffee-whole-bean-1kg'),
@@ -56,6 +80,12 @@ VALUES
         (SELECT id FROM product_variants WHERE sku='COF-WB-1KG-DK'),
         (SELECT id FROM product_attributes WHERE name='Blend'),
         (SELECT get_product_attribute_value_id('Blend', 'DK'))
+    ),
+    (
+        (SELECT id FROM products WHERE slug='coffee-whole-bean-1kg'),
+        (SELECT id FROM product_variants WHERE sku='COF-WB-1KG-ES'),
+        (SELECT id FROM product_attributes WHERE name='Blend'),
+        (SELECT get_product_attribute_value_id('Blend', 'ES'))
     );
 
 COMMIT;
