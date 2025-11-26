e8b29699b58f8d4617a761fe7d26347a1b7c88b6
njcushing <129661299+njcushing@users.noreply.github.com>
2025-11-26 22:43:46 +0000

    Create NumericFilter component

---
+++ packages/frontend/src/features/CategoryProductList/components/CategoryProductsSortAndFilters/components/NumericFilter/index.tsx
@@ -0,0 +1,17 @@
+import { RangeSlider } from "@mantine/core";
+import { ResponseBody as GetCategoryBySlugResponseDto } from "@/api/categories/[slug]/GET";
+
+export type TNumericFilter = {
+    data: GetCategoryBySlugResponseDto["filters"][number];
+    awaiting?: boolean;
+};
+
+export function NumericFilter({ data, awaiting = false }: TNumericFilter) {
+    const { values } = data;
+
+    const min = Math.min(...values.map((v) => Number(v.value)));
+    const max = Math.max(...values.map((v) => Number(v.value)));
+    const step = (Math.floor(Math.log10(max - min)) - 2) ** 10;
+
+    return <RangeSlider color="black" size="lg" min={min} max={max} step={step} />;
+}
--- packages/frontend/src/features/CategoryProductList/components/CategoryProductsSortAndFilters/index.tsx
+++ packages/frontend/src/features/CategoryProductList/components/CategoryProductsSortAndFilters/index.tsx
@@ -1,11 +1,11 @@
 import { useContext, useCallback } from "react";
-import { RangeSlider } from "@mantine/core";
 import { CategoryContext } from "@/pages/Category";
 import { skeletonCategory } from "@/utils/products/categories";
 import { useQueryContexts } from "@/hooks/useQueryContexts";
 import { ResponseBody as GetCategoryBySlugResponseDto } from "@/api/categories/[slug]/GET";
-import { ColorFilter } from "./components/ColorFilter";
 import { StringFilter } from "./components/StringFilter";
+import { NumericFilter } from "./components/NumericFilter";
+import { ColorFilter } from "./components/ColorFilter";
 import { SelectFilter } from "./components/SelectFilter";
 import styles from "./index.module.css";
 
@@ -26,18 +26,13 @@ export function CategoryProductsSortAndFilters() {
 
     const filterElements = useCallback(
         (filter: GetCategoryBySlugResponseDto["filters"][number]) => {
-            const { type, values } = filter;
+            const { type } = filter;
 
             switch (type) {
                 case "string":
                     return <StringFilter data={filter} awaiting={awaitingAny} />;
-                case "numeric": {
-                    const min = Math.min(...values.map((v) => Number(v.value)));
-                    const max = Math.max(...values.map((v) => Number(v.value)));
-                    const step = (Math.floor(Math.log10(max - min)) - 2) ** 10;
-
-                    return <RangeSlider color="black" size="lg" min={min} max={max} step={step} />;
-                }
+                case "numeric":
+                    return <NumericFilter data={filter} awaiting={awaitingAny} />;
                 case "boolean":
                     return null;
                 case "color":
