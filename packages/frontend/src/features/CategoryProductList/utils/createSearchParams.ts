397651bfa75e1f6ee2c50ce88d9b5381f733d438
njcushing <129661299+njcushing@users.noreply.github.com>
2025-12-03 23:21:54 +0000

    Write 'createSearchParams' function

---
+++ packages/frontend/src/features/CategoryProductList/utils/createSearchParams.ts
@@ -0,0 +1,30 @@
+import { ICategoryProductListContext } from "..";
+
+export const createSearchParams = (params: {
+    filters: ICategoryProductListContext["filterSelections"];
+    sort: ICategoryProductListContext["sortSelection"];
+    page: number;
+    pageSize: number;
+}): URLSearchParams => {
+    const { filters, sort, page, pageSize } = params;
+
+    const searchParams = new URLSearchParams();
+
+    if (filters.size > 0) {
+        const filterString = [...filters.entries()]
+            .flatMap((entry) => {
+                const [key, value] = entry;
+                if (typeof value === "string") return `${key}=${value}`;
+                if (Array.isArray(value)) return `${key}=${(value as Array<string>).join("|")}`;
+                return [];
+            })
+            .join("~");
+        searchParams.set("filter", `${filterString}`);
+    }
+
+    if (sort) searchParams.set("sort", sort);
+    if (page) searchParams.set("page", `${page}`);
+    if (pageSize) searchParams.set("pageSize", `${pageSize}`);
+
+    return searchParams;
+};
