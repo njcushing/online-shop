bf029eaa7515a348fa97578adbe05814a0491456
njcushing <129661299+njcushing@users.noreply.github.com>
2025-12-16 23:12:24 +0000

    Ensure URL search params aren't cleared on CategoryProductsList mount

--- packages/frontend/src/features/CategoryProductList/index.tsx
+++ packages/frontend/src/features/CategoryProductList/index.tsx
@@ -75,8 +75,6 @@ export function CategoryProductList() {
     const { categories } = useContext(RootContext);
     const { urlPathSplit, categoryBranch, categoryData } = useContext(CategoryContext);
 
-    let category = skeletonCategory as GetCategoryBySlugResponseDto;
-
     const { data, awaitingAny: contextAwaitingAny } = useQueryContexts({
         contexts: [
             { name: "categories", context: categories },
@@ -84,29 +82,24 @@ export function CategoryProductList() {
         ],
     });
 
+    const category = useMemo(() => {
+        if (!contextAwaitingAny && data.categories && data.category) return data.category;
+        return skeletonCategory as GetCategoryBySlugResponseDto;
+    }, [data.categories, data.category, contextAwaitingAny]);
+
     const [searchParams, setSearchParams] = useSearchParams();
+    const searchParamsRef = useRef<URLSearchParams>(searchParams);
+    useEffect(() => {
+        searchParamsRef.current = searchParams;
+    }, [searchParams]);
 
     const [filterSelections, filterSelectionsSetter] = useState<
         ICategoryProductListContext["filterSelections"]
-    >(new Map(parseSearchParams(searchParams, category.filters).filters));
-    const [sortSelection, setSortSelection] = useState<
-        ICategoryProductListContext["sortSelection"]
-    >(parseSearchParams(searchParams, category.filters).sort || null);
-    const [page, setPage] = useState<number>(
-        parseSearchParams(searchParams, category.filters).page,
-    );
-    const [pageSize, setPageSize] = useState<number>(
-        parseSearchParams(searchParams, category.filters).pageSize,
-    );
-
-    let productsData = {
-        products: mockProducts,
-        price: { min: 0, max: 0 },
-    } as GetCategoryBySlugProductsResponseDto;
-
-    if (!contextAwaitingAny) {
-        if (data.categories && data.category) category = data.category;
-    }
+    >(new Map());
+    const [sortSelection, setSortSelection] =
+        useState<ICategoryProductListContext["sortSelection"]>(null);
+    const [page, setPage] = useState<number>(1);
+    const [pageSize, setPageSize] = useState<number>(defaultPageSize);
 
     const products = useAsync.GET(
         getCategoryBySlugProducts,
@@ -115,15 +108,17 @@ export function CategoryProductList() {
                 params: {
                     path: { slug: urlPathSplit.at(-1)! },
                     query: {
-                        filter: searchParams.get("filter") ?? undefined,
-                        sort: searchParams.get("sort") ?? undefined,
+                        filter: searchParamsRef.current.get("filter") ?? undefined,
+                        sort: searchParamsRef.current.get("sort") ?? undefined,
                         page:
-                            searchParams.has("page") && isNumeric(searchParams.get("page")!)
-                                ? Number(searchParams.get("page"))
+                            searchParamsRef.current.has("page") &&
+                            isNumeric(searchParamsRef.current.get("page")!)
+                                ? Number(searchParamsRef.current.get("page"))
                                 : 1,
                         pageSize:
-                            searchParams.has("pageSize") && isNumeric(searchParams.get("pageSize")!)
-                                ? Number(searchParams.get("pageSize"))
+                            searchParamsRef.current.has("pageSize") &&
+                            isNumeric(searchParamsRef.current.get("pageSize")!)
+                                ? Number(searchParamsRef.current.get("pageSize"))
                                 : undefined,
                     },
                 },
@@ -138,14 +133,44 @@ export function CategoryProductList() {
         awaiting: productsAwaiting,
     } = products;
 
-    if (!productsAwaiting) {
-        if (productsResponse.success) productsData = productsResponse.data;
-    }
+    const productsData = useMemo(() => {
+        if (!productsAwaiting && productsResponse.success) return productsResponse.data;
+        return {
+            products: mockProducts,
+            price: { min: 0, max: 0 },
+        } as GetCategoryBySlugProductsResponseDto;
+    }, [productsResponse, productsAwaiting]);
+
+    const getProducts = useCallback(
+        (params: URLSearchParams) => {
+            productsSetParams([
+                {
+                    params: {
+                        path: { slug: urlPathSplit.at(-1)! },
+                        query: {
+                            filter: params.get("filter") ?? undefined,
+                            sort: params.get("sort") ?? undefined,
+                            page:
+                                params.has("page") && isNumeric(params.get("page")!)
+                                    ? Number(params.get("page"))
+                                    : 1,
+                            pageSize:
+                                params.has("pageSize") && isNumeric(params.get("pageSize")!)
+                                    ? Number(params.get("pageSize"))
+                                    : undefined,
+                        },
+                    },
+                },
+            ]);
+            productsAttempt();
+        },
+        [urlPathSplit, productsSetParams, productsAttempt],
+    );
 
     /**
      * Custom setter to avoid unnecessary rerenders when filterSelections is set to a new Map
-     * instance even though it has identical key-value pairs, as a change in the Map's reference
-     * triggers the useEffect hook.
+     * instance even though it has identical key-value pairs, and to control when new queries are
+     * sent to the server
      */
     const setFilterSelections = useCallback(
         (newValue: Filters | ((curr: Filters) => Filters), preventRerender: boolean = false) => {
@@ -164,51 +189,88 @@ export function CategoryProductList() {
         [],
     );
 
+    const awaitingCategory =
+        categoryData.awaiting || categoryData.response.status === customStatusCodes.unattempted;
+    const awaitingCategoryRef = useRef<boolean>(awaitingCategory);
     useEffect(() => {
-        productsSetParams([
-            {
-                params: {
-                    path: { slug: urlPathSplit.at(-1)! },
-                    query: {
-                        filter: searchParams.get("filter") ?? undefined,
-                        sort: searchParams.get("sort") ?? undefined,
-                        page:
-                            searchParams.has("page") && isNumeric(searchParams.get("page")!)
-                                ? Number(searchParams.get("page"))
-                                : 1,
-                        pageSize:
-                            searchParams.has("pageSize") && isNumeric(searchParams.get("pageSize")!)
-                                ? Number(searchParams.get("pageSize"))
-                                : undefined,
-                    },
-                },
-            },
-        ]);
-        productsAttempt();
-    }, [urlPathSplit, productsSetParams, productsAttempt, searchParams]);
+        awaitingCategoryRef.current = awaitingCategory;
+    }, [awaitingCategory]);
+
+    /**
+     * Parse URL search params when category is successfully fetched to remove invalid params from
+     * URL and update URL search params accordingly
+     */
+    useEffect(() => {
+        if (awaitingCategory) return;
+
+        const { current } = searchParamsRef;
+
+        const parsedSearchParams = parseSearchParams(current, category.filters);
+
+        setFilterSelections(parsedSearchParams.filters);
+        setSortSelection(parsedSearchParams.sort);
+        setPage(parsedSearchParams.page);
+        setPageSize(parsedSearchParams.pageSize);
+
+        const newSearchParams = createSearchParams(parsedSearchParams);
+        setSearchParams(newSearchParams);
+    }, [category.filters, setSearchParams, setFilterSelections, getProducts, awaitingCategory]);
 
     useEffect(() => {
-        const newSearchParams = createSearchParams({
+        if (awaitingCategory) return;
+
+        const newParams = createSearchParams({
             filters: filterSelections,
             sort: sortSelection,
             page,
             pageSize,
         });
-        setSearchParams(newSearchParams);
-    }, [setSearchParams, filterSelections, sortSelection, page, pageSize]);
+
+        if (newParams.get("filter")) searchParams.set("filter", newParams.get("filter")!);
+        else if (searchParams.has("filter")) searchParams.delete("filter");
+
+        if (newParams.get("sort")) searchParams.set("sort", newParams.get("sort")!);
+        else if (searchParams.has("sort")) searchParams.delete("sort");
+
+        if (newParams.get("page")) searchParams.set("page", newParams.get("page")!);
+        else if (searchParams.has("page")) searchParams.delete("page");
+
+        if (newParams.get("pageSize")) searchParams.set("pageSize", newParams.get("pageSize")!);
+        else if (searchParams.has("pageSize")) searchParams.delete("pageSize");
+
+        window.history.pushState(
+            {},
+            "",
+            searchParams.size > 0 ? `?${searchParams.toString()}` : window.location.pathname,
+        );
+
+        getProducts(newParams);
+    }, [
+        searchParams,
+        filterSelections,
+        sortSelection,
+        page,
+        pageSize,
+        getProducts,
+        awaitingCategory,
+    ]);
+
+    const clearSearchParams = useCallback(() => {
+        setFilterSelections(new Map());
+        setSortSelection(null);
+        setPage(1);
+        setPageSize(defaultPageSize);
+        setSearchParams(new URLSearchParams());
+    }, [setFilterSelections, setSearchParams]);
 
     // Clear search params when navigating to other categories
     const cachedCategoryName = useRef<string>(urlPathSplit.at(-1)!);
     useEffect(() => {
         if (cachedCategoryName.current !== urlPathSplit.at(-1)!) {
-            setFilterSelections(new Map());
-            setSortSelection(null);
-            setPage(1);
-            setPageSize(defaultPageSize);
-            setSearchParams(new URLSearchParams());
+            clearSearchParams();
             cachedCategoryName.current = urlPathSplit.at(-1)!;
         }
-    }, [urlPathSplit, setFilterSelections, setSearchParams]);
+    }, [urlPathSplit, clearSearchParams]);
 
     const displaySidebar = useMemo(() => {
         const currentCategory = categoryBranch.find((l) => l.slug === urlPathSplit.at(-1));
@@ -216,9 +278,6 @@ export function CategoryProductList() {
         if (currentCategory.subcategories.length > 0) return false;
         return true;
     }, [urlPathSplit, categoryBranch]);
-
-    const awaitingCategory =
-        categoryData.awaiting || categoryData.response.status === customStatusCodes.unattempted;
 
     const awaitingProducts =
         productsAwaiting || productsResponse.status === customStatusCodes.unattempted;
@@ -235,13 +294,20 @@ export function CategoryProductList() {
 
     useEffect(() => {
         if (awaitingCategory) return;
+
         setFilterSelections((curr) => {
             const allFilterNames = new Set<string>([
                 ...category.filters.map((filter) => filter.name),
             ]);
             const newSelections = new Map(curr);
             newSelections.keys().forEach((key) => {
-                if (!allFilterNames.has(key) && key !== "Rating") newSelections.delete(key);
+                if (
+                    !allFilterNames.has(key) &&
+                    key.toLowerCase() !== "rating" &&
+                    key.toLowerCase() !== "price"
+                ) {
+                    newSelections.delete(key);
+                }
             });
             return newSelections;
         }, true);
