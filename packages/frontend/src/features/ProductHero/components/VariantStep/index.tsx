80d17b780e6a38a0560360aca0a10d16471bbbca
njcushing <129661299+njcushing@users.noreply.github.com>
2025-12-20 13:00:43 +0000

    Create styled options for other types of variant attribute values

--- packages/frontend/src/features/ProductHero/components/VariantStep/index.tsx
+++ packages/frontend/src/features/ProductHero/components/VariantStep/index.tsx
@@ -1,5 +1,6 @@
 import { useContext, useCallback, useMemo } from "react";
 import { ProductContext } from "@/pages/Product";
+import { Checkbox } from "@mantine/core";
 import { ResponseBody as GetProductBySlugResponseDto } from "@/api/products/[slug]/GET";
 import styles from "./index.module.css";
 
@@ -8,7 +9,7 @@ export type TVariantStep = {
 };
 
 export function VariantStep({ attribute }: TVariantStep) {
-    const { name, type, values } = attribute;
+    const { name, title, type, values } = attribute;
 
     const { selectedAttributeParams, setSelectedAttributeParams } = useContext(ProductContext);
 
@@ -22,14 +23,39 @@ export function VariantStep({ attribute }: TVariantStep) {
     );
 
     const items = useMemo(() => {
-        return values.map((v) => {
-            const { code, name: valueName, value } = v;
+        switch (type) {
+            case "text":
+            case "numeric":
+            case "date":
+            case "select": {
+                return values.map((v) => {
+                    const { code, name: valueName } = v;
 
-            const isSelected =
-                name in selectedAttributeParams && selectedAttributeParams[name] === code;
+                    const isSelected =
+                        name in selectedAttributeParams && selectedAttributeParams[name] === code;
 
-            switch (type) {
-                case "color": {
+                    return (
+                        <button
+                            type="button"
+                            onClick={() => onClick && onClick(code)}
+                            className={styles["variant-step-text"]}
+                            data-selected={isSelected}
+                            tabIndex={isSelected ? -1 : 0}
+                            disabled={isSelected}
+                            key={`variant-options-${name}-${code}`}
+                        >
+                            {valueName}
+                        </button>
+                    );
+                });
+            }
+            case "color": {
+                return values.map((v) => {
+                    const { code, name: valueName, value } = v;
+
+                    const isSelected =
+                        name in selectedAttributeParams && selectedAttributeParams[name] === code;
+
                     return (
                         <button
                             type="button"
@@ -47,17 +73,46 @@ export function VariantStep({ attribute }: TVariantStep) {
                             {valueName}
                         </button>
                     );
-                }
-                default:
-                    return null;
+                });
             }
-        });
+            default:
+                return null;
+        }
     }, [values, name, type, selectedAttributeParams, onClick]);
+
+    const content = useMemo(() => {
+        if (type === "boolean") {
+            const isSelected =
+                name in selectedAttributeParams && selectedAttributeParams[name] === "true";
+
+            return (
+                <Checkbox
+                    label={<p className={styles["variant-step-title"]}>{name}</p>}
+                    onChange={() => onClick && onClick(isSelected ? "false" : "true")}
+                    checked={isSelected}
+                    data-selected={isSelected}
+                    className={styles["variant-step-boolean"]}
+                    classNames={{
+                        root: styles["checkbox-root"],
+                        body: styles["checkbox-body"],
+                        input: styles["checkbox-input"],
+                        label: styles["checkbox-label"],
+                    }}
+                />
+            );
+        }
+
+        return (
+            <>
+                <p className={styles["variant-step-title"]}>{title}</p>
+                <div className={styles["variant-step-options"]}>{items}</div>
+            </>
+        );
+    }, [name, title, type, selectedAttributeParams, onClick, items]);
 
     return (
         <div className={styles["variant-step"]} key={`variant-options-${name}`}>
-            <p className={styles["variant-step-title"]}>{attribute.title}</p>
-            <div className={styles["variant-step-options"]}>{items}</div>
+            {content}
         </div>
     );
 }
