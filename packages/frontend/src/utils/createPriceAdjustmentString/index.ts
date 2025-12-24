79bce51eb03fafc79d30b5be6256098403509f69
njcushing <129661299+njcushing@users.noreply.github.com>
2025-12-24 20:44:28 +0000

    Allow price reduction value to display maximum of 2 decimal places

--- packages/frontend/src/utils/createPriceAdjustmentString/index.ts
+++ packages/frontend/src/utils/createPriceAdjustmentString/index.ts
@@ -1,5 +1,5 @@
 export const createPriceAdjustmentString = (current: number, base: number): string => {
     if (current === base) return "";
     const reduction = (current / Math.max(base, 1)) * 100 - 100;
-    return `${reduction < 0 ? "" : "+"}${reduction.toFixed(0)}%`;
+    return `${reduction < 0 ? "" : "+"}${+reduction.toFixed(2)}%`;
 };
