7f851f32b233fa03fa328be8bfc9588eae683dd8
njcushing <129661299+njcushing@users.noreply.github.com>
2026-01-09 13:01:18 +0000

    Write boilerplate for CoffeeBlendInfoPanels component

---
+++ packages/frontend/src/features/CoffeeBlendInfoPanels/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+export function CoffeeBlendInfoPanels() {
+    return (
+        <section className={styles["coffee-blend-info-panels"]}>
+            <div className={styles["coffee-blend-info-panels-width-controller"]}>test</div>
+        </section>
+    );
+}
