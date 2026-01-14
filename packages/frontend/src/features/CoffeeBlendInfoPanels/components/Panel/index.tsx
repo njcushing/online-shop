8af84f4df0a324f3ba501d16f20ba98f2d611a67
njcushing <129661299+njcushing@users.noreply.github.com>
2026-01-14 22:57:41 +0000

    Render country of origin flags in Panel component

--- packages/frontend/src/features/CoffeeBlendInfoPanels/components/Panel/index.tsx
+++ packages/frontend/src/features/CoffeeBlendInfoPanels/components/Panel/index.tsx
@@ -7,13 +7,31 @@ export type TPanel = {
 };
 
 export function Panel({ data }: TPanel) {
-    const { name } = data;
+    const { name, origins } = data;
 
     return (
         <div className={styles["panel"]}>
-            <p className={styles["name"]}>{name}</p>
+            <div className={styles["content-top"]}>
+                <p className={styles["name"]}>{name}</p>
+
+                <div className={styles["flags"]}>
+                    <Divider orientation="vertical" className={styles["Divider"]} />
+
+                    {origins.map((origin) => (
+                        <div className={`fi fi-${origin} ${styles["flag"]}`} key={origin}></div>
+                    ))}
+                </div>
+            </div>
 
             <Divider className={styles["Divider"]} />
+
+            <div className={styles["content-main"]}>
+                <div className={styles["content-left"]}></div>
+
+                <Divider orientation="vertical" className={styles["Divider"]} />
+
+                <div className={styles["content-right"]}></div>
+            </div>
         </div>
     );
 }
