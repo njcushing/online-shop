32dea31240491fc7cc5b7b5cc276cd94c54f6b75
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-01 14:19:16 +0000

    Create infinitely-scrolling marquee effect in InfoMarquee component

--- packages/frontend/src/features/HomepageContent/components/InfoMarquee/index.tsx
+++ packages/frontend/src/features/HomepageContent/components/InfoMarquee/index.tsx
@@ -1,5 +1,25 @@
+import { useMemo } from "react";
+import { v4 as uuid } from "uuid";
 import styles from "./index.module.css";
 
 export function InfoMarquee() {
-    return <section className={styles["info-marquee"]}></section>;
+    const marqueeItems = useMemo(() => {
+        return <span className={styles["marquee-item"]}>text</span>;
+    }, []);
+
+    return (
+        <section className={styles["info-marquee"]}>
+            <div className={styles["marquee"]}>
+                <div className={styles["marquee-inner"]}>
+                    {Array.from({ length: 100 }).map(() => {
+                        return (
+                            <div className={styles["marquee-track"]} key={uuid()}>
+                                {marqueeItems}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    );
 }
