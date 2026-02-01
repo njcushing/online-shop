a7aacd6a742f5d9b084259ef17572553ad9f1c3c
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-01 13:51:02 +0000

    Render InfoMarquee component within HomepageContent component

--- packages/frontend/src/features/HomepageContent/index.tsx
+++ packages/frontend/src/features/HomepageContent/index.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "./components/Hero";
+import { InfoMarquee } from "./components/InfoMarquee";
 import { CoffeeBlendInfoPanels } from "../CoffeeBlendInfoPanels";
 import { CategoryCards } from "./components/CategoryCards";
 import styles from "./index.module.css";
@@ -7,6 +8,7 @@ export function HomepageContent() {
     return (
         <div className={styles["homepage-content"]}>
             <Hero />
+            <InfoMarquee />
             <CoffeeBlendInfoPanels />
             <CategoryCards />
         </div>
