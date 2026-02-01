9fb47a1899c36434c081ea60d1fa9b8948dd9752
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-01 15:01:54 +0000

    Add relevant symbols and text for marquee items in InfoMarquee

--- packages/frontend/src/features/HomepageContent/components/InfoMarquee/index.tsx
+++ packages/frontend/src/features/HomepageContent/components/InfoMarquee/index.tsx
@@ -1,17 +1,64 @@
 import { useMemo } from "react";
+import { useMatches } from "@mantine/core";
+import {
+    Heart,
+    CoffeeBean,
+    Truck,
+    Empty,
+    Recycle,
+    Leaf,
+    Star,
+    IconProps,
+} from "@phosphor-icons/react";
 import { v4 as uuid } from "uuid";
 import styles from "./index.module.css";
 
 export function InfoMarquee() {
+    const symbolProps = useMatches<IconProps>(
+        {
+            base: {
+                color: "rgba(0, 0, 0, 0.8)",
+                size: 20,
+                weight: "fill",
+            },
+            xs: {
+                color: "rgba(0, 0, 0, 0.8)",
+                size: 22,
+                weight: "fill",
+            },
+        },
+        { getInitialValueInEffect: false },
+    );
+
+    const marqueeItemsInfo = useMemo(() => {
+        return [
+            { symbol: <Heart {...symbolProps} />, text: "Ethically-Sourced" },
+            { symbol: <CoffeeBean {...symbolProps} />, text: "Roasted In-House" },
+            { symbol: <Truck {...symbolProps} />, text: "Free Delivery Available" },
+            { symbol: <Empty {...symbolProps} />, text: "Caffiene-Free" },
+            { symbol: <Recycle {...symbolProps} />, text: "Recyclable Packaging" },
+            { symbol: <Leaf {...symbolProps} />, text: "Biodegradable Pods" },
+            { symbol: <Star {...symbolProps} />, text: "Speciality Blends" },
+        ];
+    }, [symbolProps]);
+
     const marqueeItems = useMemo(() => {
-        return <span className={styles["marquee-item"]}>text</span>;
-    }, []);
+        return marqueeItemsInfo.map((marqueeItem) => {
+            const { symbol, text } = marqueeItem;
+            return (
+                <span className={styles["marquee-item"]} key={text}>
+                    {symbol}
+                    {text}
+                </span>
+            );
+        });
+    }, [marqueeItemsInfo]);
 
     return (
         <section className={styles["info-marquee"]}>
             <div className={styles["marquee"]}>
                 <div className={styles["marquee-inner"]}>
-                    {Array.from({ length: 100 }).map(() => {
+                    {Array.from({ length: 10 }).map(() => {
                         return (
                             <div className={styles["marquee-track"]} key={uuid()}>
                                 {marqueeItems}
