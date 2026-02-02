4c0fe45eeec4a47b84bbd060111bdf2519013522
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-02 22:53:44 +0000

    Add button for toggling collapsible element in SustainabilityBanner

--- packages/frontend/src/features/HomepageContent/components/SustainabilityBanner/index.tsx
+++ packages/frontend/src/features/HomepageContent/components/SustainabilityBanner/index.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+import { Button, Collapse } from "@mantine/core";
+import { CaretDown, CaretUp } from "@phosphor-icons/react";
 import styles from "./index.module.css";
 
 export function SustainabilityBanner() {
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
         <section className={styles["sustainability-banner"]}>
             <div className={styles["sustainability-text-container"]}>
@@ -15,6 +20,27 @@ export function SustainabilityBanner() {
                     cup you make is created with both love and the utmost respect for our world and
                     its inhabitants.`}
                 </div>
+
+                <Collapse in={open}></Collapse>
+
+                <Button
+                    onClick={() => setOpen((c) => !c)}
+                    color="transparent"
+                    variant="filled"
+                    classNames={{ root: styles["Button-root"], label: styles["Button-label"] }}
+                >
+                    {open ? (
+                        <>
+                            <CaretUp color="black" size={20} weight="regular" />
+                            Show less
+                        </>
+                    ) : (
+                        <>
+                            Find out more
+                            <CaretDown color="black" size={20} weight="regular" />
+                        </>
+                    )}
+                </Button>
             </div>
         </section>
     );
