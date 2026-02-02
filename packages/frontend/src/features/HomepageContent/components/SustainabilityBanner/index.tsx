5f26563147b4168f7ab2817954c8274646de9a54
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-02 22:34:47 +0000

    Add heading and opening text to SustainabilityBanner component

--- packages/frontend/src/features/HomepageContent/components/SustainabilityBanner/index.tsx
+++ packages/frontend/src/features/HomepageContent/components/SustainabilityBanner/index.tsx
@@ -1,5 +1,21 @@
 import styles from "./index.module.css";
 
 export function SustainabilityBanner() {
-    return <section className={styles["sustainability-banner"]}></section>;
+    return (
+        <section className={styles["sustainability-banner"]}>
+            <div className={styles["sustainability-text-container"]}>
+                <h2 className={styles["sustainability-heading"]}>
+                    We take sustainability <strong>seriously</strong>.
+                </h2>
+
+                <div className={styles["sustainability-opening-text"]}>
+                    {`At Cafree, we want to bring you the best flavours the world has to offer. We
+                    also believe it's our responsibility to maintain a sustainable approach to the
+                    farming, packaging and transportation of our goods, so that you know every
+                    cup you make is created with both love and the utmost respect for our world and
+                    its inhabitants.`}
+                </div>
+            </div>
+        </section>
+    );
 }
