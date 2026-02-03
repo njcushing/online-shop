82876b3896377e9613211fd9a43608d75c1c1199
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-03 19:49:45 +0000

    Update layout for SustainabilityBanner component

--- packages/frontend/src/features/HomepageContent/components/SustainabilityBanner/index.tsx
+++ packages/frontend/src/features/HomepageContent/components/SustainabilityBanner/index.tsx
@@ -1,28 +1,99 @@
 import { useState } from "react";
-import { Button, Collapse } from "@mantine/core";
+import { useMatches, Button, Collapse } from "@mantine/core";
 import { CaretDown, CaretUp } from "@phosphor-icons/react";
 import styles from "./index.module.css";
 
 export function SustainabilityBanner() {
+    const layout = useMatches<string>(
+        { base: "thin", sm: "wide" },
+        { getInitialValueInEffect: false },
+    );
+
     const [open, setOpen] = useState<boolean>(false);
 
     return (
         <section className={styles["sustainability-banner"]}>
-            <div className={styles["sustainability-text-container"]}>
-                <h2 className={styles["sustainability-heading"]}>
-                    We take sustainability <strong>seriously</strong>.
-                </h2>
+            <h2 className={styles["sustainability-heading"]}>
+                We take sustainability <strong>seriously</strong>.
+            </h2>
 
-                <div className={styles["sustainability-opening-text"]}>
-                    {`At Cafree, we want to bring you the best flavours the world has to offer. We
+            <p className={styles["sustainability-opening-text"]}>
+                {`At Cafree, we want to bring you the best flavours the world has to offer. We
                     also believe it's our responsibility to maintain a sustainable approach to the
                     farming, packaging and transportation of our goods, so that you know every
-                    cup you make is created with both love and the utmost respect for our world and
-                    its inhabitants.`}
+                    cup you make is created with both love and respect for our world and its
+                    inhabitants.`}
+            </p>
+
+            <Collapse in={open} className={styles["Collapse"]}>
+                <div className={styles["Collapse-vertical-padding"]}></div>
+
+                <div className={styles["sustainability-main-sections"]} data-layout={layout}>
+                    <div className={styles["sustainability-main-section-container"]}>
+                        <div className={styles["sustainability-main-section-container-inner"]}>
+                            <h3 className={styles["sustainability-main-section-heading"]}>
+                                Farming
+                            </h3>
+
+                            <p className={styles["sustainability-main-section-body-text"]}>
+                                Placeholder
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles["sustainability-main-section-container"]}>
+                        <div className={styles["sustainability-main-section-container-inner"]}>
+                            <h3 className={styles["sustainability-main-section-heading"]}>
+                                Trading
+                            </h3>
+
+                            <p className={styles["sustainability-main-section-body-text"]}>
+                                Placeholder
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles["sustainability-main-section-container"]}>
+                        <div className={styles["sustainability-main-section-container-inner"]}>
+                            <h3 className={styles["sustainability-main-section-heading"]}>
+                                Recyclable Packaging
+                            </h3>
+
+                            <p className={styles["sustainability-main-section-body-text"]}>
+                                Placeholder
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles["sustainability-main-section-container"]}>
+                        <div className={styles["sustainability-main-section-container-inner"]}>
+                            <h3 className={styles["sustainability-main-section-heading"]}>
+                                Compostable Pods
+                            </h3>
+
+                            <p className={styles["sustainability-main-section-body-text"]}>
+                                Placeholder
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles["sustainability-main-section-container"]}>
+                        <div className={styles["sustainability-main-section-container-inner"]}>
+                            <h3 className={styles["sustainability-main-section-heading"]}>
+                                Our Team
+                            </h3>
+
+                            <p className={styles["sustainability-main-section-body-text"]}>
+                                Placeholder
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
-                <Collapse in={open}></Collapse>
+                <div className={styles["Collapse-vertical-padding"]}></div>
+            </Collapse>
 
+            <div className={styles["Button-container"]}>
                 <Button
                     onClick={() => setOpen((c) => !c)}
                     color="transparent"
