d31e380230612b007cfb067def47d00732117296
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-04 19:45:52 +0000

    Create OtherCategoryCards component

---
+++ packages/frontend/src/features/HomepageContent/components/OtherCategoryCards/index.tsx
@@ -0,0 +1,197 @@
+import { useContext, useState, useEffect, useCallback, useRef, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
+import { RootContext } from "@/pages/Root";
+import { useMatches, Image } from "@mantine/core";
+import { Carousel, CarouselProps, Embla } from "@mantine/carousel";
+import { useQueryContexts } from "@/hooks/useQueryContexts";
+import { ArrowLeft, ArrowRight } from "@phosphor-icons/react";
+import { buildCategoriesTree } from "@/utils/products/categories";
+import styles from "./index.module.css";
+
+const slideGapPx = 16;
+
+export function OtherCategoryCards() {
+    const navigate = useNavigate();
+
+    const { categories } = useContext(RootContext);
+
+    const { data, awaitingAny } = useQueryContexts({
+        contexts: [{ name: "categories", context: categories }],
+    });
+
+    const categoriesData = useMemo(() => {
+        if (data.categories) return data.categories;
+        return [];
+    }, [data]);
+
+    const categoryTree = useMemo<ReturnType<typeof buildCategoriesTree>>(() => {
+        return buildCategoriesTree(categoriesData || []);
+    }, [categoriesData]);
+
+    const categoriesToDisplay = useMemo(() => {
+        return categoryTree.flatMap((category) => (category.slug !== "coffee" ? category : []));
+    }, [categoryTree]);
+
+    const [displaySkeletons, setDisplaySkeletons] = useState(true);
+    useEffect(() => setDisplaySkeletons(awaitingAny), [awaitingAny]);
+
+    const emblaRef = useRef<Embla | null>(null);
+
+    /*
+     * I have to do this using useMatches instead of just setting slidesToScroll="auto" directly on
+     * the component and breakpoints on the slideSize prop because the number of indicators doesn't
+     * update when slideSize changes when a breakpoint is crossed.
+     */
+
+    const carouselProps: {
+        slideSize: CarouselProps["slideSize"];
+        slidesToScroll: number;
+    } = useMatches(
+        {
+            base: {
+                slideSize: "100%",
+                slidesToScroll: 1,
+            },
+            xs: {
+                slideSize: `calc((100% / 2) - (${slideGapPx}px * (1 / 2)))`,
+                slidesToScroll: 2,
+            },
+            md: {
+                slideSize: `calc((100% / 3) - (${slideGapPx}px * (2 / 3)))`,
+                slidesToScroll: 3,
+            },
+            lg: {
+                slideSize: `calc((100% / 4) - (${slideGapPx}px * (3 / 4)))`,
+                slidesToScroll: 4,
+            },
+        },
+        { getInitialValueInEffect: false },
+    );
+
+    /**
+     * Don't test logic dependent on window dimensions - window width is 0px in unit tests using
+     * jsdom as an environment
+     */
+    /* v8 ignore start */
+
+    const handleFocus = useCallback(
+        (i: number) => {
+            if (emblaRef.current) {
+                emblaRef.current.scrollTo(Math.floor(i) / carouselProps.slidesToScroll);
+            }
+        },
+        [carouselProps.slidesToScroll],
+    );
+
+    /* v8 ignore stop */
+
+    const carouselSlidesMemo = useMemo(() => {
+        return categoriesToDisplay
+            .slice(0, awaitingAny ? carouselProps.slidesToScroll : undefined)
+            .map((category, i) => {
+                const { name, slug, description } = category;
+
+                return (
+                    <Carousel.Slide
+                        data-last={i === 4}
+                        onFocus={
+                            /* v8 ignore start */
+
+                            () => handleFocus(i)
+
+                            /* v8 ignore stop */
+                        }
+                        className={styles["Carousel-slide"]}
+                        key={slug}
+                    >
+                        <div className={styles["other-category-container"]}>
+                            <div
+                                className={styles["other-category-image-container"]}
+                                style={{ visibility: displaySkeletons ? "hidden" : "initial" }}
+                            >
+                                <Image className={styles["other-category-image"]} src="" alt="" />
+                            </div>
+
+                            <div
+                                className={styles["other-category-information-container"]}
+                                style={{ visibility: displaySkeletons ? "hidden" : "initial" }}
+                            >
+                                <p className={styles["other-category-name"]}>{name}</p>
+
+                                <p className={styles["other-category-description"]}>
+                                    {description}
+                                </p>
+                            </div>
+
+                            <button
+                                type="button"
+                                role="link"
+                                onClick={() => navigate({ pathname: `/c/${slug}` })}
+                                disabled={displaySkeletons}
+                                className={styles["other-category-link"]}
+                            >
+                                Shop
+                            </button>
+                        </div>
+                    </Carousel.Slide>
+                );
+            });
+    }, [
+        navigate,
+        awaitingAny,
+        categoriesToDisplay,
+        displaySkeletons,
+        carouselProps.slidesToScroll,
+        handleFocus,
+    ]);
+
+    const carouselMemo = useMemo(() => {
+        return (
+            <Carousel
+                slideSize={carouselProps.slideSize}
+                slidesToScroll={carouselProps.slidesToScroll}
+                slideGap={`${slideGapPx}px`}
+                includeGapInSize={false}
+                skipSnaps
+                previousControlIcon={<ArrowLeft />}
+                nextControlIcon={<ArrowRight />}
+                withControls={false}
+                withIndicators={carouselProps.slidesToScroll < categoriesToDisplay.length}
+                getEmblaApi={(api) => {
+                    emblaRef.current = api;
+                }}
+                classNames={{
+                    root: styles["Carousel-root"],
+                    container: styles["Carousel-container"],
+                    viewport: styles["Carousel-viewport"],
+                    indicator: styles["Carousel-indicator"],
+                    indicators: styles["Carousel-indicators"],
+                }}
+            >
+                {carouselSlidesMemo}
+            </Carousel>
+        );
+    }, [categoriesToDisplay.length, carouselProps, carouselSlidesMemo]);
+
+    if (!awaitingAny && categoriesData.length === 0) return null;
+
+    return (
+        <section className={styles["other-category-cards"]}>
+            <div className={styles["other-category-cards-width-controller"]}>
+                <div className={styles["top-row-container"]}>
+                    <div className={styles["title-container"]}>
+                        <h2 className={styles["other-category-cards-title"]}>
+                            We {`don't`} only sell coffee...
+                        </h2>
+                        <p className={styles["other-category-cards-subcopy"]}>
+                            Try some of our exceptional tea blends, or explore the rest of our
+                            range.
+                        </p>
+                    </div>
+                </div>
+
+                {carouselMemo}
+            </div>
+        </section>
+    );
+}
