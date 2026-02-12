5aae2d35cde5d0a7679064fc783bb049bceddeef
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-12 11:52:54 +0000

    Wrap site disclaimer content in Collapse component

--- packages/frontend/src/features/Header/components/SiteDisclaimer/index.tsx
+++ packages/frontend/src/features/Header/components/SiteDisclaimer/index.tsx
@@ -1,25 +1,34 @@
 import { useState } from "react";
+import { Collapse } from "@mantine/core";
 import styles from "./index.module.css";
 
 export function SiteDisclaimer() {
     const [open, setOpen] = useState<boolean>(true);
+    const [closing, setClosing] = useState<boolean>(false);
 
     if (!open) return null;
 
     return (
-        <div className={styles["site-disclaimer"]}>
-            <p className={styles["site-disclaimer-message"]}>
-                This site is a portfolio project; any and all information, products and transactions
-                are NOT real
-            </p>
+        <Collapse in={!closing} animateOpacity={false} onTransitionEnd={() => setOpen(false)}>
+            <div className={styles["site-disclaimer"]}>
+                <div className={styles["site-disclaimer-inner"]}>
+                    <p className={styles["site-disclaimer-message"]}>
+                        This site is a portfolio project; any and all information, products and
+                        transactions are NOT real
+                    </p>
 
-            <button
-                type="button"
-                onClick={() => setOpen((o) => !o)}
-                className={styles["site-disclaimer-close-button"]}
-            >
-                ✖
-            </button>
-        </div>
+                    <button
+                        type="button"
+                        onClick={(e) => {
+                            setClosing(true);
+                            e.currentTarget.blur();
+                        }}
+                        className={styles["site-disclaimer-close-button"]}
+                    >
+                        ✖
+                    </button>
+                </div>
+            </div>
+        </Collapse>
     );
 }
