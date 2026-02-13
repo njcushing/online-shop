7960fea3bd1506334ebc73ab682cfdba67c22590
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-13 11:22:01 +0000

    Write 'getAnyBadResponse' utility function

---
+++ packages/frontend/src/hooks/useAsync/utils/getAnyBadResponse/index.ts
@@ -0,0 +1,16 @@
+import * as useAsync from "@/hooks/useAsync";
+import { customStatusCodes } from "@/api/types";
+
+/**
+ * 'any' is appropriate here: all 'UseAsyncReturnType'-type objects will contain the necessary
+ * fields; the specific shapes of the RequestParams, RequestBody and ResponseBody are irrelevant
+ */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const getAnyBadResponse = (
+    ...responses: useAsync.UseAsyncReturnType<any, any, any>[]
+): useAsync.UseAsyncReturnType<any, any, any> | undefined => {
+    return responses.find(
+        (r) => !r.response.success && r.response.status !== customStatusCodes.unattempted,
+    );
+};
+/* eslint-enable @typescript-eslint/no-explicit-any */
