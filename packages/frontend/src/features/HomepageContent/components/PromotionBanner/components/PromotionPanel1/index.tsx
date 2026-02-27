30010d43bd36e27a1067292ac98c26c3e5d54378
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-27 12:28:46 +0000

    Create design for PromotionBanner1 component

--- packages/frontend/src/features/HomepageContent/components/PromotionBanner/components/PromotionPanel1/index.tsx
+++ packages/frontend/src/features/HomepageContent/components/PromotionBanner/components/PromotionPanel1/index.tsx
@@ -4,7 +4,17 @@ export function PromotionPanel1() {
     return (
         <div className={styles["promotion-panel"]}>
             <div className={styles["promotion-panel-inner"]}>
-                <p className={styles["promotion-panel-title"]}>Test Panel 1</p>
+                <p className={styles["title"]}>£10 off*</p>
+
+                <p className={styles["subcopy"]}>when you spend over £100</p>
+
+                <p className={styles["code"]}>
+                    Use code {`'`}
+                    <strong>TAKE10</strong>
+                    {`'`}
+                </p>
+
+                <p className={styles["disclaimer"]}>*promotion applied at checkout</p>
             </div>
         </div>
     );
