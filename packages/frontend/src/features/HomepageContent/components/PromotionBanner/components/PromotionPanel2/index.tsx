b7d0be78a826c7db529d90049d3319e0aac2a4d3
njcushing <129661299+njcushing@users.noreply.github.com>
2026-02-28 13:01:50 +0000

    Create PromotionPanel2 component

---
+++ packages/frontend/src/features/HomepageContent/components/PromotionBanner/components/PromotionPanel2/index.tsx
@@ -0,0 +1,32 @@
+import { Image } from "@mantine/core";
+import styles from "./index.module.css";
+
+export function PromotionPanel2() {
+    return (
+        <div className={styles["promotion-panel"]}>
+            <div className={styles["image-container"]}>
+                <Image
+                    src="https://res.cloudinary.com/djzqtvl9l/image/upload/v1772283523/cafree/matt-brett-ZYpnq8B3Wn0-unsplash-1920_binzfa.jpg"
+                    alt=""
+                    className={styles["image"]}
+                />
+
+                <div className={styles["image-cover"]}></div>
+            </div>
+
+            <div className={styles["promotion-panel-inner"]}>
+                <p className={styles["title"]}>Seasonal Sales</p>
+
+                <p className={styles["subcopy"]}>5% off sitewide on any purchase*</p>
+
+                <p className={styles["code"]}>
+                    Use code {`'`}
+                    <strong>SEASONAL</strong>
+                    {`'`}
+                </p>
+
+                <p className={styles["disclaimer"]}>*promotion applied at checkout</p>
+            </div>
+        </div>
+    );
+}
