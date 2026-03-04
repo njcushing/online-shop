7b8076faf3074a3f7f57fbbecd0e5e497c5e5ccd
njcushing <129661299+njcushing@users.noreply.github.com>
2026-03-04 20:45:05 +0000

    Add more sections to delivery information

--- packages/frontend/src/pages/Delivery/index.tsx
+++ packages/frontend/src/pages/Delivery/index.tsx
@@ -8,7 +8,7 @@ import { ResponseBody as GetSettingsResponseDto } from "@/api/settings/GET";
 import siteConfig from "@/siteConfig.json";
 import styles from "./index.module.css";
 
-const introduction = `
+const introductionMarkdown = `
 You can place online orders with either Standard Delivery or Express
 Delivery. We do not currently offer Nominated Day Delivery (delivered on a
 day of your choosing) for any of our products.
@@ -16,11 +16,23 @@ day of your choosing) for any of our products.
 To view the status of any of your active orders, [sign in](/login) to your account.
 `;
 
-const standardDelivery = `
+const standardDeliveryMarkdown = `
 Our Standard Delivery option is **free** for all addresses in the United Kingdom.
 
 We aim to ship all orders within 48 hours of time of purchase; this is guaranteed if the order is
 placed before 5pm.
+`;
+
+const deliveryToMultipleAddressesMarkdown = `
+We currently only deliver to one address per order. If you would like to have items delivered to
+multiple addresses, you will need to place separate orders for each address.
+`;
+
+const wrongItemsDeliveredMarkdown = `
+If you have been sent the wrong item(s), please contact us within a reasonable period of time. You
+will likely be requested to return the incorrect item(s) to us and the correct item(s) will be
+shipped at no additional cost to you. If this is not possible, and you are eligible for a refund or
+replacement, these options will be discussed with you at the point of contact.
 `;
 
 const defaultAccordionPanelProps: AccordionPanelProps = {
@@ -73,7 +85,9 @@ placed before 5pm.
                     </p>
 
                     <span className={`${styles["introduction"]} ${styles["markdown"]}`}>
-                        <ReactMarkdown remarkPlugins={[remarkGfm]}>{introduction}</ReactMarkdown>
+                        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                            {introductionMarkdown}
+                        </ReactMarkdown>
                     </span>
 
                     <Accordion
@@ -94,7 +108,7 @@ placed before 5pm.
 
                             <Accordion.Panel {...defaultAccordionPanelProps}>
                                 <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                                    {standardDelivery}
+                                    {standardDeliveryMarkdown}
                                 </ReactMarkdown>
                             </Accordion.Panel>
                         </Accordion.Item>
@@ -105,6 +119,30 @@ placed before 5pm.
                             <Accordion.Panel {...defaultAccordionPanelProps}>
                                 <ReactMarkdown remarkPlugins={[remarkGfm]}>
                                     {expressDeliveryMarkdown}
+                                </ReactMarkdown>
+                            </Accordion.Panel>
+                        </Accordion.Item>
+
+                        <Accordion.Item value="delivery-to-multiple-addresses">
+                            <Accordion.Control>
+                                Can my items be delivered to multiple addresses?
+                            </Accordion.Control>
+
+                            <Accordion.Panel {...defaultAccordionPanelProps}>
+                                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                                    {deliveryToMultipleAddressesMarkdown}
+                                </ReactMarkdown>
+                            </Accordion.Panel>
+                        </Accordion.Item>
+
+                        <Accordion.Item value="wrong-items-delivered">
+                            <Accordion.Control>
+                                I{`'`}ve been sent the wrong item(s) - what are my options?
+                            </Accordion.Control>
+
+                            <Accordion.Panel {...defaultAccordionPanelProps}>
+                                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                                    {wrongItemsDeliveredMarkdown}
                                 </ReactMarkdown>
                             </Accordion.Panel>
                         </Accordion.Item>
